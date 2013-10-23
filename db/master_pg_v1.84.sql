97dc1c68b520242cb79db15e9e265876b9f94c11
allan <allan@cellpointmobile.com>
2013-10-23 11:45:23 +0000

    EKW-87: Improve mPoint's logging capabilities
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@464 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

---
+++ db/master_pg_v1.84.sql
@@ -0,0 +1,36 @@
+-- Table: log.operation_tbl
+-- Data table for operations such as saving a card, deleting a card, etc.
+CREATE TABLE log.operation_tbl
+(
+	id SERIAL NOT NULL,
+	name VARCHAR(255),
+	created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	enabled BOOL,
+	CONSTRAINT operation_pk PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE log.operation_tbl TO mpoint;
+
+
+-- Table: log.auditlog_tbl
+-- Logs the activities such as saving a card, deleting a card, etc.
+CREATE TABLE log.auditlog_tbl
+(
+	id SERIAL NOT NULL,
+	operationid INT4 NOT NULL,
+	mobile INT8,
+	email VARCHAR(255),
+	customer_ref VARCHAR(50),
+	code INT4 NOT NULL,
+	message VARCHAR(255),
+	created TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	modified TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	enabled BOOL,
+  	CONSTRAINT auditlog_pk PRIMARY KEY (id),
+  	CONSTRAINT auditlog2operation_fk FOREIGN KEY (operationid)
+      REFERENCES log.operation_tbl (id) 
+      ON UPDATE CASCADE ON DELETE CASCADE
+) WITHOUT OIDS;
+  
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE log.auditlog_tbl TO mpoint;
---
+++ db/setup_pg_v1.83.sql
@@ -0,0 +1,3 @@
+INSERT INTO Log.operation_tbl (id, name) VALUES (1, 'Card saved');
+INSERT INTO Log.operation_tbl (id, name) VALUES (2, 'Card deleted');
+INSERT INTO Log.operation_tbl (id, name) VALUES (3, 'Logged in');
