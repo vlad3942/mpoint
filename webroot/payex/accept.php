20c64f58c5806bab71c7bea2b7e7627f915ba802
allan <allan@cellpointmobile.com>
2013-11-12 12:07:21 +0000

    EBCLASSIC-8: Make "Complete" call to PayEx upon card details being accepted. Fixed and tested.
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@560 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- webroot/payex/accept.php
+++ webroot/payex/accept.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * This file contains the Controller for mPoint's Payment Completed compont.
+ * This file contains the Controller for mPoint's Payment Completed componentt.
  * The component will generate a page using the Client Configuration providing Post Payment options:
  * 	- Send E-Mail Receipt
  * 	- Go to Client's Accept URL
  *
- * @author Jonatan Evald Buus
+ * @author Jonatan Evald Buus, Allan Ray Jasa
  * @copyright Cellpoint Mobile
  * @link http://www.cellpointmobile.com
- * @package NetAxept
+ * @package PayEx
  * @subpackage Accept
  * @version 1.00
  */
@@ -25,24 +25,35 @@ require_once(sCLASS_PATH ."/payex.php");
 // Require data class for Payment Service Provider Configurations
 require_once(sCLASS_PATH ."/pspconfig.php");
 
-file_put_contents(sLOG_PATH ."/error.log", "\n". "I'M HERE! ", FILE_APPEND  | LOCK_EX );
-
-// we need to all auth in this case
-$obj_TxnInfo = TxnInfo::produceInfo($_GET['mpoint-id'] , $_OBJ_DB);
-$_OBJ_TXT = new TranslateText(array(sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/global.txt", sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/custom.txt"), sSYSTEM_PATH, 0, "UTF-8");
+$id = PayEx::getIDFromExternalID($_OBJ_DB, $_GET['transactionNumber']);
+if ($id < 0) { $id = PayEx::getIDFromExternalID($_OBJ_DB, $_GET['orderRef']); }
+$obj_TxnInfo = TxnInfo::produceInfo($id, $_OBJ_DB);
+$obj_mPoint = new PayEx($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo);
 
 $obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $obj_TxnInfo->getClientConfig()->getID(), Constants::iPAYEX_PSP); 
+if ($obj_TxnInfo->getMode() > 0) { $aHTTP_CONN_INFO["payex"]["host"] = str_replace("external.", "test-external.", $aHTTP_CONN_INFO["payex"]["host"]); }
 $aHTTP_CONN_INFO["payex"]["username"] = $obj_PSPConfig->getUsername();
 $aHTTP_CONN_INFO["payex"]["password"] = $obj_PSPConfig->getPassword();
 $obj_ConnInfo = HTTPConnInfo::produceConnInfo($aHTTP_CONN_INFO["payex"]);
 
-$obj_mPoint = new Payex($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo);
+$obj_XML = $obj_mPoint->complete($obj_ConnInfo, $obj_PSPConfig->getMerchantAccount(), $_GET['orderRef'] );
 
-$obj_XML = $obj_mPoint->complete($obj_ConnInfo, $obj_PSPConfig->getMerchantAccount(), $_GET['transactionId']);
-
-file_put_contents(sLOG_PATH ."/error.log", "\n". "accept : ".var_export($obj_XML, true), FILE_APPEND  | LOCK_EX );
-	
-
+$statuscode = (string) $obj_XML->status[0]['code'];
+switch ($statuscode) 
+{
+	case '2000':
+		$message = 'Authorized';
+		break;
+	case '2001':
+		$message = 'Capture';
+		break;
+	case '2010':
+		$message = 'Declined';
+		break;
+	default:
+		$message = 'Unknown Authorization Error';
+		break;
+}
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 	
