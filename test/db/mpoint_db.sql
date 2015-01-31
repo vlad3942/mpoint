1af0ebbbcd1cca9d6e6a451e265e93133b6c475b
johan <johan@cellpointmobile.com>
2015-01-31 19:49:26 +0000

    Removed more DB control statements
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@788 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -3010,8 +3010,6 @@ GRANT ALL ON SEQUENCE url_tbl_id_seq TO postgres;
 -- Dumped by pg_dump version 9.3.5
 -- Started on 2014-11-17 15:26:16 CET
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
