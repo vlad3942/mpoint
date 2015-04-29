f238d421ba5a48594bff37643fd0d42a5408fa96
johan <johan@cellpointmobile.com>
2015-04-29 14:35:38 +0000

    CMP-145 : Re-added required test-includes
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@957 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- test/inc/testinclude.php
+++ test/inc/testinclude.php
@@ -3,13 +3,13 @@
 if (defined('sPROJECT_BASE_DIR') === false) { define('sPROJECT_BASE_DIR', __DIR__. '/../..'); }
 chdir(sPROJECT_BASE_DIR);
 
-/*include_once 'api/classes/general.php';
+include_once 'api/classes/general.php';
 include_once 'api/classes/home.php';
 include_once 'api/classes/enduser_account.php';
 include_once 'api/classes/validate.php';
 include_once 'api/classes/callback.php';
 include_once 'api/classes/basicconfig.php';
-include_once 'api/classes/constants.php';*/
+include_once 'api/classes/constants.php';
 
 define("TESTDB_TOKEN", "test_". time() );
 
--- test/model/MobilePayCancelTest.php
+++ test/model/MobilePayCancelTest.php
@@ -6,7 +6,6 @@
  */
 require_once __DIR__ . '/../../webroot/inc/include.php';
 require_once __DIR__ . '/../inc/testinclude.php';
-require_once(sCLASS_PATH ."/callback.php");
 require_once sINTERFACE_PATH . '/cpm_psp.php';
 require_once sCLASS_PATH . '/mobilepay.php';
 
