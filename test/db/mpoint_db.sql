cb5523a986932b7399fe3744faa599ad86fe8b2e
johan <johan@cellpointmobile.com>
2015-05-28 20:01:43 +0000

    CMP-239 : Updated unittest datamodel to include info_tbls and updated mobilepay test-response xsds
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@1015 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -6710,3 +6710,40 @@ ALTER TABLE client.client_tbl
 ADD COLUMN transaction_ttl integer DEFAULT 0;
 COMMENT ON COLUMN client.client_tbl.transaction_ttl
 IS 'Transaction Time To Live in seconds';
+
+-- Table: Client.InfoType_Tbl
+-- Definition table for all information types: About Text, Contact Info, Terms & Conditions, Help Text, Reminder Message, OTP Message etc.
+CREATE TABLE Client.InfoType_Tbl
+(
+  id			SERIAL,
+
+  name		VARCHAR(100),
+  note		TEXT,
+
+  CONSTRAINT InfoType_PK PRIMARY KEY (id),
+  LIKE Template.General_Tbl INCLUDING DEFAULTS
+) WITHOUT OIDS;
+
+CREATE UNIQUE INDEX InfoType_UQ ON Client.InfoType_Tbl (Lower(name) );
+
+-- Table: Client.Info_Tbl
+-- Data table for all Client Information, texts may be in XHTML format
+CREATE TABLE Client.Info_Tbl
+(
+  id			SERIAL,
+  infotypeid	INT4 NOT NULL,	-- ID of the Information Type
+  clientid	INT4 NOT NULL,	-- ID of the Client who owns the Info
+  pspid		INT4,
+  language	CHAR(2) DEFAULT 'gb',
+
+  text		TEXT,			-- Information text
+
+  CONSTRAINT Info_PK PRIMARY KEY (id),
+  CONSTRAINT Info2InfoType_FK FOREIGN KEY (infotypeid) REFERENCES Client.InfoType_Tbl ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT Info2Client_FK FOREIGN KEY (clientid) REFERENCES Client.Client_Tbl ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT Info2PSP_FK FOREIGN KEY (pspid) REFERENCES System.PSP_Tbl ON UPDATE CASCADE ON DELETE CASCADE,
+  LIKE Template.General_Tbl INCLUDING DEFAULTS
+) WITHOUT OIDS;
+
+CREATE UNIQUE INDEX Info_PSP_UQ ON Client.Info_Tbl (infotypeid, clientid, language, pspid) WHERE pspid IS NOT NULL;
+CREATE UNIQUE INDEX Info_UQ ON Client.Info_Tbl (infotypeid, clientid, language) WHERE pspid IS NULL;
