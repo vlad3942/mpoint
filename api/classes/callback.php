67289d589618f47b1c6baad7db47ace28787677e
johan <johan@cellpointmobile.com>
2015-08-21 18:50:55 +0000

    CMP-378 : Corrected copy/paste mistake in Callback:producePSP as part of code review
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@1290 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -582,7 +582,7 @@ class Callback extends EndUserAccount
 			return new NetAxept($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["netaxept"]);
 		case (Constants::iMOBILEPAY_PSP):
 			return new MobilePay($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["mobilepay"]);
-		case (Constants::iMOBILEPAY_PSP):
+		case (Constants::iADYEN_PSP):
 				return new Adyen($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["adyen"]);
 		default:
 			throw new CallbackException("Unkown Payment Service Provider", 1001);
