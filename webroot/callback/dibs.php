0e00c86504a4a919d8985fedd849a7962c7f87a4
Jona <jonatan.buus@cellpointmobile.com>
2015-09-11 14:23:05 +0000

    CMP-467 Refactored the callback logic slightly so that a "Payment Authorized" callback will always be triggered to the client even if the Auto-Capture is Declined.
    git-svn-id: svn://svn.cellpointmobile.com/mpoint/server/trunk@1324 86c063f6-31ef-4d33-b9ba-d2d3e4924f03

--- webroot/callback/dibs.php
+++ webroot/callback/dibs.php
@@ -143,19 +143,18 @@ try
 	// Callback URL has been defined for Client and transaction hasn't been duplicated
 	if ($obj_TxnInfo->getCallbackURL() != "" && $iStateID != Constants::iPAYMENT_DUPLICATED_STATE)
 	{
+		$obj_mPoint->notifyClient($iStateID, $_POST);
 		// Transaction uses Auto Capture and Authorization was accepted
 		if ($obj_TxnInfo->useAutoCapture() === true && $iStateID == Constants::iPAYMENT_ACCEPTED_STATE)
 		{
 			// Capture automatically performed by DIBS or invocation of capture operation with DIBS succeeded
 			if (array_key_exists("capturenow", $_POST) === true || $obj_mPoint->capture() == 1000)
 			{
-				$obj_mPoint->notifyClient(Constants::iPAYMENT_ACCEPTED_STATE, $_POST);
 				$obj_mPoint->notifyClient(Constants::iPAYMENT_CAPTURED_STATE, $_POST);
 				if (array_key_exists("capturenow", $_POST) === true) { $obj_mPoint->newMessage($obj_TxnInfo->getID(), Constants::iPAYMENT_CAPTURED_STATE, ""); }
 			}
-			else { $obj_mPoint->notifyClient(Constants::iPAYMENT_DECLINED_STATE, $_REQUEST); }
+			else { $obj_mPoint->notifyClient(Constants::iPAYMENT_DECLINED_STATE, $_POST); }
 		}
-		else { $obj_mPoint->notifyClient($iStateID, $_POST); }
 	}
 
 	// Client has SMS Receipt enabled and payment has been authorized
