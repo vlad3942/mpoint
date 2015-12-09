c68dd3fb8a6f04cc4f8a7aa263155b2304f8dabe
Simon Boriis <simon@cellpointmobile.com>
2015-12-09 11:40:21 +0100

    CMP-586 	Add new column position to table Client.CardAccess_Tbl that will be used for sorting the cards returned to the client 	Added setup for client DSB with their sorting order

---
+++ db/master_pg_v1.90.sql
@@ -0,0 +1,4 @@
+/* ==================== Client SCHEMA START ==================== */
+ALTER TABLE Client.CardAccess_tbl ADD position integer default NULL;
+/* ==================== Client SCHEMA END ==================== */
+
--- db/setup_pg_v1.90.sql
+++ db/setup_pg_v1.90.sql
@@ -189,8 +189,8 @@ INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength
 INSERT INTO System.PSPCard_Tbl (pspid, cardid) VALUES (15, 24);
 INSERT INTO System.CardPricing_Tbl (pricepointid, cardid) SELECT C.id * -1 AS pricepointid, 24 FROM System.Country_Tbl C, System.Card_Tbl Card WHERE C.id = 100 GROUP BY pricepointid;
 INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (24, 0, 0);
-
-INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) VALUES (10005, 24, 15);
+INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid, position) VALUES (10005, 24, 15, 1);
+UPDATE Client.CardAccess_Tbl  SET position = 2 WHERE cardid = 17 and clientid = 10005;
 /* ========== CONFIGURE DSB INVOICE PAYMENT END ========= */
 
 /* ========== CONFIGURE DSB PSP AND VOUCHER PAYMENT ========= */
@@ -206,3 +206,4 @@ INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) SELECT A.id, 
 
 INSERT INTO Log.State_Tbl (id, name) VALUES (2007, 'Payment with voucher');
 /* ========== CONFIGURE DSB PSP AND VOUCHER PAYMENT ========= */
+
