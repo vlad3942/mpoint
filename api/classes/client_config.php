47a8f01f7af4127dba3b85685b86ab85e85da1b1
deblina <deblina@cellpointmobile.com>
2015-12-22 11:19:13 +0530

    CMC-2423 reverting changes

--- api/classes/client_config.php
+++ api/classes/client_config.php
@@ -28,17 +28,17 @@ class ClientConfig extends BasicConfig
 	 *
 	 * @var integer
 	 */
-	const iCUSTOMER_IMPORT_URL = 0;
-	const iAUTHENTICATION_URL = 1;
-	const iNOTIFICATION_URL = 2;
-	const iMESB_URL = 3;
+	const iCUSTOMER_IMPORT_URL = 1;
+	const iAUTHENTICATION_URL = 2;
+	const iNOTIFICATION_URL = 3;
+	const iMESB_URL = 4;
 
-	const iLOGO_URL = 4;
-	const iCSS_URL = 5;
-	const iCALLBACK_URL = 6;
-	const iACCEPT_URL = 7;
-	const iCANCEL_URL = 8;
-	const iICON_URL = 9;		
+	const iLOGO_URL = 5;
+	const iCSS_URL = 6;
+	const iCALLBACK_URL = 7;
+	const iACCEPT_URL = 8;
+	const iCANCEL_URL = 9;
+	const iICON_URL = 10;		
 	/**
 	 * ID of the Flow the Client's customers have to go through in order to complete the Payment Transaction
 	 *
