61ddf21333cabd5850dbeb4ffe81a3c117edd3f2
Abhishek Sawant <abhishek.sawant@cellpointmobile.com>
2015-12-28 12:56:02 +0530

    CPMRM-493:Wirecard mPoint Database changes

--- db/setup_oracle_v1.90.sql
+++ db/setup_oracle_v1.90.sql
@@ -286,3 +286,17 @@ INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (10000
 -- Route VISA Card to Data Cash
 UPDATE Client.CardAccess_Tbl SET pspid = 17 WHERE clientid = 10001 AND cardid = 8;
 /* ========== CONFIGURE DEMO ACCOUNT FOR DATA CASH END ====== */
+
+/* ========== CONFIGURE Wire Card START ========== */
+INSERT INTO System.PSP_Tbl (id, name) VALUES (18, 'Wire Card');
+INSERT INTO System.PSPCurrency_Tbl (countryid, pspid, name) SELECT countryid, 18, name FROM System.PSPCurrency_Tbl WHERE pspid = 4;
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) SELECT cardid, 18 FROM System.PSPCard_Tbl WHERE pspid = 4;
+/* ========== CONFIGURE Wire Card END ========== */
+
+/* ========== CONFIGURE DEMO ACCOUNT FOR Wire Card START ========== */
+-- Wire Card
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, password) VALUES (10001, 18, '1b3be510-a992-48aa-8af9-6ba4c368a0ac', '70000-APIDEMO-CARD', 'ohysS0-dvfMx');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100001, 18, '-1');
+-- Route VISA Card to Wire Card
+UPDATE Client.CardAccess_Tbl SET pspid = 18 WHERE clientid = 10001 AND cardid = 8;
+/* ========== CONFIGURE DEMO ACCOUNT FOR Wire Card END ====== */
--- db/setup_pg_v1.90.sql
+++ db/setup_pg_v1.90.sql
@@ -192,11 +192,12 @@ INSERT INTO System.PSPCard_Tbl (cardid, pspid) SELECT cardid, 17 FROM System.PSP
 
 /* ========== CONFIGURE DEMO ACCOUNT FOR DATA CASH START ========== */
 -- Data Cash
-INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, password) VALUES (10001, 17, 'TESTCellpoint_02', 'merchant.TESTCellpoint_02', '684d51b8db5870b514404055d988512a');
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10001, 17, 'TESTCellpoint_02', 'merchant.TESTCellpoint_02', '684d51b8db5870b514404055d988512a');
 INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100001, 17, '-1');
 -- Route VISA Card to Data Cash
 UPDATE Client.CardAccess_Tbl SET pspid = 17 WHERE clientid = 10001 AND cardid = 8;
 /* ========== CONFIGURE DEMO ACCOUNT FOR DATA CASH END ====== */
+
 /* ========== ALTER TABLE FOR MERCHANT ACCOUNT TO HAVE PASSWORD OF 4000 CHARS START ====== */
 ALTER TABLE Client.MerchantAccount_Tbl ALTER COLUMN passwd TYPE character varying(4000);
 ALTER TABLE Client.MerchantAccount_Tbl ALTER COLUMN name TYPE character varying(100);
@@ -284,3 +285,17 @@ INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (10000
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) VALUES (10001, 25, 4);
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) VALUES (10001, 25, 9);
 /* ========== CONFIGURE DEMO ACCOUNT FOR AMEX EXPRESS CHECKOUT END ====== */
+
+/* ========== CONFIGURE Wire Card START ========== */
+INSERT INTO System.PSP_Tbl (id, name) VALUES (18, 'Wire Card');
+INSERT INTO System.PSPCurrency_Tbl (countryid, pspid, name) SELECT countryid, 18, name FROM System.PSPCurrency_Tbl WHERE pspid = 4;
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) SELECT cardid, 18 FROM System.PSPCard_Tbl WHERE pspid = 4;
+/* ========== CONFIGURE Wire Card END ========== */
+
+/* ========== CONFIGURE DEMO ACCOUNT FOR Wire Card START ========== */
+-- Wire Card
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10001, 18, '1b3be510-a992-48aa-8af9-6ba4c368a0ac', '70000-APIDEMO-CARD', 'ohysS0-dvfMx');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100001, 18, '-1');
+-- Route VISA Card to Wire Card
+UPDATE Client.CardAccess_Tbl SET pspid = 18 WHERE clientid = 10001 AND cardid = 8;
+/* ========== CONFIGURE DEMO ACCOUNT FOR Wire Card END ====== */
