4ed1001e48200d13b8fcd279882095ac8168c19d
Abhishek Sawant <abhishek.sawant@cellpointmobile.com>
2016-01-04 17:21:26 +0530

    CMP-605:Implement Wire Card Callback controller

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -147,7 +147,7 @@ class Callback extends EndUserAccount
 		$sql = "UPDATE Log".sSCHEMA_POSTFIX.".Transaction_Tbl
 				SET pspid = ". intval($pspid) .", cardid = ". intval($cid).", fee =".intval($fee) . $sql ."
 				WHERE id = ". $this->_obj_TxnInfo->getID() ." AND (cardid IS NULL OR cardid = 0)";
-		if (intval($txnid) != -1) { $sql .= " AND (extid IS NULL OR extid = '' OR extid = '". $this->getDBConn()->escStr($txnid) ."')"; }
+		if (intval($txnid) == -1) { $sql .= " AND (extid IS NULL OR extid = ''"; }
 //		echo $sql ."\n";
 		$res = $this->getDBConn()->query($sql);
 
