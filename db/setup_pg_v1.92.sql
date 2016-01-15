821416c464b6134cd64eb5ddb63aa8f89fe4af02
Nitin Gaikwad <nitin@cellpointmobile.com>
2016-01-15 13:51:08 +0100

    CMP-621 Add new SQL query in mPoint Databse

---
+++ db/setup_pg_v1.92.sql
@@ -0,0 +1,5 @@
+/* ========== CONFIGURE State table for support Card block by PSP ========== */
+INSERT INTO Log.State_Tbl (id, name, module) VALUES (2012, 'Card blocked by PSP', 'Payment');
+/* ========== END CONFIGURE State table for support Card block by PSP ========= */
+
+
