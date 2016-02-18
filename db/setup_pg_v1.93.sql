2a2a61d59a30a9f30126926c29a409f480738fc5
Jona <jonatan.buus@cellpointmobile.com>
2016-02-18 13:39:25 +0100

    CMP-674 Added new client configuration: "Number of Masked Card Digits". This has added a new column: num_masked_digits to database table: Client.Client_Tbl. Additionally the configuration for the following clients have been changed to only display the last 2 digits in the masked card number: - DSB App PRODUCTION (10005) - DSB App Test (10014) - Mobile Travel Card (10019)

---
+++ db/setup_pg_v1.93.sql
@@ -0,0 +1,3 @@
+UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10005;	-- DSB App PRODUCTION
+UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10014;	-- DSB App Test
+UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10019;	-- Mobile Travel Card
