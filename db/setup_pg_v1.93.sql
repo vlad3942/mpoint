a314fd4b40e9e5b7de68d2777d721563b6c56f03
Johan Thomsen <johan@cellpointmobile.com>
2016-02-23 16:11:33 +0100

    CMP-677 : Raised amount limit for DSB client to 5000000 minor units

--- db/setup_pg_v1.93.sql
+++ db/setup_pg_v1.93.sql
@@ -1,3 +1,7 @@
 UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10005;	-- DSB App PRODUCTION
 UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10014;	-- DSB App Test
 UPDATE Client.Client_Tbl SET num_masked_digits = 2 WHERE id = 10019;	-- Mobile Travel Card
+
+-- Setting maxamount for DSB clients to DKK 50.000,- since new and more expensive products have been introduced
+UPDATE Client.Client_Tbl SET maxamount = 5000000 WHERE id = 10005; -- DSB App PRODUCTION
+UPDATE Client.Client_Tbl SET maxamount = 5000000 WHERE id = 10014; -- DSB App Test
