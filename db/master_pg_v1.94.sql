3c87f91938f8d3f30201d1101368f69af45424d2
Simon Boriis <simon@cellpointmobile.com>
2016-04-17 17:36:22 +0200

    CMP-719 	Altered point data model to support decline url for a client that can be used for storing the url that the PSP will return to

---
+++ db/master_pg_v1.94.sql
@@ -0,0 +1,4 @@
+/* ==================== Client SCHEMA START ==================== */
+ALTER TABLE Client.Client_Tbl ADD declineurl character varying(255);
+/* ==================== Client SCHEMA END ==================== */
+
