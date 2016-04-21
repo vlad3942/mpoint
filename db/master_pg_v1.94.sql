873ed6e78c727496efc703a7baccd4eb69393e37
Johan Thomsen <johan@cellpointmobile.com>
2016-04-21 12:03:47 +0200

    DMTC-2083 : Updated version specifier for 3Dsecure branch and moved DB changes to v1.95

--- db/master_pg_v1.94.sql
+++
@@ -1,4 +0,0 @@
-/* ==================== Client SCHEMA START ==================== */
-ALTER TABLE Client.Client_Tbl ADD declineurl character varying(255);
-/* ==================== Client SCHEMA END ==================== */
-
