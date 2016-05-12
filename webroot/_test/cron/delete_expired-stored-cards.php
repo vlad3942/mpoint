16f5241bc077453599a018b153ecd0e90702b034
Rohit Malhotra <rohit.malhotra@cellpointmobile.com>
2016-05-12 13:05:17 +0530

    PCI-15 - Cronjob will be scheduled that deletes PAN & Hash for expired cards - Implemented changesas suggested by Bhala to re-use the delStoredCard within the api/classes/my_account.php class

--- webroot/_test/cron/delete_expired-stored-cards.php
+++ webroot/_test/cron/delete_expired-stored-cards.php
@@ -3,7 +3,10 @@
 // Require Global Include File
 require_once("inc/include.php");
 
-$sql = "SELECT id, expiry
+$obj_CountryConfig = CountryConfig::produceConfig($_OBJ_DB, 100);
+$obj_mPoint = new MyAccount($_OBJ_DB, $_OBJ_TXT, $obj_CountryConfig);
+
+$sql = "SELECT id, accountid, expiry
 		FROM EndUser".sSCHEMA_POSTFIX.".Card_Tbl
 		WHERE enabled = '1'";
 //echo $sql ."\n";
@@ -14,13 +17,15 @@ while ($RS = $_OBJ_DB->fetchName($res) )
 	if(empty(trim($RS['EXPIRY'])) === false)
 	{
 		$aExpiry = explode('/', $RS['EXPIRY']);
+		$iAccountID = intval($RS['ACCOUNTID']);
+		$iCardID = intval($RS['ID']);
 		if(empty(trim($aExpiry[0])) === false && empty(trim($aExpiry[1])) === false && cardNotExpired(intval($aExpiry[0]), intval($aExpiry[1])) === false )
 		{		
-			$sql2 = "DELETE FROM EndUser".sSCHEMA_POSTFIX.".Card_Tbl						
-						WHERE id = ". $RS['ID'];
-			//echo $sql2 ."\n";
-			$_OBJ_DB->query($sql2);		
-			$iCount++;
+			$iDelStatus = $obj_mPoint->delStoredCard($iAccountID, $iCardID);					
+			if(intval($iDelStatus) == 10)
+			{
+				$iCount++;
+			}			
 		}
 	}
 }
