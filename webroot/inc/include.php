c811b63cf2ede7b6ee0e47d4d889c0fa479ca489
Jona <jonatan.buus@cellpointmobile.com>
2016-05-25 10:28:49 +0200

    Revert "CMP-715"
    This reverts commit 9fb75e936a23178d358a5109d347d13d85bd6367.

--- webroot/inc/include.php
+++ webroot/inc/include.php
@@ -22,11 +22,11 @@ else
 /* ========== Define System path End ========== */
 
 // Define path to the General API classes
-define("sAPI_CLASS_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../../php5api/classes/");
+define("sAPI_CLASS_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../php5api/classes/");
 // Define path to the General API interfaces
-define("sAPI_INTERFACE_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../../php5api/interfaces/");
+define("sAPI_INTERFACE_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../php5api/interfaces/");
 // Define path to the General API functions
-define("sAPI_FUNCTION_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../../php5api/functions/");
+define("sAPI_FUNCTION_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/") ) ."/../php5api/functions/");
 
 // Define path to the System classes
 define("sCLASS_PATH", sSYSTEM_PATH ."/api/classes/");
