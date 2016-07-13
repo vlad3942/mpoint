5493a8a4c2596b461f157d517e94700bd99d6cb6
Abhishek Sawant <abhishek.sawant@cellpointmobile.com>
2016-07-13 15:22:11 +0530

    CMP-848 : [Wallets] System level configuration for wallets with PSP's are missing in system.pspcard_tbl

--- db/setup_pg_v1.98.sql
+++ db/setup_pg_v1.98.sql
@@ -1,3 +1,26 @@
 UPDATE Client.Client_Tbl SET salt = 'Fh17_8sFgd' WHERE id = 10005;
 UPDATE Client.Client_Tbl SET salt = 'Fh17_8sFgd' WHERE id = 10014;
 UPDATE Client.Client_Tbl SET salt = '8sFgd_Fh17' WHERE id = 10019;
+
+/**
+ * Below queries are added to address CMP-848
+ */
+DELETE FROM system.pspcard_tbl WHERE cardid = 27 and pspid IN (SELECT id FROM system.pspcard_tbl WHERE id > 0);
+INSERT INTO  system.pspcard_tbl(cardid, pspid)
+SELECT 27, id
+FROM system.psp_tbl WHERE id > 0;
+
+DELETE FROM system.pspcard_tbl WHERE cardid = 25 and pspid IN (SELECT id FROM system.pspcard_tbl WHERE id > 0);
+INSERT INTO  system.pspcard_tbl(cardid, pspid)
+SELECT 25, id
+FROM system.psp_tbl WHERE id > 0;
+
+DELETE FROM system.pspcard_tbl WHERE cardid = 23 and pspid IN (SELECT id FROM system.pspcard_tbl WHERE id > 0);
+INSERT INTO  system.pspcard_tbl(cardid, pspid)
+SELECT 23, id
+FROM system.psp_tbl WHERE id > 0;
+
+DELETE FROM system.pspcard_tbl WHERE cardid = 16 and pspid IN (SELECT id FROM system.pspcard_tbl WHERE id > 0);
+INSERT INTO  system.pspcard_tbl(cardid, pspid)
+SELECT 16, id
+FROM system.psp_tbl WHERE id > 0;
