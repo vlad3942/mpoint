abdae08f687917fa31b39f92c6312ba712a71a2b
Tejashree Waghmare <tejashree@cellpointmobile.com>
2016-08-30 11:24:26 +0530

    CMP-999 added query to update symbol for country Denmark in System.Country_tbl

--- db/setup_pg_v1.99.sql
+++ db/setup_pg_v1.99.sql
@@ -6,3 +6,7 @@ UPDATE Client.Client_Tbl SET salt = '8sFgd_Fh17' WHERE id = 10019;
  * CMP-917
  */
 INSERT INTO System.Type_Tbl (id, name) VALUES (10091, 'New Card Purchase');
+/**
+ * CMP-999
+ */
+UPDATE system.country_tbl SET symbol='Kr.' WHERE id = 100;
