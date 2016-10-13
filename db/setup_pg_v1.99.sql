18e4a4c619f65ac2c9e81c06f48367e03dc0bdfb
Abhishek Sawant <abhishek.sawant@cellpointmobile.com>
2016-10-13 13:33:55 +0530

    Revert "Merged changes from development."
    This reverts commit c8aac3b16d23fbd9e8418fa27a308de45cc61a92.

--- db/setup_pg_v1.99.sql
+++ db/setup_pg_v1.99.sql
@@ -11,28 +11,6 @@ INSERT INTO System.Type_Tbl (id, name) VALUES (10091, 'New Card Purchase');
  */
 UPDATE system.country_tbl SET symbol='Kr.' WHERE id = 100;
 
-/**
- *CMC-3289
- */
-Delete from system.cardpricing_tbl c where cardid=16
-Insert into system.cardpricing_tbl  (cardid , pricepointid) VALUES (16,-200);
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =500;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =202;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =400;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =403;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =404;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =609;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =405;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =614;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =429;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =638;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =201;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =513;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =642;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =355;
-Insert into system.cardpricing_tbl  (cardid , pricepointid) SELECT 16 , id from system.pricepoint_tbl  where countryid =647;
-
-
 /***
  * CMP-1030
  */
