f4842ed7f4c3dee71700a0e4a61d42608635b277
Qaeed Ramiwala <qaeed.ramiwala@cellpointmobile.com>
2016-12-12 14:26:11 +0530

    CMP-1240 [PayFort] 3D secure sandbox default flow for HPP

--- webroot/mApp/api/authorize.php
+++ webroot/mApp/api/authorize.php
@@ -816,7 +816,8 @@ try
 																	$xml .= '<status code="100">Payment Authorized using stored card</status>';
 																} else if($code == "2000") { $xml .= '<status code="2000">Payment authorized</status>'; }
 																else if($code == "2009") { $xml .= '<status code="2009">Payment authorized and card stored.</status>'; }
-																
+																else if(strpos($code, '2005') !== false) { $xml = $code; }
+																	
 																// Error: Authorization declined
 																else
 																{
