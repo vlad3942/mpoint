cde6b2c7cc817b614ab2ed02b5699c84abfad136
Qaeed Ramiwala <qaeed.ramiwala@cellpointmobile.com>
2016-12-15 15:49:36 +0530

    CMP-1208 [SADAD] Add sadad as payment method database script

--- db/setup_pg_v2.00.sql
+++ db/setup_pg_v2.00.sql
@@ -7,3 +7,12 @@ UPDATE Client.Client_Tbl SET salt = '8sFgd_Fh17' WHERE id = 10019;
  * CMP-1146 Support "Payment Settled" state for mConsole Search transaction API
  */ 
 INSERT INTO Log.State_Tbl (id, name) VALUES (2020, 'Payment Settled');
+
+
+
+/* ========== Global Configuration for SADAD - Payment Method : START========== */
+INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength) VALUES (31, 'SADAD', 23, -1, -1, -1);
+INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (31, 0, 0);
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 31, id FROM System.PricePoint_Tbl WHERE amount = -1 AND countryid = 608;
+
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (31, 23);
