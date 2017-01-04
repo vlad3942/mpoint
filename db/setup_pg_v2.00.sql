5a8345e1286878b7ebcbe9e5024d4f143eb9f2cc
deblina <deblina@cellpointmobile.com>
2017-01-04 11:41:51 +0530

    CMP-1276 Adding transaction states for Account Validation

--- db/setup_pg_v2.00.sql
+++ db/setup_pg_v2.00.sql
@@ -7,3 +7,12 @@ UPDATE Client.Client_Tbl SET salt = '8sFgd_Fh17' WHERE id = 10019;
  * CMP-1146 Support "Payment Settled" state for mConsole Search transaction API
  */ 
 INSERT INTO Log.State_Tbl (id, name) VALUES (2020, 'Payment Settled');
+
+
+
+/**
+ * CMP-1276 Support "Payment Settled" state for mConsole Search transaction API
+ */ 
+INSERT INTO Log.State_Tbl (id, name) VALUES (1998, 'Account Validated');
+INSERT INTO Log.State_Tbl (id, name) VALUES (19980, 'Account Validated and Cancelled');
+INSERT INTO Log.State_Tbl (id, name) VALUES (1997, 'Account Validation Failed');
