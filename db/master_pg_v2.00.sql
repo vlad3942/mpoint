5faf7ce8826c75deaf11a19fd6b46f89ac4a14d5
Manish <manish@cellpointmobile.com>
2017-01-10 15:27:11 +0530

    CMP-1257 Update Database schema and scripts to save flight and passenger information

--- db/master_pg_v2.00.sql
+++ db/master_pg_v2.00.sql
@@ -17,19 +17,23 @@ CREATE TYPE log.additional_data_ref AS ENUM
 
 CREATE TABLE log.flight_tbl
 (
+  id serial NOT NULL,
   service_class character varying(10) NOT NULL,
   departure_airport character varying(10) NOT NULL,
   arrival_airport character varying(10) NOT NULL,
   airline_code character varying(10) NOT NULL,
-  id serial NOT NULL,
   order_id integer NOT NULL,
   arrival_date timestamp without time zone NOT NULL,
   departure_date timestamp without time zone NOT NULL,
   created timestamp without time zone NOT NULL DEFAULT now(),
-  modified time without time zone NOT NULL DEFAULT now(),
+  modified timestamp without time zone NOT NULL DEFAULT now(),
+  additional_data_ref integer NOT NULL,
   CONSTRAINT flight_pk PRIMARY KEY (id),
   CONSTRAINT order_fk FOREIGN KEY (order_id)
       REFERENCES log.order_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT ref_additional_fk FOREIGN KEY (additional_data_ref)
+      REFERENCES log.additional_data_tbl (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
 )
 WITHOUT OIDS;
@@ -43,16 +47,20 @@ WITHOUT OIDS;
 
 CREATE TABLE log.passenger_tbl
 (
-  id serial NOT NULL,
+   id serial NOT NULL,
   first_name character varying(20) NOT NULL,
   last_name character varying(20) NOT NULL,
   type character varying(10) NOT NULL,
   order_id integer NOT NULL,
   created timestamp without time zone DEFAULT now(),
   modified timestamp without time zone DEFAULT now(),
+  additional_data_ref integer NOT NULL,
   CONSTRAINT passenger_pk PRIMARY KEY (id),
   CONSTRAINT order_fk FOREIGN KEY (order_id)
       REFERENCES log.order_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT ref_additional_fk FOREIGN KEY (additional_data_ref)
+      REFERENCES log.additional_data_tbl (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
 )
 WITHOUT OIDS;
@@ -67,20 +75,12 @@ WITHOUT OIDS;
 
 CREATE TABLE log.additional_data_tbl
 (
-  id serial NOT NULL,
-  reference_id integer,
+ id serial NOT NULL,
   name character varying(20),
   value character varying(20),
   type log.additional_data_ref,
   created timestamp without time zone DEFAULT now(),
   modified timestamp without time zone DEFAULT now(),
-  CONSTRAINT additional_data_pk PRIMARY KEY (id),
-  CONSTRAINT ref_flight_fk FOREIGN KEY (reference_id)
-      REFERENCES log.flight_tbl (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT ref_pax_fk FOREIGN KEY (reference_id)
-      REFERENCES log.passenger_tbl (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT additional_data_uk UNIQUE (reference_id, name, type)
+  CONSTRAINT additional_data_pk PRIMARY KEY (id)
 )
 WITHOUT OIDS;
