88079b4596a411f833081f7d62605ba63a95af90
deblina <deblina@cellpointmobile.com>
2017-01-10 12:46:29 +0530

    CMESB-786 Updating the code as 303 for non-optimized 3ds flow

--- webroot/mApp/api/authorize.php
+++ webroot/mApp/api/authorize.php
@@ -777,7 +777,7 @@ try
 																}
 																else if($code == "2000") { $xml .= '<status code = "2000">Payment authorized</status>'; }
 																else if($code == "2009") { $xml .= '<status code="2009">Payment authorized and card stored.</status>'; }
-																else if(strpos($code, '2005') !== false) { $xml = $code; }
+																else if(strpos($code, '2005') !== false) { header("HTTP/1.1 303"); $xml = $code; }
 																// Error: Authorization declined
 																else
 																{
