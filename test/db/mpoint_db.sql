f83d503005fc2fe2801935fbfc24c6f8d5ea868b
Abhishek Sawant <abhishek.sawant@cellpointmobile.com>
2017-01-10 17:21:50 +0530

    CMP-1255: Added new tables in mpoint_db.sql of tests.

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -6879,3 +6879,138 @@ ALTER TABLE Client.Client_Tbl ADD COLUMN salt VARCHAR(20);
 INSERT INTO System.URLType_Tbl (id, name) VALUES (12, 'Parse 3D Secure Challenge URL');
 INSERT INTO Log.State_Tbl (id, name) VALUES (1100, '3D Secure Activated');
 /* ========== Mobile Optimized 3D Secure END ========== */
+
+/* ==================== LOG.ORDER_TBL SCHEMA START ==================== */
+CREATE TABLE log.order_tbl
+(
+  id serial NOT NULL,
+  txnid integer NOT NULL,
+  countryid integer NOT NULL,
+  amount integer,
+  productsku character varying(40),
+  productname character varying(40),
+  productdescription text,
+  productimageurl character varying(255),
+  points integer,
+  reward integer,
+  quantity integer,
+  created timestamp without time zone DEFAULT now(),
+  modified timestamp without time zone DEFAULT now(),
+  enabled boolean DEFAULT true,
+  CONSTRAINT order_pk PRIMARY KEY (id),
+  CONSTRAINT order2country_fk FOREIGN KEY (countryid)
+      REFERENCES system.country_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT order2txn_fk FOREIGN KEY (txnid)
+      REFERENCES log.transaction_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE log.order_tbl
+  OWNER TO mpoint;
+
+CREATE INDEX order_created_idx
+  ON log.order_tbl
+  USING btree
+  (created);
+
+CREATE INDEX order_transaction_idx
+  ON log.order_tbl
+  USING btree
+  (id, txnid);
+  
+/* ==================== LOG.ORDER_TBL SCHEMA START ==================== */
+
+
+
+
+-------------  Airline Data ----------------
+
+
+
+-- Type: log.additional_data_ref
+
+-- DROP TYPE log.additional_data_ref;
+
+CREATE TYPE log.additional_data_ref AS ENUM
+   ('Flight',
+    'Passenger');
+    
+-- Table: log.additional_data_tbl
+
+-- DROP TABLE log.additional_data_tbl;
+
+CREATE TABLE log.additional_data_tbl
+(
+ id serial NOT NULL,
+  name character varying(20),
+  value character varying(20),
+  type log.additional_data_ref,
+  created timestamp without time zone DEFAULT now(),
+  modified timestamp without time zone DEFAULT now(),
+  CONSTRAINT additional_data_pk PRIMARY KEY (id)
+)
+WITHOUT OIDS;
+
+
+-- Table: log.flight_tbl
+
+-- DROP TABLE log.flight_tbl;
+
+CREATE TABLE log.flight_tbl
+(
+  id serial NOT NULL,
+  service_class character varying(10) NOT NULL,
+  departure_airport character varying(10) NOT NULL,
+  arrival_airport character varying(10) NOT NULL,
+  airline_code character varying(10) NOT NULL,
+  order_id integer NOT NULL,
+  arrival_date timestamp without time zone NOT NULL,
+  departure_date timestamp without time zone NOT NULL,
+  created timestamp without time zone NOT NULL DEFAULT now(),
+  modified timestamp without time zone NOT NULL DEFAULT now(),
+  additional_data_ref integer NOT NULL,
+  CONSTRAINT flight_pk PRIMARY KEY (id),
+  CONSTRAINT order_fk FOREIGN KEY (order_id)
+      REFERENCES log.order_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT ref_additional_fk FOREIGN KEY (additional_data_ref)
+      REFERENCES log.additional_data_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITHOUT OIDS;
+
+  
+  
+  
+-- Table: log.passenger_tbl
+
+-- DROP TABLE log.passenger_tbl;
+
+CREATE TABLE log.passenger_tbl
+(
+   id serial NOT NULL,
+  first_name character varying(20) NOT NULL,
+  last_name character varying(20) NOT NULL,
+  type character varying(10) NOT NULL,
+  order_id integer NOT NULL,
+  created timestamp without time zone DEFAULT now(),
+  modified timestamp without time zone DEFAULT now(),
+  additional_data_ref integer NOT NULL,
+  CONSTRAINT passenger_pk PRIMARY KEY (id),
+  CONSTRAINT order_fk FOREIGN KEY (order_id)
+      REFERENCES log.order_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT ref_additional_fk FOREIGN KEY (additional_data_ref)
+      REFERENCES log.additional_data_tbl (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITHOUT OIDS;
+
+  
+  
+  
+  
+
