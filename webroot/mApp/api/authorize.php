233e793a3b0744b2e05ed6cbde87e5bc2bc47758
Rohit Malhotra <rohit@cellpointmobile.com>
2017-01-30 16:26:28 +0530

    Reverted changes to Manish's revision dfd2c0d20b203b079ecd4f5ed7e8cd33f82e4190

--- webroot/mApp/api/authorize.php
+++ webroot/mApp/api/authorize.php
@@ -238,6 +238,7 @@ try
 											}
 											else { $code = $obj_mPoint->auth($obj_TxnInfo->getAccountID(), (string) $obj_DOM->{'authorize-payment'}[$i]->password); }
 											// Authentication succeeded
+											
 											if ($code == 10 || ($code == 11 && $obj_ClientConfig->smsReceiptEnabled() === false) )
 											{
 												
@@ -274,7 +275,7 @@ try
 															try
 															{
 																$obj_PSP = new CellpointMobile($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo);
-																// Initialise Callback to Client
+																//Initialise Callback to Client
 																$obj_PSP->initCallback(HTTPConnInfo::produceConnInfo($aCPM_CONN_INFO), Constants::iWALLET, Constants::iPAYMENT_ACCEPTED_STATE);
 															}
 															catch (HTTPException $ignore) { /* Ignore */ }
@@ -285,9 +286,7 @@ try
 														else
 														{
 															$obj_mPoint->delMessage($obj_TxnInfo->getID(), Constants::iPAYMENT_WITH_ACCOUNT_STATE);
-	
 															header("HTTP/1.1 500 Internal Server Error");
-	
 															$xml .= '<status code="91">Unable to debit account</status>';
 														}
 													}
@@ -304,6 +303,7 @@ try
 													// 3rd Party Wallet
 													if(count($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->token) == 1)
 													{
+														
 														switch (intval($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]["type-id"]) )
 														{
 														case (Constants::iAPPLE_PAY):
@@ -347,7 +347,6 @@ try
 														if(isset($obj_Wallet) == true && is_object($obj_Wallet) == true)
 														{
 															$obj_XML = simpledom_load_string($obj_Wallet->getPaymentData($obj_PSPConfig, $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]) );
-															
 															if (count($obj_XML->{'payment-data'}) == 1)
 															{
 																$obj_Elem = $obj_XML->{'payment-data'}->card;
@@ -398,7 +397,7 @@ try
 																// Merge CVC / CVV code from request
 																if (count($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc) == 1)
 																{
-																	$obj_Elem->cvc = $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc;
+																	$obj_Elem->cvc = (string) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc;
 																}
 																															
 																$obj_PSPConfig = $obj_Wallet->getPSPConfigForRoute(intval($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]["type-id"]),
@@ -460,13 +459,13 @@ try
 																												
 														if (count($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc) == 1) 
 														{ 
-															$obj_Elem->cvc = (integer) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc; 
+															$obj_Elem->cvc = (string) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc; 
 														}
 													}
 													else
 													{
 														$obj_Elem = $obj_XML->xpath("/stored-cards/card[@id = ". $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]["id"] ."]");
-														if (count($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc) == 1) { $obj_Elem->cvc = (integer) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc; }
+														if (count($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc) == 1) { $obj_Elem->cvc = (string) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc; }
 														if (count($obj_Elem->mask) == 1 && intval($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]["type-id"]) != 28 )  { $code = $obj_Validator->valIssuerIdentificationNumber($_OBJ_DB, $obj_ClientConfig->getID(), substr(str_replace(" ", "", $obj_Elem->mask), 0, 6) ); }
 														else { $code = 10; }
 													}
@@ -500,22 +499,18 @@ try
 															case (Constants::iWORLDPAY_PSP):
 																// Authorise payment with PSP based on Ticket
 																$obj_PSP = new WorldPay($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO["worldpay"]);
-																
 																if ($obj_TxnInfo->getMode() > 0) { $aHTTP_CONN_INFO["worldpay"]["host"] = str_replace("secure.", "secure-test.", $aHTTP_CONN_INFO["worldpay"]["host"]); }
-																
 																// WorldPay doesn't enable support for 3D Secure on Mechant Codes intended for Recurring payments
 																if (empty($obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]['id']) == false)
 																{
 																	$bStoredCard = true;
 																}
 																else { $bStoredCard = false; }
-	
 																$aLogin = $obj_PSP->getMerchantLogin($obj_TxnInfo->getClientConfig()->getID(), Constants::iWORLDPAY_PSP, $bStoredCard);
 																$aHTTP_CONN_INFO["worldpay"]["username"] = $aLogin["username"];
 																$aHTTP_CONN_INFO["worldpay"]["password"] = $aLogin["password"];
 	
 																$obj_ConnInfo = HTTPConnInfo::produceConnInfo($aHTTP_CONN_INFO["worldpay"]);
-																
 																$obj_XML = $obj_PSP->authTicket($obj_ConnInfo, $obj_Elem);
 																// Authorization succeeded
 																if (is_null($obj_XML) === false && ($obj_XML instanceof SimpleXMLElement) === true && intval($obj_XML["code"]) == Constants::iPAYMENT_ACCEPTED_STATE)
@@ -544,12 +539,15 @@ try
 																break;
 															case (Constants::iDIBS_PSP):	// DIBS
 																// Authorise payment with PSP based on Ticket
+																
 																$obj_PSP = new DIBS($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO['dibs']);
+	
 																$iTxnID = $obj_PSP->authTicket($obj_Elem);
+														
 																// Authorization succeeded
 																if ($iTxnID > 0)
 																{
-																	// Only generate internal callback for payments made with a Stored Card
+																    // Only generate internal callback for payments made with a Stored Card
 																	if (count($obj_Elem->ticket) == 1)
 																	{
 																		try
@@ -562,9 +560,7 @@ try
 																		
 																		//$xml = '<status code="100">Payment Authorized using Stored Card</status>';
 																	} //else { $xml = '<status code="2000">Payment authorized using new card</status>'; }
-																
-																	$xml = '<status code="100">Payment Authorized using Stored Card</status>';
-																	
+																	$xml = '<status code="100">Payment Authorized using Stored Card</status>';	
 																}
 																// Error: Authorization declined
 																else
@@ -576,7 +572,8 @@ try
 																	$xml .= '<status code="92">Authorization failed, DIBS returned error code'. $iTxnID .'</status>';
 																}
 																break;
-															case (Constants::iWANNAFIND_PSP):	// WannaFind
+																case (Constants::iWANNAFIND_PSP):	
+																// WannaFind
 																// Authorise payment with PSP based on Ticket
 																$obj_PSP = new WannaFind($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO["wannafind"]);
 																$iTxnID = $obj_PSP->authTicket( (integer) $obj_Elem->ticket);
@@ -686,8 +683,9 @@ try
 																$obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $obj_TxnInfo->getClientConfig()->getID(), $obj_TxnInfo->getClientConfig()->getAccountConfig()->getID(), Constants::iDATA_CASH_PSP);
 															
 																$obj_PSP = new DataCash($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO["data-cash"]);
-																	
+															
 																$code = $obj_PSP->authorize($obj_PSPConfig , $obj_Elem);
+																
 																// Authorization succeeded
 																if ($code == "100")
 																{
@@ -827,7 +825,8 @@ try
 																	$xml .= '<status code="100">Payment Authorized using stored card</status>';
 																} else if($code == "2000") { $xml .= '<status code="2000">Payment authorized</status>'; }
 																else if($code == "2009") { $xml .= '<status code="2009">Payment authorized and card stored.</status>'; }
-																
+																else if(strpos($code, '2005') !== false) { $xml = $code; }
+																	
 																// Error: Authorization declined
 																else
 																{
