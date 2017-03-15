800f04f164eff33e7b4c816d736401c6b3e8ab08
Qaeed Ramiwala <qaeed.ramiwala@cellpointmobile.com>
2017-03-15 12:09:58 +0530

    CMP-1425 Database scripts, support for Qiwi in additional payment methods, general changes

--- db/setup_pg_v2.00.sql
+++ db/setup_pg_v2.00.sql
@@ -185,3 +185,28 @@ INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (10000
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid, enabled, countryid) VALUES (10001, 32, 30, true, 200);
 /* ========== Global Configuration for AliPay = ENDS ========== */
 
+/* ========== Global Configuration for AliPay - Payment Method : START========== */
+INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength) VALUES (32, 'AliPay', 23, -1, -1, -1);
+INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (32, 0, 0);
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 32, id FROM System.PricePoint_Tbl WHERE amount = -1 AND countryid = 200;
+/* ========== Global Configuration for AliPay - Payment Method : END========== */
+
+
+/* ========== Global Configuration for Qiwi - Payment Method : START========== */
+INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength) VALUES (33, 'Qiwi', 23, -1, -1, -1);
+INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (33, 0, 0);
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 33, id FROM System.PricePoint_Tbl WHERE amount = -1 AND countryid = 200;
+/* ========== Global Configuration for Qiwi - Payment Method : END========== */
+/* ========== Global Configuration for Qiwi = STARTS ========== */
+INSERT INTO System.PSP_Tbl (id, name) VALUES (31, 'Qiwi');
+INSERT INTO System.PSPCurrency_Tbl (countryid, pspid, name) VALUES (200,31,'USD');
+
+/*Qiwi*/
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (33, 31);
+
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10001, 31, 'Qiwi', 'TBD', 'TBD');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100001, 31, '-1');
+
+-- Route Qiwi Card to Qiwi with country USA
+INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid, enabled, countryid) VALUES (10001, 33, 31, true, 200);
+/* ========== Global Configuration for Qiwi = ENDS ========== */
