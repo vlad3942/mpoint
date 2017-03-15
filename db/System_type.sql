7a8410c60bd5adf9a2f1e0094406ab8faa9adb55
Manish <manish@cellpointmobile.com>
2017-03-15 12:49:42 +0530

    CMP-1458 Write an update script to update the existing PSP table entries.

--- db/System_type.sql
+++ db/System_type.sql
@@ -20,6 +20,7 @@ ALTER TABLE system.processortype_tbl
 -- ALTER TABLE system.psp_tbl DROP COLUMN system_type;
 
 ALTER TABLE system.psp_tbl ADD COLUMN system_type integer;
+ALTER TABLE system.psp_tbl ALTER COLUMN system_type SET NOT NULL;
 
    -- Foreign Key: system.psptoproccessingtype_fk
 
@@ -44,3 +45,96 @@ ALTER TABLE system.psp_tbl
     INSERT INTO system.processortype_tbl(
             id, name)
     VALUES (4, 'APM');
+	
+   -- Insert data : system.psp_tbl 
+   -- Value : system_type;	
+   
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=0;
+ UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=1;
+  UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=2;
+  UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=3;
+  UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=4;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=5;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=6;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=7;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=8;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=9;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=10;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=11;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=12;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=13;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=14;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=15;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=16;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=17;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=18;
+   UPDATE system.psp_tbl
+   SET system_type=2
+ WHERE id=19;
+   UPDATE system.psp_tbl
+   SET system_type=3
+ WHERE id=20;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=21;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=22;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=23;
+   UPDATE system.psp_tbl
+   SET system_type=4
+ WHERE id=24;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=25;
+   UPDATE system.psp_tbl
+   SET system_type=1
+ WHERE id=26;
+   UPDATE system.psp_tbl
+   SET system_type=2
+ WHERE id=27;
+   UPDATE system.psp_tbl
+   SET system_type=2
+ WHERE id=28;
+ 
+	
