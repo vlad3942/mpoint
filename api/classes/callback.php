f114a55b71faa2aa5a26e89da3bc84a5d2990181
Manish <manish@cellpointmobile.com>
2017-05-24 11:31:22 +0530

    CMESB-1056 [Public Bank]Update Callback to include masked card number in request to general.php CMESB-1057 [Maybank] Update Callback to include masked card number in request to general.php

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -312,7 +312,7 @@ abstract class Callback extends EndUserAccount
 	 * @param 	SurePayConfig $$obj_SurePay SurePay Configuration Object. Default value null
 	 * @param 	integer $fee				The amount the customer will pay in fee�s for the Transaction. Default value 0
 	 */
-	public function notifyClient($sid, $pspid, $amt,  $cardno="", $cardid=0, $exp=0, SurePayConfig &$obj_SurePay=null, $fee=0)
+	public function notifyClient($sid, $pspid, $amt,  $cardno="", $cardid=0, $exp=null, SurePayConfig &$obj_SurePay=null, $fee=0)
 	{		
 		/* ----- Construct Body Start ----- */
 		$sBody = "";
@@ -331,7 +331,7 @@ abstract class Callback extends EndUserAccount
 		if ( strlen($this->_obj_TxnInfo->getDescription() ) > 0) { $sBody .= "&description=". urlencode($this->_obj_TxnInfo->getDescription() ); }
 		$sBody .= $this->getVariables();
 		$sBody .= "&hmac=". urlencode($this->_obj_TxnInfo->getHMAC() );
-		if($exp!=0)
+		if(empty($exp)===false)
 		{
 			$sBody .= "&expiry=". $exp;
 		}
