afadc3bc8816a2aad6be036a805b45a377ab4b1e
Rohit Malhotra <rohit@cellpointmobile.com>
2017-08-03 15:16:58 +0530

    CMP-1815 mRetail callback url should send country-id of customer from mPoint Removed the redundant params.

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -350,13 +350,7 @@ abstract class Callback extends EndUserAccount
 		if (($this->_obj_TxnInfo->getAccountID() > 0) === true )
         {
             $obj_CustomerInfo = CustomerInfo::produceInfo($this->getDBConn(), $this->_obj_TxnInfo->getAccountID());
-            $sBody .= "&customer-email=". urlencode($obj_CustomerInfo->getEMail()).
-                "&customer-mobile=". $obj_CustomerInfo->getMobile().
-                "&customer-country-id=". $obj_CustomerInfo->getCountryID();
-            if(empty($sDeviceID) === false)
-            {
-                $sBody .= "&customer-device-id=". urlencode($sDeviceID);
-            }
+            $sBody .= "&customer-country-id=". $obj_CustomerInfo->getCountryID();
         }
 
         /* ----- Construct Body End ----- */
