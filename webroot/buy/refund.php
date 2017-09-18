e1fea4ab0646e44c90ac4ccd90828fd21a956c6f
deblina <deblina@cellpointmobile.com>
2017-09-18 20:56:01 +0530

    Updating refund.php to have Klarna

--- webroot/buy/refund.php
+++ webroot/buy/refund.php
@@ -71,7 +71,8 @@ require_once(sCLASS_PATH ."/maybank.php");
 require_once(sCLASS_PATH ."/publicbank.php");
 // Require specific Business logic for the Qiwi component
 require_once(sCLASS_PATH ."/qiwi.php");
-
+// Require specific Business logic for the Klarna component
+require_once(sCLASS_PATH ."/klarna.php");
 
 header("Content-Type: application/x-www-form-urlencoded");
 
