3935d4dd5b743f2eac969675e1ac389e42cb2f02
Manish <manish@cellpointmobile.com>
2017-09-26 12:59:14 +0530

    CMP-1896 Capture and Refund of transaction fail when attempted the 1st time on PoC

--- webroot/buy/capture.php
+++ webroot/buy/capture.php
@@ -73,7 +73,8 @@ require_once(sCLASS_PATH ."/paypal.php");
 
 // Require specific Business logic for the Nets component
 require_once(sCLASS_PATH ."/nets.php");
-
+// Require specific Business logic for Customer Info component
+require_once(sCLASS_PATH ."/customer_info.php");
 header("Content-Type: application/x-www-form-urlencoded");
 
 // Require Business logic for the validating client Input
