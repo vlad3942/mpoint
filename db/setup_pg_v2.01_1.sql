07dd70bc3548cb4c5cce8df632ed5cb86e034c3c
Johan Thomsen <johan@cellpointmobile.com>
2017-10-09 10:37:02 +0200

    TAXA-43 : Added Taxa Mobilepay merchant configuration to setup_pg_v2.01.sql

--- db/setup_pg_v2.01_1.sql
+++ db/setup_pg_v2.01_1.sql
@@ -7,3 +7,7 @@ INSERT INTO Client.URL_Tbl
   (clientid, urltypeid, url)
 VALUES
   (10028, 4, 'https://taxa.mesb.cellpointmobile.com');
+
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name) VALUES (10028, 11, 'APPDK8535306001');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100043, 11, '-1');
+INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) VALUES (10028, 17, 11);
