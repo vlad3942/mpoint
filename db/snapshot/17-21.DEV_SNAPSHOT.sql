336d4f2766b2b2304894b982fdc2676963bd71ee
Amar kumar <amar@cellpointmobile.com>
2017-10-18 13:21:42 +0530

    CMP-1993 mPoint Database setup for paytabs

--- db/snapshot/17-21.DEV_SNAPSHOT.sql
+++ db/snapshot/17-21.DEV_SNAPSHOT.sql
@@ -1,11 +1,11 @@
 <-- Pay Tabs Database Script-->
 
-INSERT INTO System.PSP_Tbl (id, name) VALUES (40, 'PayTabs');
-INSERT INTO System.PSPCurrency_Tbl (countryid, pspid, name) VALUES (602,40,'AED');
-INSERT INTO System.PspCard_Tbl(cardid, pspid) VALUES (31, 40);
-INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 40, 'PayTabs', '10004931', 'zoVCrg1wOzCN22cXIZt5YM3TnAKoA5paulNWBOtqo6eq8roRqSWoEZh1A2qb7PlCa9yMX2cm8qMgSb7i34HH3ZID19P9YaL9jkVh');
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 40, '-1');
-UPDATE Client.CardAccess_Tbl SET pspid = 40, countryid = 602 WHERE clientid = 10007 AND cardid = 31;
+INSERT INTO System.PSP_Tbl (id, name) VALUES (38, 'PayTabs');
+INSERT INTO System.PSPCurrency_Tbl (countryid, pspid, name) VALUES (602,38,'AED');
+INSERT INTO System.PspCard_Tbl(cardid, pspid) VALUES (31, 38);
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 38, 'PayTabs', '10004931', 'zoVCrg1wOzCN22cXIZt5YM3TnAKoA5paulNWBOtqo6eq8roRqSWoEZh1A2qb7PlCa9yMX2cm8qMgSb7i34HH3ZID19P9YaL9jkVh');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 38, '-1');
+UPDATE Client.CardAccess_Tbl SET pspid = 38, countryid = 602 WHERE clientid = 10007 AND cardid = 31;
 
 INSERT INTO System.EndPoint_Tbl (productid, path, note) SELECT Max(id), '/mpoint/paytabs/initialize', 'mesb - Paytabs initialize of payment' FROM System.Product_Tbl WHERE id = 4;
 INSERT INTO System.Transformation_Tbl (endpointid, contenttypeid, note) SELECT EP.id, 4, EP.note  FROM System.EndPoint_Tbl EP WHERE path = '/mpoint/paytabs/initialize';
