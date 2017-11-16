a992660f35933d4e26fe7b0ab5602a6e8ed3f851
deblina <deblina@cellpointmobile.com>
2017-11-16 21:51:01 +0530

    CMP-1856 updating 2.02 scripts

--- db/master_pg_v2.00.sql
+++ db/master_pg_v2.00.sql
@@ -181,37 +181,7 @@ WITH (
 ALTER TABLE client.gomobileconfiguration_tbl
   OWNER TO mpoint;
 
-/*---------START : ADDED CHANGE FOR SUPPORTING CURRENCY SCHEMA-------------*/
--- Table: system.currency_tbl
 
--- DROP TABLE system.currency_tbl;
-
-CREATE TABLE system.currency_tbl
-(
-  id serial NOT NULL,
-  name character varying(100),
-  code character(3),
-  decimals integer,
-  created timestamp without time zone DEFAULT now(),
-  modified timestamp without time zone DEFAULT now(),
-  enabled boolean DEFAULT true,
-  CONSTRAINT currency_pk PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE system.currency_tbl
-  OWNER TO mpoint;
-
-
-ALTER TABLE system.country_tbl ADD COLUMN alpha2code character(2);
-ALTER TABLE system.country_tbl ADD COLUMN alpha3code character(3);
-ALTER TABLE system.country_tbl ADD COLUMN code integer;
-ALTER TABLE system.country_tbl ADD COLUMN currencyid integer;
-ALTER TABLE system.country_tbl ADD CONSTRAINT Country2Currency_FK FOREIGN KEY (currencyid) REFERENCES System.Currency_Tbl(id) ON UPDATE CASCADE ON DELETE RESTRICT;
-ALTER TABLE system.country_tbl DROP COLUMN currency;
-
-/*---------END : ADDED CHANGE FOR SUPPORTING CURRENCY SCHEMA-------------*/
 
 
 /* ==================== SYSTEM PAYMENT MODE START ==================== */
@@ -270,35 +240,6 @@ ALTER TABLE system.Card_tbl ADD CONSTRAINT Card2PaymentMode_FK FOREIGN KEY (mode
 REFERENCES System.paymentmode_tbl (id)
 ON UPDATE CASCADE ON DELETE RESTRICT;
 /* ==================== ALTER SYSTEM CARD END ==================== */
-
-
-/* ==================== ALTER TRANSACTION LOG START ==================== */
-ALTER TABLE Log.Transaction_Tbl ADD COLUMN currencyid integer;
-ALTER TABLE Log.Transaction_Tbl ADD CONSTRAINT Txn2Currency_FK FOREIGN KEY (currencyid)
-REFERENCES System.currency_tbl (id)
-ON UPDATE CASCADE ON DELETE RESTRICT;
-/* ==================== ALTER TRANSACTION LOG END ==================== */
-
-ALTER TABLE system.pspcurrency_tbl ADD COLUMN currencyid integer;
-/* Run Alter Scripts to update currency Id before deleting country id column */
-ALTER TABLE system.pspcurrency_tbl DROP COLUMN countryid integer;
-
-
-/* ================ Update pricepoint table  ===================*/
-
-ALTER TABLE system.pricepoint_tbl ADD COLUMN currencyid integer;
-
-ALTER TABLE system.pricepoint_tbl  ADD CONSTRAINT Price2Currency_FK FOREIGN KEY (currencyid)
-REFERENCES System.currency_tbl (id);
-
-
-
-ALTER TABLE system.pricepoint_tbl DROP COLUMN countryid;
-
-ALTER TABLE system.cardpricing_tbl ADD COLUMN currencyid integer;
-
-ALTER TABLE system.cardpricing_tbl  ADD CONSTRAINT Card2Currency_FK FOREIGN KEY (currencyid)
-REFERENCES System.currency_tbl (id);
 
  /*
  *
