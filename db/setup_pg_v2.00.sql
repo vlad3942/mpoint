8fbc241724e27c50247d59063086e68597b580c6
Amar kumar <amar@cellpointmobile.com>
2017-12-14 16:56:01 +0530

    CPMRM-1611 [2C2P-ALC] Country currency mapping for PAL

--- db/setup_pg_v2.00.sql
+++ db/setup_pg_v2.00.sql
@@ -1315,3 +1315,14 @@ INSERT INTO system.pspcard_tbl (cardid, pspid) VALUES (7, 40);
 
 /* END*/
 
+
+
+--2C2P-ALC MID's-- [Please change clientid as per your environment]
+
+INSERT INTO client.additionalproperty_tbl (key,value,enabled,externalid,type) SELECT 'mid.THB','NMATHB','t', id, 'merchant' FROM client.merchantaccount_tbl WHERE clientid=10007 AND pspid=40
+INSERT INTO client.additionalproperty_tbl (key,value,enabled,externalid,type) SELECT 'mid.PHP','NMAPHP','t', id, 'merchant' FROM client.merchantaccount_tbl WHERE clientid=10007 AND pspid=40
+INSERT INTO client.additionalproperty_tbl (key,value,enabled,externalid,type) SELECT 'mid.USD','NMAUSD','t', id, 'merchant' FROM client.merchantaccount_tbl WHERE clientid=10007 AND pspid=40
+
+-------------------------------------------------
+
+
