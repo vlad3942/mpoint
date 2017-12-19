bc5e9ddc56185ca8a8c0c34f8718125de60f7aea
Urmila S <urmila.s@cellpointmobile.com>
2017-12-20 01:16:35 +0530

    CMP-2138: updated for card holder name, api tested

--- webroot/mApp/api/save_card.php
+++ webroot/mApp/api/save_card.php
@@ -130,9 +130,9 @@ if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PH
 							// Modifying an existing Stored Card
 							if (intval($obj_DOM->{'save-card'}[$i]->card[$j]["id"]) > 0)
 							{
-								if(intval($obj_DOM->{'save-card'}[$i]->card[$j]["card-holder-name"]) > 0)
+								if(count($obj_DOM->{'save-card'}[$i]->card[$j]->{'card-holder-name'}) == 1)
                                 {
-                                    $code = $obj_mPoint->saveCardName($obj_DOM->{'save-card'}[$i]->card[$j]["id"], (string) $obj_DOM->{'save-card'}[$i]->card[$j]->name, General::xml2bool($obj_DOM->{'save-card'}[$i]->card[$j]["preferred"]),(string) $obj_DOM->{'save-card'}[$i]->card[$j]->card-holder-name );
+                                    $code = $obj_mPoint->saveCardName($obj_DOM->{'save-card'}[$i]->card[$j]["id"], (string) $obj_DOM->{'save-card'}[$i]->card[$j]->name, General::xml2bool($obj_DOM->{'save-card'}[$i]->card[$j]["preferred"]),(string) $obj_DOM->{'save-card'}[$i]->card[$j]->{'card-holder-name'} );
                                 }
                                 else
                                 {
