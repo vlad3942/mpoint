c289979b84dd0d0bcac4e580aaac75624cc3f44b
Sagar Narayane <sagar@cellpointmobile.com>
2018-01-30 13:25:10 +0530

    CMP-2228
    Add a new payment method type "VIRTUAL"

--- db/snapshot/18-01-SIT-Build.sql
+++ db/snapshot/18-01-SIT-Build.sql
@@ -7,7 +7,7 @@ INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength
 INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (39, 0, 0);
 INSERT INTO system.cardpricing_tbl ( pricepointid, cardid) VALUES ( -840, 39);
 
-INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (41, 'Citcon',6);
+INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (41, 'Citcon',5);
 INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (840,41,'USD');
 INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (39, 41);
 INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 41, 'Citcon', '', '');
