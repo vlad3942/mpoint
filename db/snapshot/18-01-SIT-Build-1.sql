c534796e0f240647e65f00d48a4f7dc3d39986da
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-01-31 21:07:30 +0530

    CMP-2229 Update VCO profile name in client.merchantsubaccount_tbl

--- db/snapshot/18-01-SIT-Build-1.sql
+++ db/snapshot/18-01-SIT-Build-1.sql
@@ -1,2 +1,3 @@
 /*  ===========  START : Adding Default value to   ==================  */
 UPDATE Client.Client_Tbl SET communicationchannels = 5;
+UPDATE client.merchantsubaccount_tbl SET name='Default' where pspid=13
