cfe9665063ba72f82d53564f05ad4524020ff72e
Sagar Narayane <sagar@cellpointmobile.com>
2018-02-07 18:40:49 +0530

    CMP-2243
    Add new Processor type 'MPI'

---
+++ db/snapshot/18-02-SIT-Build-1.sql
@@ -0,0 +1,3 @@
+/*  ===========  START : Adding New Processor Type  ==================  */
+INSERT INTO system.processortype_tbl (id, name) VALUES (6, 'Merchant Plug-in');
+/*  ===========  END : Adding New Processor Type  ==================  */
