9e4e0b8f1732ed1e0ebb1bb7adf92752f73b1e2c
Sagar Narayane <sagar@cellpointmobile.com>
2018-02-07 18:42:28 +0530

    CMP-2244
    Add new MPI - NetsMpi

--- db/snapshot/18-02-SIT-Build-1.sql
+++ db/snapshot/18-02-SIT-Build-1.sql
@@ -1,3 +1,9 @@
 /*  ===========  START : Adding New Processor Type  ==================  */
 INSERT INTO system.processortype_tbl (id, name) VALUES (6, 'Merchant Plug-in');
 /*  ===========  END : Adding New Processor Type  ==================  */
+
+INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (42, 'NETS MPI',6);
+INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (840,42,'USD');
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 42, 'NETS MPI', '', '');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 42, '-1');
+INSERT INTO client.cardaccess_tbl ( clientid, cardid, enabled, pspid, countryid, stateid, position) VALUES (10007, 8, true, 42, 200, 1, null);
