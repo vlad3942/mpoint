d83075ce52e26ea1036d80fbd1bf68be9295a4dc
Sagar Narayane <sagar@cellpointmobile.com>
2018-02-07 21:41:01 +0530

    CMP-2200
    Add support to authentication

--- db/snapshot/18-02-SIT-Build-1.sql
+++ db/snapshot/18-02-SIT-Build-1.sql
@@ -7,3 +7,5 @@ INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (840,42,'USD
 INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 42, 'NETS MPI', '', '');
 INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 42, '-1');
 INSERT INTO client.cardaccess_tbl ( clientid, cardid, enabled, pspid, countryid, stateid, position) VALUES (10007, 8, true, 42, 200, 1, null);
+
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('NETS_3DVERIFICATION', 'true', 10007, 'client');
