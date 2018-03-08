c3098184d14661cd091d4c6456215eeb508bf386
Urmila S <urmila.s@cellpointmobile.com>
2018-03-08 17:33:56 +0530

    CMP-2262: updated to remove redundant entry for psp 44

--- db/snapshot/18-03-DEV_SNAPSHOT.sql
+++ db/snapshot/18-03-DEV_SNAPSHOT.sql
@@ -15,7 +15,6 @@ INSERT INTO System.PSPCard_Tbl (pspid, cardid) VALUES (44, 41);
 
 INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10052, 44, 'BARJB0sDNz5hR1S7/3OdMlHoslZuiQ+uLDfVudq3p7HFbPZAX7yK0HUjeUnAxF6w9iplh0wONq7s4g7QbmOZVTo=', NULL, NULL);
 INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100071, 44, 'Google Pay');
-INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid,countryid, psp_type) VALUES (10052, 41, 44,200,3)
 
 --Enable WireCard for GPay
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid,countryid, psp_type) VALUES (10052, 41, 18,200,1);
