84ef4570d9b78bfac084df130f4d9381da9b57e7
arvind <arvind@cellpointmobile.com>
2018-03-13 19:15:10 +0530

    CPMRM-2434

--- webroot/cron/void-pending-sessions.php
+++ webroot/cron/void-pending-sessions.php
@@ -1,53 +1,5 @@
 <?php
-if (isset($_SERVER['DOCUMENT_ROOT']) === true && empty($_SERVER['DOCUMENT_ROOT']) === false) {
-    $_SERVER['DOCUMENT_ROOT'] = str_replace("\\", "/", $_SERVER['DOCUMENT_ROOT']);
-    // Define system path constant
-    define("sSYSTEM_PATH", substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], "/")));
-}
-// Command line
-else {
-    $aTemp = explode("/", str_replace("\\", "/", __FILE__));
-    $sPath = "";
-    for ($i = 0; $i < count($aTemp) - 3; $i++) {
-        $sPath .= $aTemp[$i] . "/";
-    }
-    // Define system path constant
-    define("sSYSTEM_PATH", substr($sPath, 0, strlen($sPath) - 1));
-}
-// Define path to the General API classes
-define("sAPI_CLASS_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/")) . "/php5api/classes/");
-// Define path to the General API interfaces
-define("sAPI_INTERFACE_PATH", substr(sSYSTEM_PATH, 0, strrpos(sSYSTEM_PATH, "/")) . "/php5api/interfaces/");
-// Define path to the System Configuration
-define("sCONF_PATH", sSYSTEM_PATH . "/conf/");
-// Define path to the System classes
-define("sCLASS_PATH", sSYSTEM_PATH . "/api/classes/");
-
-require_once(sAPI_INTERFACE_PATH . "database.php");
-require_once(sCLASS_PATH . "general.php");
-require_once(sAPI_CLASS_PATH . "report.php");
-require_once(sAPI_CLASS_PATH ."/template.php");
-require_once(sAPI_CLASS_PATH ."/http_client.php");
-require_once(sAPI_CLASS_PATH . "database.php");
-require_once(sCONF_PATH . "global.php");
-
-require_once(sCLASS_PATH ."/basicconfig.php");
-require_once(sCLASS_PATH ."/countryconfig.php");
-require_once(sCLASS_PATH ."/currencyconfig.php");
-require_once(sCLASS_PATH ."/client_config.php");
-require_once(sCLASS_PATH ."/account_config.php");
-require_once(sCLASS_PATH ."/client_merchant_subaccount_config.php");
-require_once(sCLASS_PATH ."/client_merchant_account_config.php");
-require_once(sCLASS_PATH ."/client_payment_method_config.php");
-require_once(sCLASS_PATH ."/client_url_config.php");
-require_once(sCLASS_PATH ."/client_issuer_identifcation_number_range_config.php");
-require_once(sCLASS_PATH ."/client_gomobile_config.php");
-require_once(sCLASS_PATH ."/client_communication_channel_config.php");
-require_once(sCLASS_PATH ."/keywordconfig.php");
-
-
-$_OBJ_DB = RDB::produceDatabase($aDB_CONN_INFO["mpoint"]);
-
+require_once("../inc/include.php");
 
 $sql = "SELECT sn.id, sn.amount
           FROM log" . sSCHEMA_POSTFIX . ".session_tbl sn
@@ -74,6 +26,7 @@ while ($RS = $_OBJ_DB->fetchName($res)) {
         $results = array_merge($results, $transactionIds);
     }
 }
+
 // Call Void API for Cancel/Refund the transaction
 foreach ($results as $result) {
     $xml = '<?xml version="1.0" encoding="UTF-8"?>';
@@ -91,7 +44,7 @@ foreach ($results as $result) {
 //Performs a VOID (Refund or cancel) operation for the provided transaction.
 function void($xml, $obj_ClientConfig) {
     try {
-        $obj_ConnInfo = new HTTPConnInfo('http', 'mpoint.dev2.cellpointmobile.com', 80, 20, '/mApp/api/void.php', 'POST', 'text/xml', $obj_ClientConfig->getUsername(), $obj_ClientConfig->getPassword());
+        $obj_ConnInfo = new HTTPConnInfo('http', $_SERVER['HTTP_HOST'], 80, 20, '/mApp/api/void.php', 'POST', 'text/xml', $obj_ClientConfig->getUsername(), $obj_ClientConfig->getPassword());
 
         $obj_HTTP = new HTTPClient(new Template(), $obj_ConnInfo);
         $obj_HTTP->connect();
