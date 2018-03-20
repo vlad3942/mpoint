d4917d6d21ff709f674d0199c58f5f53e179f63a
Urmila S <urmila.s@cellpointmobile.com>
2018-03-20 16:27:25 +0530

    CPMRM-2470: Additional property updated to set timer value in mm:ss

--- db/snapshot/18-05-SIT-Build.sql
+++ db/snapshot/18-05-SIT-Build.sql
@@ -3,8 +3,8 @@
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('QR_CODE_TIMEOUT', '180', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10007 and pspid = 41), 'merchant');
 
 
---Virtual payment page timer value in minutes, this should be less than or equal to the QR code timeout property
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('VIRTUAL_PAYMENT_TIMER', '3', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10007 and pspid = 41), 'merchant');
+--Virtual payment page timer value in mm:ss, this should be less than or equal to the QR code timeout property
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('VIRTUAL_PAYMENT_TIMER', '02:00', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10007 and pspid = 41), 'merchant');
 
 
 --url to link wechat icon
