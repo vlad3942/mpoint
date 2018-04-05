ebf658ef57f81dd9ab43f904e833a0a7ff5afe1e
arvind <arvind@cellpointmobile.com>
2018-04-05 12:00:07 +0530

    CPMRM-2774

--- api/interfaces/cpm_psp.php
+++ api/interfaces/cpm_psp.php
@@ -430,8 +430,8 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 		$b  = '<?xml version="1.0" encoding="UTF-8"?>';
 		$b .= '<root>';
 		$b .= '<authorize client-id="'. $this->getClientConfig()->getID(). '" account="'. $this->getClientConfig()->getAccountConfig()->getID(). '">';
-                $b .= '<client-config>';
-                $b .= '<additional-config>';
+        $b .= '<client-config>';
+        $b .= '<additional-config>';
 
         foreach ($this->getClientConfig()->getAdditionalProperties() as $aAdditionalProperty)
         {
@@ -815,12 +815,12 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 		$b .= '<expiry-month>'. $expiry_month .'</expiry-month>';
 		$b .= '<expiry-year>'. $expiry_year .'</expiry-year>';
                 
-                if(count($obj_Card->{'valid-from'}) > 0) {
-                    list($valid_from_month, $valid_from_year) = explode("/", $obj_Card->{'valid-from'});
-                    $valid_from_year = substr_replace(date('Y'), $valid_from_year, -2);
-                    $b .= '<valid-from-month>'. $valid_from_month .'</valid-from-month>';
-                    $b .= '<valid-from-year>'. $valid_from_year .'</valid-from-year>';
-                }
+        if(count($obj_Card->{'valid-from'}) > 0) {
+            list($valid_from_month, $valid_from_year) = explode("/", $obj_Card->{'valid-from'});
+            $valid_from_year = substr_replace(date('Y'), $valid_from_year, -2);
+            $b .= '<valid-from-month>'. $valid_from_month .'</valid-from-month>';
+            $b .= '<valid-from-year>'. $valid_from_year .'</valid-from-year>';
+        }
                 
 		if(count($obj_Card->cvc) > 0) { $b .= '<cvc>'. $obj_Card->cvc .'</cvc>'; }	
 
@@ -849,12 +849,12 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 		$b .= '<expiry-year>'. $expiry_year .'</expiry-year>';
 		$b .= '<token>'. $obj_Card->ticket .'</token>';
                 
-                if(count($obj_Card->{'valid-from'}) > 0) {
-                    list($valid_from_month, $valid_from_year) = explode("/", $obj_Card->{'valid-from'});
-                    $valid_from_year = substr_replace(date('Y'), $valid_from_year, -2);
-                    $b .= '<valid-from-month>'. $valid_from_month .'</valid-from-month>';
-                    $b .= '<valid-from-year>'. $valid_from_year .'</valid-from-year>';
-                }
+        if(count($obj_Card->{'valid-from'}) > 0) {
+            list($valid_from_month, $valid_from_year) = explode("/", $obj_Card->{'valid-from'});
+            $valid_from_year = substr_replace(date('Y'), $valid_from_year, -2);
+            $b .= '<valid-from-month>'. $valid_from_month .'</valid-from-month>';
+            $b .= '<valid-from-year>'. $valid_from_year .'</valid-from-year>';
+        }
                 
 		if(count($obj_Card->cvc) > 0) { $b .= '<cvc>'. $obj_Card->cvc .'</cvc>'; }		
 
