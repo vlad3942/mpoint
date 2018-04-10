7ac589ae79e43460d87f3f22bdd3bcbc359900e6
Urmila S <urmila.s@cellpointmobile.com>
2018-04-10 12:15:40 +0530

    CMP-2330: Datacash MID sql script

--- db/snapshot/18-06-SIT-Build.sql
+++ db/snapshot/18-06-SIT-Build.sql
@@ -24,3 +24,8 @@ ALTER TABLE log.flight_tbl
 
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) SELECT 'pid.html', '2088102135220161', id, 'merchant' FROM client.merchantaccount_tbl WHERE clientid=10007 AND pspid=43 ;
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) SELECT 'pid.app', '2088102170185364', id, 'merchant' FROM client.merchantaccount_tbl WHERE clientid=10007 AND pspid=43 ;
+
+
+
+---Datacash MID
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, password) VALUES (10007, 17, 'SGBSABB01', 'merchant.SGBSABB01', 'bebd68b2fa491f807e40462a6f85617e');
