8d7af25d2d8014fcae7e933ffbbf78ea81aa0ade
Sagar Narayane <sagar@cellpointmobile.com>
2018-04-11 18:55:33 +0530

    PHP Unit changes

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -7342,36 +7342,6 @@ ALTER TABLE client.client_tbl ADD COLUMN communicationchannels integer DEFAULT 0
 /*  ===========  END : Adding communicationchannels to Client.Client_Tbl  ==================  */
 
 
-CREATE TABLE system.ProductType_Tbl
-(
-  id INT PRIMARY KEY,
-  name VARCHAR(10) NOT NULL
-);
-CREATE UNIQUE INDEX ProductType_Tbl_name_uindex ON system.ProductType_Tbl (name);
-COMMENT ON COLUMN system.ProductType_Tbl.id IS 'Unique number of product type';
-COMMENT ON COLUMN system.ProductType_Tbl.name IS 'Product type name';
-COMMENT ON TABLE system.ProductType_Tbl IS 'Contains all product types';
-
-ALTER TABLE system.ProductType_Tbl
-  OWNER TO mpoint;
-
-/*  ===========  END : Adding Table System.ProductType_Tbl  ==================  */
-
-/*  ===========  START : Adding new product types in System.ProductType_Tbl ==================  */
-INSERT INTO system.producttype_tbl (id, name) VALUES (100, 'Ticket');
-INSERT INTO system.producttype_tbl (id, name) VALUES (200, 'Ancillary');
-INSERT INTO system.producttype_tbl (id, name) VALUES (210, 'Insurance');
-/*  ===========  END : Adding new product types in System.ProductType_Tbl ==================  */
-
-
-/*  ===========  START : Adding producttype to Log.Transaction_Tbl  ==================  */
-ALTER TABLE log.transaction_tbl ADD producttype INT DEFAULT 100 NOT NULL;
-COMMENT ON COLUMN log.transaction_tbl.producttype IS 'Product type of transaction';
-ALTER TABLE log.transaction_tbl
-  ADD CONSTRAINT transaction_tbl_producttype_tbl_id_fk
-FOREIGN KEY (producttype) REFERENCES system.producttype_tbl (id);
-
-/*  ===========  END : Adding producttype to Log.Transaction_Tbl  ==================  */
 ALTER TABLE client.cardaccess_tbl ADD psp_type INT DEFAULT 1 NOT NULL;
 ALTER TABLE client.cardaccess_tbl
   ADD CONSTRAINT cardaccess_tbl_processortype_tbl_id_fk
@@ -7444,8 +7414,13 @@ ALTER TABLE client.producttype_tbl
 INSERT INTO system.producttype_tbl( id, name, description, code )  VALUES (110, 'Airline Ticket', 'Flight Tickets', 'AIRTCKT');
 INSERT INTO system.producttype_tbl( id, name, description, code )  VALUES (210, 'Airline Insurance', 'Insurance products purchased', 'INSRNC');
 
- 
 
+/*  ===========  START : Adding producttype to Log.Transaction_Tbl  ==================  */
+ALTER TABLE log.transaction_tbl ADD producttype INT ;
+COMMENT ON COLUMN log.transaction_tbl.producttype IS 'Product type of transaction';
+
+
+/*  ===========  END : Adding producttype to Log.Transaction_Tbl  ==================  */
 /*=========== Gateway Triggers ============*/
 
 -- Table: system.triggerunit_tbl
