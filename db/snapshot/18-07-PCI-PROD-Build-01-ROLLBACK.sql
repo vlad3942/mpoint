63f2d0e9e4404c5405720bd52bae851cb985b5ef
Rohit Malhotra <rohit@cellpointmobile.com>
2018-05-02 20:51:33 +0530

    PCI-171 PCI : Service window for cpm_prod1_appserver2/cpm_prod1_appserver1 mPoint/ MESB upgrade (2nd May)
    Added rollback scripts

--- db/snapshot/18-07-PCI-PROD-Build-01-ROLLBACK.sql
+++ db/snapshot/18-07-PCI-PROD-Build-01-ROLLBACK.sql
@@ -1,5 +1,7 @@
 DROP INDEX client.cardaccess_card_country_uq RESTRICT;
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl (clientid, cardid, countryid);
+ALTER TABLE client.cardaccess_tbl DROP CONSTRAINT cardaccess_tbl_processortype_tbl_id_fk;
+ALTER TABLE client.cardaccess_tbl DROP COLUMN psp_type;
 
 ALTER TABLE log.transaction_tbl DROP CONSTRAINT transaction_tbl_producttype_tbl_id_fk;
 ALTER TABLE log.transaction_tbl DROP COLUMN producttype;
