274d64fb442f1f1f0ba9141d6d28ea520fde26d8
deblina <deblina@cellpointmobile.com>
2018-05-14 17:56:55 +0530

    CMP-2383 updated transacion count conditions

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -157,10 +157,10 @@ abstract class Callback extends EndUserAccount
 		if (is_resource($res) === true)
 		{
 				$iIsCompleteTransactionStateLogged = $this->_obj_TxnInfo->hasEitherState ( $this->getDBConn (), $sid );
-				if ($iIsCompleteTransactionStateLogged == 1 && $sid == Constants::iPAYMENT_ACCEPTED_STATE) {
+				if ($iIsCompleteTransactionStateLogged > 0 && $sid == Constants::iPAYMENT_ACCEPTED_STATE) {
 					$this->newMessage ( $this->_obj_TxnInfo->getID (), Constants::iPAYMENT_DUPLICATED_STATE, var_export ( $debug, true ) );
 					$sid = Constants::iPAYMENT_DUPLICATED_STATE;
-				} else if ($iIsCompleteTransactionStateLogged != 1) {
+				} else if ($iIsCompleteTransactionStateLogged == 0 ) {
 					$this->newMessage ( $this->_obj_TxnInfo->getID (), $sid, var_export ( $debug, true ) );
 				}
 			
