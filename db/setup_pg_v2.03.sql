9c515fa52e135bce3abc426fc856ed7d39c4831d
Apeksha <apeksha.saxena@cellpointmobile.com>
2018-05-22 18:28:22 +0530

    CPMRM-2949

--- db/setup_pg_v2.03.sql
+++ db/setup_pg_v2.03.sql
@@ -1,6 +1,8 @@
 /*START: Fixing incorrect values for USD in PSPCurrency_tbl in system schema */
 
-update system.pspcurrency_tbl set name = 'USD' where currencyid=840
-update system.pspcurrency_tbl set currencyid = 840 where name = 'USD'
+UPDATE system.pspcurrency_tbl
+SET name = currency_tbl.code
+FROM system.currency_tbl
+WHERE currency_tbl.id = pspcurrency_tbl.currencyid;
 
 /*END: Fixing incorrect values for USD in PSPCurrency_tbl in system schema*/
