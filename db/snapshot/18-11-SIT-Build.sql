9b426bc52eb02e1565eecdca00f03b3ca372f174
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-06-19 17:36:10 +0530

    CMP-2444 ForcedAuth changed to caps

--- db/snapshot/18-11-SIT-Build.sql
+++ db/snapshot/18-11-SIT-Build.sql
@@ -10,6 +10,6 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES 
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('MERCHANT_NAME', 'Cellpoint Mobile', 10007, 'client');
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('MERCHANT_GMTOFFSET', '+1', 10007, 'client');
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('MERCHANT_URL', 'http://www.cpm.com', 10007, 'client');
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('ForcedAuth', 'true', 10007, 'client');
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('FORCED_AUTH', 'true', 10007, 'client');
 
 --END NETS acquirer additional property
