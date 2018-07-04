5f4198f57509a093d45bad02c83f57d4d13842b2
arvind <arvind@cellpointmobile.com>
2018-07-04 19:34:14 +0530

    CMP-2426

--- db/snapshot/18-12-SIT-Build.sql
+++ db/snapshot/18-12-SIT-Build.sql
@@ -4,3 +4,7 @@ UPDATE log.state_tbl SET name = 'Issuer timed out' WHERE id = 20109;
 INSERT INTO log.state_tbl (id, name, module) VALUES (20108, 'PSP timed out', 'Payment');
 
 -------- TXN Time Out -------------
+
+-------- CMP-2426: PCI Password expose --------
+DROP TABLE admin.user_tbl CASCADE;
+-------- CMP-2426: PCI Password expose --------
