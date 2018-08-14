ba9f3b2b0d4ef38407bdc22e5209d0dde5c37ec4
Rohit Malhotra <rohit@cellpointmobile.com>
2018-08-14 12:02:51 +0530

    Adding the sprint end script

---
+++ db/snapshot/18-15-SIT-Build-2.sql
@@ -0,0 +1,4 @@
+/*=================== Adding new states for tokenization used for UATP SUVTP generation : START =======================*/
+INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2020 , 'Tokenization complete - Virtual card created', 'Authorization', true);
+INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2021 , 'Tokenization Failed', 'Authorization', true);
+/*=================== Adding new states for tokenization used for UATP SUVTP generation : END =======================*/
