74de37d3114ba83573d320ef67f3a67db973ab99
Anna Lagad <anna.lagad@cellpointmobile.com>
2018-08-14 17:20:15 +0530

    CPMEH-197 Added sql script

--- db/snapshot/18-15-SIT-Build-2.sql
+++ db/snapshot/18-15-SIT-Build-2.sql
@@ -2,3 +2,6 @@
 INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2030 , 'Tokenization complete - Virtual card created', 'Authorization', true);
 INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2031 , 'Tokenization Failed', 'Authorization', true);
 /*=================== Adding new states for tokenization used for UATP SUVTP generation : END =======================*/
+
+INSERT INTO client.additionalproperty_tbl(key, value,externalid, type)
+    VALUES ('webSessionTimeout', 2000, 10007, 'client');
