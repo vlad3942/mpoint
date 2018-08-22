7036b63e0bd925f6b1fa6d5add9980178393fee6
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-08-23 01:56:34 +0530

    CMP-2532 Create Database script Optimized Flow for Modirum

--- db/snapshot/18-16-SIT-Build-2.sql
+++ db/snapshot/18-16-SIT-Build-2.sql
@@ -5,4 +5,7 @@ UPDATE client.additionalproperty_tbl SET value = 10 WHERE key ='webSessionTimeou
 INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, 10007, 'http://od.mesb.dev2.cellpointmobile.com:10080/mpoint/parse-3dsecure-challenge');
 -- End NETS 3D Optimized Flow Url Configuration
 
+-- Modirum 3D Optimized Flow Url Configuration
+INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, 10007, 'http://od.mesb.dev2.cellpointmobile.com:10080/mpoint/parse-3dsecure-challenge');
+-- End Modirum 3D Optimized Flow Url Configuration
 
