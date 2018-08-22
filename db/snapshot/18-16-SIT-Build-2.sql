ec04c5077943d7c3b7b0191447c0a6f89206a1fd
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-08-23 01:34:57 +0530

    CMP-2543 Create Database script Optimized Flow for NETS

--- db/snapshot/18-16-SIT-Build-2.sql
+++ db/snapshot/18-16-SIT-Build-2.sql
@@ -1,5 +1,8 @@
 /*=================== Web session timeout in minutes : START =======================*/
 UPDATE client.additionalproperty_tbl SET value = 10 WHERE key ='webSessionTimeout' AND externalid = 10007
 /*=================== Web session timeout in minutes : END =======================*/
+-- NETS 3D Optimized Flow Url Configuration
+INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, 10007, 'http://od.mesb.dev2.cellpointmobile.com:10080/mpoint/parse-3dsecure-challenge');
+-- End NETS 3D Optimized Flow Url Configuration
 
 
