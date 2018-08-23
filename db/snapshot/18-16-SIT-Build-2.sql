b29fa27668c3650c4be9f232701c88ba460acde1
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-08-23 12:17:10 +0530

    CMP-2543 Adding PlaceHolder for script

--- db/snapshot/18-16-SIT-Build-2.sql
+++ db/snapshot/18-16-SIT-Build-2.sql
@@ -2,10 +2,10 @@
 UPDATE client.additionalproperty_tbl SET value = 10 WHERE key ='webSessionTimeout' AND externalid = 10007
 /*=================== Web session timeout in minutes : END =======================*/
 -- NETS 3D Optimized Flow Url Configuration
-INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, 10007, 'http://od.mesb.dev2.cellpointmobile.com:10080/mpoint/parse-3dsecure-challenge');
+INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, <CLIENTID>, '<MESB-URL>/mpoint/parse-3dsecure-challenge');
 -- End NETS 3D Optimized Flow Url Configuration
 
 -- Modirum 3D Optimized Flow Url Configuration
-INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, 10007, 'http://od.mesb.dev2.cellpointmobile.com:10080/mpoint/parse-3dsecure-challenge');
+INSERT INTO client.url_tbl ( urltypeid, clientid, url) VALUES(12, <CLIENTID>, '<MESB-URL>/mpoint/parse-3dsecure-challenge');
 -- End Modirum 3D Optimized Flow Url Configuration
 
