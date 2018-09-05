28dfbc5ea62c818f289c6c085d1cf64f38ed846e
Urmila S <urmila.s@cellpointmobile.com>
2018-09-05 17:21:31 +0530

    CMP-2550: DB config for Paytabs - MADA Integration

--- db/setup_pg_v2.03.sql
+++ db/setup_pg_v2.03.sql
@@ -119,5 +119,14 @@ INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2021 , 'Tokenizati
 DROP INDEX client.cardaccess_card_country_uq RESTRICT;
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl (clientid, cardid, pspid, countryid, psp_type) WHERE enabled='true';
 
+---- MADA Integration Start ---
 
+INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength,paymenttype) VALUES (71, 'MADA', 23, -1, -1, -1,4);
 
+INSERT INTO client.cardaccess_tbl ( clientid, cardid, enabled, pspid, countryid, stateid, position, psp_type) VALUES (10007, 71, true, 38, 601, 1, null, 1);
+INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (71, 0, 0);
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 71, id FROM System.PricePoint_Tbl WHERE amount = -1 AND currencyid = 48;
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (71, 38, true);
+
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 71, id FROM System.PricePoint_Tbl WHERE amount = -1 AND currencyid = 414;
+-- MADA Integration stop --
---
+++ db/snapshot/18-17-SIT-Build.sql
@@ -0,0 +1,11 @@
+---- MADA Integration Start ---
+
+INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength,paymenttype) VALUES (71, 'MADA', 23, -1, -1, -1,4);
+
+INSERT INTO client.cardaccess_tbl ( clientid, cardid, enabled, pspid, countryid, stateid, position, psp_type) VALUES (10007, 71, true, 38, 601, 1, null, 1);
+INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (71, 0, 0);
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 71, id FROM System.PricePoint_Tbl WHERE amount = -1 AND currencyid = 48;
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (71, 38, true);
+
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 71, id FROM System.PricePoint_Tbl WHERE amount = -1 AND currencyid = 414;
+-- MADA Integration stop --
