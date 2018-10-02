c15a175d4f9fe8e6c9481ea41da0bef006dabe59
Urmila S <urmila.s@cellpointmobile.com>
2018-10-02 22:18:28 +0530

    CMP-2589: DB script for country-code/IDD.

--- db/master_pg_v2.03.sql
+++ db/master_pg_v2.03.sql
@@ -6,3 +6,7 @@ ALTER TABLE log.settlement_tbl ALTER COLUMN status TYPE varchar(100) USING statu
 ALTER TYPE LOG.ADDITIONAL_DATA_REF ADD VALUE 'Transaction';
 DROP INDEX client.cardaccess_card_country_uq RESTRICT;
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl (clientid, cardid, countryid, psp_type) WHERE enabled='true';
+
+
+-- country calling code
+ALTER TABLE system.country_tbl ADD country_calling_code INTEGER NULL;
