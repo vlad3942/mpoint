6e007a1ffb3fc3bf703a4737d01b0f14fa344f53
Urmila S <urmila.s@cellpointmobile.com>
2018-10-03 18:19:07 +0530

    CMP-2601: removed existing sql scripts

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -7685,17 +7685,6 @@ DROP INDEX client.cardaccess_card_country_uq RESTRICT;
 
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl (clientid, cardid, countryid, psp_type) WHERE enabled='true';
 
-
--- Adding Virtual Token for Saving SUVTP in mPoint schema
-ALTER TABLE Log.Transaction_Tbl ADD COLUMN virtualtoken character varying(512);
-
-ALTER TABLE log.settlement_tbl ALTER COLUMN status TYPE varchar(100) USING status::varchar(100);
-
-ALTER TYPE LOG.ADDITIONAL_DATA_REF ADD VALUE 'Transaction';
-DROP INDEX client.cardaccess_card_country_uq RESTRICT;
-CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl (clientid, cardid, countryid, psp_type) WHERE enabled='true';
-
-
 -- country calling code
 ALTER TABLE system.country_tbl ADD country_calling_code INTEGER NULL;
 update system.country_tbl set country_calling_code=297 where alpha3code='ABW';
