57e887df3056ed8f02890a5951e4d6bffbb751b0
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2018-10-08 19:29:21 +0530

    CMP-2609
    Chase Payment Acquirer Integration mPoint Scripts Configuration

--- db/master_pg_v2.03.sql
+++ db/master_pg_v2.03.sql
@@ -13,3 +13,31 @@ ALTER TABLE system.country_tbl ADD country_calling_code INTEGER NULL;
 
 -- URL where the customer may be redirected if txn fails.
 ALTER TABLE Log.Transaction_Tbl ADD declineurl VARCHAR(255);
+
+--== CONFIGURE Chase Payment Acquirer ==--
+
+-- PSP tbl config --
+INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (52, 'Chase Payment',2);
+-- END PSP tbl config --
+-- Currencys for Chase Payment --
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (840,52,'USD');
+-- END Currencys for Chase Payment --
+
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (1, 52, true);
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (5, 52, true);
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (7, 52, true);
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (8, 52, true);
+INSERT INTO system.pspcard_tbl (cardid, pspid, enabled) VALUES (22, 52, true);
+
+INSERT INTO client.cardaccess_tbl (clientid, cardid, pspid, countryid, stateid, enabled) VALUES (10007, 1, 52, 200, 1, true);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, pspid, countryid, stateid, enabled) VALUES (10007, 5, 52, 200, 1, true);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, pspid, countryid, stateid, enabled) VALUES (10007, 7, 52, 200, 1, true);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, pspid, countryid, stateid, enabled) VALUES (10007, 8, 52, 200, 1, true);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, pspid, countryid, stateid, enabled) VALUES (10007, 22, 52, 200, 1, true);
+
+-- Merchant MID configuration --
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10007, 52, 'nconline1', '', '');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 52, '-1');
+-- End Merchant MID configuration --
+
+--== END CONFIGURE Chase Payment Acquirer ==--
