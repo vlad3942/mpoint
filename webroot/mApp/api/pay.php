4ef978efbb1ab96b080aa889c3213699d1b75bd4
Sagar Narayane <sagar@cellpointmobile.com>
2018-10-26 11:21:29 +0530

    CMP-2636
    Refactor Authorize flow

--- webroot/mApp/api/pay.php
+++ webroot/mApp/api/pay.php
@@ -562,7 +562,7 @@ if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PH
 											break;
 										case (Constants::iGLOBAL_COLLECT_PSP):
 												$obj_PSP = new GlobalCollect($_OBJ_DB, $_OBJ_TXT, $oTI, $aHTTP_CONN_INFO["global-collect"]);
-												$obj_XML = $obj_PSP->initialize($obj_PSPConfig, $obj_TxnInfo->getAccountID(), General::xml2bool($obj_DOM->pay[$i]->transaction["store-card"]), $obj_DOM->pay[$i]->transaction->card["type-id"] );
+												$obj_XML = $obj_PSP->initialize($obj_PSPConfig, $obj_TxnInfo->getAccountID(), General::xml2bool($obj_DOM->pay[$i]->transaction["store-card"]), $obj_DOM->pay[$i]->transaction->card["type-id"], '',  $obj_DOM->{'pay'}[$i]->transaction->{'billing-address'}, $obj_DOM->{'pay'}[$i]->{'client-info'});
 											
 												foreach ($obj_XML->children() as $obj_Elem)
 												{
