98d754e5f920919a9759a1fa1f65ff51743a3627
Sagar Narayane <sagar@cellpointmobile.com>
2018-11-28 15:16:32 +0530

    CMP-2677
    mPoint onboarding scripts for OD in Production

--- db/snapshot/Production-PCI-2.07-OD-Onboarding.sql
+++ db/snapshot/Production-PCI-2.07-OD-Onboarding.sql
@@ -12,12 +12,13 @@ INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (702,48,'SGD
 INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (840,48,'USD');
 INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (764,48,'THB');
 
-
+--Currency
+UPDATE system.country_tbl SET alpha2code = 'MM', alpha3code = 'MMR', code = 104, currencyid = 104 WHERE id = 652;
 
 --Client Schema
 
 --Client
-INSERT INTO client.client_tbl (id, countryid, flowid, name, username, passwd, callbackurl, maxamount, lang, emailrcpt, method, terms, enabled, auto_capture, send_pspid, store_card, show_all_cards, max_cards, num_masked_digits, communicationchannels) VALUES (10018, 100, 1, 'Malindo Air', 'odMBE', 'nB4y8BFGfJe5jHXQ', 'http://od.mretail.cellpointmobile.net/mOrder/sys/mpoint.php', 947483647, 'gb', false, 'mPoint', null, true, false, true, 3, false, 20, 2, 5);
+INSERT INTO client.client_tbl (id, countryid, flowid, name, username, passwd, callbackurl, maxamount, lang, emailrcpt, method, terms, enabled, auto_capture, send_pspid, store_card, show_all_cards, max_cards, num_masked_digits, communicationchannels) VALUES (10018, 100, 1, 'Malindo Air', 'odMBE', '852pvRLCZthLgBNB', 'http://od.mretail.cellpointmobile.net/mOrder/sys/mpoint.php', 947483647, 'gb', false, 'mPoint', null, true, false, true, 3, false, 20, 2, 5);
 INSERT INTO client.account_tbl (id, clientid, name, mobile, enabled, markup) VALUES (100181, 10018, 'Malindo - App', null, true, 'app');
 
 --Static Route
@@ -42,6 +43,9 @@ INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, 
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 505, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 302, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 639, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 613, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 608, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 7, true, 28, 652, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 639, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 634, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 642, 1, null, false, 1);
@@ -54,6 +58,9 @@ INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, 
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 609, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 646, 1, null, false, 1);
 INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 200, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 613, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 608, 1, null, false, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, stateid, position, preferred, psp_type) VALUES (10018, 8, true, 28, 652, 1, null, false, 1);
 
 
 --countrycurrency
@@ -62,29 +69,34 @@ INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled
 INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled) VALUES (10018, 610, 840, true);
 INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled) VALUES (10018, 639, 840, true);
 INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled) VALUES (10018, 649, 840, true);
+INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled) VALUES (10018, 613, 840, true);
+INSERT INTO client.countrycurrency_tbl (clientid, countryid, currencyid, enabled) VALUES (10018, 652, 840, true);
 
 
 --merchant account
-
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 1, '<name>', true, '<username>', '<password>', null);
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 25, '<name>', true, '<username>', '<password>', null);
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 26, '<name>', true, '<username>', '<password>', null);
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 27, '<name>', true, '<username>', '<password>', null);
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 28, '<name>', true, '<username>', '<password>', null);
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, stored_card) VALUES (10018, 48, '<name>', true, '<username>', '<password>', null);
-
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10018, 36, 'mvault', '', '');
 
 --merchant sub account
-
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 1, '-1', true);
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 25, '-1', true);
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 26, '-1', true);
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 27, '-1', true);
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 28, '-1', true);
 INSERT INTO client.merchantsubaccount_tbl (accountid, pspid, name, enabled) VALUES (100181, 48, '-1', true);
-
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name, enabled) VALUES (100181, 36, '-1', true);
 
 --URL
-
 INSERT INTO client.url_tbl (urltypeid, clientid, url, enabled) VALUES (2, 10018, 'https://od.voyage.cellpointmobile.net/mpoint/mprofile/authenticate-user', true);
 INSERT INTO client.url_tbl (urltypeid, clientid, url, enabled) VALUES (4, 10018, 'https://od.velocity.cellpointmobile.net', true);
+
+
+--additional property
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('mvault', 'true', 10018, 'client');
+
+
