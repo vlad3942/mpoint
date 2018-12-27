cb2cce39642a8c7d7ba2390f906f4ff0911d80b1
Anna Lagad <anna.lagad@cellpointmobile.com>
2018-12-27 11:12:56 +0530

    CMP-2710: Revive comment done

--- db/snapshot/Production-PCI-2.07-SGA-Onboarding.sql
+++ db/snapshot/Production-PCI-2.07-SGA-Onboarding.sql
@@ -5,10 +5,18 @@ INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd)
 
 INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10021, 1, 'mVault', 'blank', 'blank');
 
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100220, 1, '-1');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100210, 1, '-1');
 
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100220, 36, '-1');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100210, 36, '-1');
 
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('mvault', 'true', 10021, 'client');
 
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid, stateid) values (10021,11, 1, 1);
+
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name, enabled) VALUES (784,36,'AED','true');
+
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name, enabled) VALUES (682,36,'SAR','true');
+
+INSERT INTO system.cardpricing_tbl ( pricepointid, cardid) VALUES ( -682, 35 );
+
+INSERT INTO system.cardpricing_tbl ( pricepointid, cardid) VALUES ( -784, 35 );
