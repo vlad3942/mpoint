4eb9699759cd6675870f1259414585c8f5d618bb
Urmila S <urmila.s@cellpointmobile.com>
2018-12-27 09:32:10 +0530

    CMP-2720: SQL for notification secret by currency for DC

--- db/setup_pg_v2.03.sql
+++ db/setup_pg_v2.03.sql
@@ -604,10 +604,14 @@ INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (10000
 --== END CONFIGURE Chase Payment Acquirer ==--
 
 --Datacash Start --
---Edit notification secret value column.
+--Edit notification secret value column for AED.
 -- Edit client id if required.
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('Notification-Secret.AED', '<notification-secret-from-SABBorDatacash>', (SELECT id FROM client.merchantaccount_tbl t WHERE clientid=10021 and pspid=17), 'merchant');
 
 --Existing entry with key 'Notification-Secret' should be updated as for SAR currency.
 UPDATE client.additionalproperty_tbl set  key='Notification-Secret.SAR' where key='Notification-Secret';
+
+--Generic sql for any new currency added for DC
+--Edit the currency code (3 char alpha code), notification secret value and client id.
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('Notification-Secret.<alpha3code>', '<notification-secret-from-SABBorDatacash>', (SELECT id FROM client.merchantaccount_tbl t WHERE clientid=10021 and pspid=17), 'merchant');
 --Datacash END --
--- db/snapshot/18-24-SITL-Build.sql
+++ db/snapshot/18-24-SITL-Build.sql
@@ -1,8 +1,13 @@
 --Datacash Start --
---Edit notification secret value column.
+--Edit notification secret value column for AED currency.
 -- Edit client id if required.
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('Notification-Secret.AED', '<notification-secret-from-SABBorDatacash>', (SELECT id FROM client.merchantaccount_tbl t WHERE clientid=10021 and pspid=17), 'merchant');
 
 --Existing entry with key 'Notification-Secret' should be updated as for SAR currency.
 UPDATE client.additionalproperty_tbl set  key='Notification-Secret.SAR' where key='Notification-Secret';
+
+--Generic sql for any new currency added for DC
+--Edit the currency code (3 char alpha code), notification secret value and client id.
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('Notification-Secret.<alpha3code>', '<notification-secret-from-SABBorDatacash>', (SELECT id FROM client.merchantaccount_tbl t WHERE clientid=10021 and pspid=17), 'merchant');
+
 --Datacash END --
