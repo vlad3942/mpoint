77088a6d846edd5dafb478437e344cfb6f504f42
Urmila S <urmila.s@cellpointmobile.com>
2019-01-14 16:26:08 +0530

    CMP-2757: Adyen sql script, todo consolidate

--- db/setup_pg_v2.03.sql
+++ db/setup_pg_v2.03.sql
@@ -643,3 +643,11 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 
 -- End Merchant MID configuration --
 
 --== END CONFIGURE Chase Payment Acquirer ==--
+
+--Setup this additional property if 3DS is to be requested with every request to Adyen, the rules configured in Adyen will override
+--Do not add this if 3DS is not required
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'MANUALTHREED', 'true', id, 'merchant' from client.merchantaccount_tbl where clientid=<clientid> and pspid=12;
+
+--Setup this additional property if 3DS is to be requested to Adyen based on dynamic rules configured.
+--Do not add this if 3DS is not required
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'DYNAMICTHREED', 'true', id, 'merchant' from client.merchantaccount_tbl where clientid=<clientid> and pspid=12;
