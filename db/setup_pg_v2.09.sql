8b0ea4c7361c66ec51a4e41486002e4c6889e01a
Urmila S <urmila.s@cellpointmobile.com>
2019-02-06 18:50:23 +0530

    CMP-2801: Installment feature db scripts

--- db/setup_pg_v2.09.sql
+++ db/setup_pg_v2.09.sql
@@ -6,3 +6,6 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 
 --Setup this additional property if 3DS is to be requested to Adyen based on dynamic rules configured.
 --Do not add this if 3DS is not required
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'DYNAMICTHREED', 'true', id, 'merchant' from client.merchantaccount_tbl where clientid=<clientid> and pspid=12;
+
+--enable Offline Installment option for a PSP - Adyen
+UPDATE system.psp_tbl SET installment = 1 WHERE id = 12;
