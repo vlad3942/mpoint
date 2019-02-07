f1b9c54a5c451db4643575975b5abe8e0f394a1a
Rohit Malhotra <rohit@cellpointmobile.com>
2019-02-07 17:02:15 +0530

    CMESB-3488 Invalid Currency issue for RGN so checkout is failing

--- db/snapshot/19-02-SITL-Build.sql
+++ db/snapshot/19-02-SITL-Build.sql
@@ -8,3 +8,9 @@ update client.client_tbl cl set maxamount=947483647 where maxamount=-1 and id=10
 UPDATE System.Country_Tbl set id = 653, currencyid = 104, alpha2code = 'MM', alpha3code = 'MMR', code = 104 WHERE id = 653;
 --Inserting the entry for Malindo to work with Myanmar with USD currency.
 INSERT INTO client.countrycurrency_tbl(clientid, countryid, currencyid) VALUES (10018,653,840);
+
+DELETE FROM client.countrycurrency_tbl WHERE clientid = 10018 AND countryid = 653 AND currencyid = 840;
+
+INSERT INTO client.countrycurrency_tbl(clientid, countryid, currencyid, enabled) VALUES (10018,653,840, true);
+
+ALTER TABLE client.countrycurrency_tbl ALTER COLUMN enabled SET DEFAULT true;
