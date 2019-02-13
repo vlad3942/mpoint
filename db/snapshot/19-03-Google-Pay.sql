0326011cd3bce4c21a2011fdf3345a4bc6d738c6
Urmila S <urmila.s@cellpointmobile.com>
2019-02-13 17:26:03 +0530

    CMP-2812: consolidated gpay sql scripts

--- db/snapshot/19-03-Google-Pay.sql
+++ db/snapshot/19-03-Google-Pay.sql
@@ -1,12 +1,3 @@
---Setup this additional property if 3DS is to be requested with every request to Adyen, the rules configured in Adyen will override
---Do not add this if 3DS is not required
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'MANUALTHREED', 'true', id, 'merchant' from client.merchantaccount_tbl where clientid=<clientid> and pspid=12;
-
---Setup this additional property if 3DS is to be requested to Adyen based on dynamic rules configured.
---Do not add this if 3DS is not required
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'DYNAMICTHREED', 'true', id, 'merchant' from client.merchantaccount_tbl where clientid=<clientid> and pspid=12;
-
-
 /* ========== Consolidated script for CONFIGURING GOOGLE PAY - START ========== */
 INSERT INTO System.Card_Tbl (id, name, position, minlength, maxlength, cvclength,paymenttype) VALUES (41, 'Google Pay', 19, -1, -1, -1,3);
 INSERT INTO System.CardPrefix_Tbl (cardid, min, max) VALUES (41, -1, -1);
