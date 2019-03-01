802bbe45fd4da0820d4becd2cf9c68888a5809be
Urmila S <urmila.s@cellpointmobile.com>
2019-03-01 14:38:37 +0530

    CMP-2836: Based on online IIN range Diners overlaps Discover range.

--- db/snapshot/19-05-SITL-Build.sql
+++ db/snapshot/19-05-SITL-Build.sql
@@ -1,9 +1,3 @@
---ODMBE-5192
-update client.client_tbl cl set maxamount=947483647 where maxamount=-1 and id=100018;
-
---SGAMBE-4207
-UPDATE client.client_tbl set smsrcpt=false where id=10021;
-
 --CMP-2836
 UPDATE system.cardprefix_tbl SET enabled=false where cardid=3 and min = 60110000 and max = 60110999;
 UPDATE system.cardprefix_tbl SET enabled=false where cardid=3 and min = 60112000 and max = 60114999;
