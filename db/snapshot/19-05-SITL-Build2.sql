a7d2a74b7324f08615aa527ceb543a0df0c72961
arvind <arvind@cellpointmobile.com>
2019-03-12 18:55:46 +0530

    Code review comments

--- db/snapshot/19-05-SITL-Build2.sql
+++ db/snapshot/19-05-SITL-Build2.sql
@@ -1,2 +1,2 @@
 -- Set HPP Payment complete method Per client
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled) VALUES ('paymentCompleteMethod', 'POST', <clientId>, 'client', true);
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled) VALUES ('hppFormRedirect', 'POST', <clientId>, 'client', true);
