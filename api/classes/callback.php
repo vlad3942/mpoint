29d142e572a15e87b2982b5ad20fe78071511770
Sagar Narayane <sagar@cellpointmobile.com>
2019-04-02 16:06:47 +0530

    CMP-2897
    Enhance mPoint to support Cielo

--- api/classes/callback.php
+++ api/classes/callback.php
@@ -825,6 +825,8 @@ abstract class Callback extends EndUserAccount
                 return new Chase($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["chase"]);
         case (Constants::iPAYU_PSP):
                 return new PayU($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["payu"]);
+		case (Constants::iCielo_ACQUIRER):
+                return new PayU($obj_DB, $obj_Txt, $obj_TxnInfo, $aConnInfo["cielo"]);
         default:
 			throw new CallbackException("Unkown Payment Service Provider: ". $obj_TxnInfo->getPSPID() ." for transaction: ". $obj_TxnInfo->getID(), 1001);
 		}
