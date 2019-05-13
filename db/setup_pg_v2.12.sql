5a776c9724318a3665cba50986d5be9a6a89ea01
Nitin Gaikwad <nitin@cellpointmobile.com>
2019-05-13 16:59:35 +0530

    Reverse commit

--- db/setup_pg_v2.12.sql
+++ db/setup_pg_v2.12.sql
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 -- Get status Additional properties 
 -- Retry count value should be integer 
 insert into client.additionalproperty_tbl (key,value,enabled,externalid,type,scope) values ('status_retry_count',<value>,true,<clientid>,'client',2);
 -- Retry count value should be integer and it will be seconds 
 insert into client.additionalproperty_tbl (key,value,enabled,externalid,type,scope) values ('status_retry_interval',<value>,true,<clientid>,'client',2);
+=======
+>>>>>>> parent of f01a61f... CMP-2968
