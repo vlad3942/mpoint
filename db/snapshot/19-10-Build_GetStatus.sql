f01a61f23e0e6c2afffc2444afe37b36ce1a68b2
Nitin Gaikwad <nitin@cellpointmobile.com>
2019-05-13 16:45:55 +0530

    CMP-2968 - Add additional property for the get status API changes - User need to replace the value and the client details

---
+++ db/snapshot/19-10-Build_GetStatus.sql
@@ -0,0 +1,3 @@
+-- Get status Additional properties 
+insert into client.additionalproperty_tbl (key,value,enabled,externalid,type,scope) values ('status_retry_count',3,true,<clientid>,'client',2);
+insert into client.additionalproperty_tbl (key,value,enabled,externalid,type,scope) values ('status_retry_interval',10,true,<clientid>,'client',2);
