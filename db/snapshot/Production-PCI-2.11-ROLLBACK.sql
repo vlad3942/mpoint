1271b07e9d42c535ce10ae677150a751cfaff7c7
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-05-14 15:57:37 +0530

    CPMRM-4196: Rollbck script

--- db/snapshot/Production-PCI-2.11-ROLLBACK.sql
+++ db/snapshot/Production-PCI-2.11-ROLLBACK.sql
@@ -2,8 +2,7 @@
 -- 2c2p JCB card - ROLLBACK
 
 DELETE FROM client.additionalproperty_tbl 
-WHERE key = 'MID.5'
-AND  value = <MID> 
+WHERE value = <MID> 
 AND externalid = (SELECT ID FROM client.merchantaccount_tbl WHERE clientid = <client id> and pspid = <pspid>)
 AND type = 'merchant';
 
