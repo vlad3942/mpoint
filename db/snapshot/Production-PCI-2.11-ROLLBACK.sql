768059acb4a59973b2b8a52dabb6bde092b4deea
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-05-14 17:41:01 +0530

    CPMRM-4196: Rollback script

--- db/snapshot/Production-PCI-2.11-ROLLBACK.sql
+++ db/snapshot/Production-PCI-2.11-ROLLBACK.sql
@@ -2,8 +2,20 @@
 -- 2c2p JCB card - ROLLBACK
 
 DELETE FROM client.additionalproperty_tbl 
-WHERE value = <MID> 
+WHERE key = 'MID.5'
+AND value = <MID> 
 AND externalid = (SELECT ID FROM client.merchantaccount_tbl WHERE clientid = <client id> and pspid = <pspid>)
+AND type = 'merchant';
+
+
+
+-- MID selection based on card id - Rollback
+
+DELETE FROM client.additionalproperty_tbl 
+WHERE key = 'mechantaccountrule'
+AND value = 'merchantaccount ::= (property[@name=''<midpath>''])
+midpath ::= "MID."(@card-id)'
+AND externalid = (SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10007 and pspid = 18)
 AND type = 'merchant';
 
 
