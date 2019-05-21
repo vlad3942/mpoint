b787e0394af439d007d4ef488dfe2a513670c0ed
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2019-05-21 22:22:26 +0530

    CMP-2979
    [VISION] Dashboard query optimization

--- api/classes/mConsole.php
+++ api/classes/mConsole.php
@@ -1494,7 +1494,7 @@ class mConsole extends Admin
                     $aFiltersClauses[] = " AND T.created <= '". $this->getDBConn()->escStr(date("Y-m-d H:i:s", strtotime($value)))."' ".$sAtTimeZone." ";
                     break;
                 case 'state':
-                    $aFiltersClauses[] = " AND M.STATEID in (".implode(",", $value)."))";
+                    $aFiltersClauses[] = " AND M.STATEID in (".implode(",", $value).")";
                     // Sub query for getting latest state only
                     break;
 				case 'cardid':
