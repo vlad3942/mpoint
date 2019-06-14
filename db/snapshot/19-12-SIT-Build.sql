65fb00ab55ca6a2f59b3e7de27fc902e158eaedf
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-06-14 11:19:01 +0530

    PR-4638: PR: AID changes for PAL for paypal

--- db/snapshot/19-12-SIT-Build.sql
+++ db/snapshot/19-12-SIT-Build.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
-    SELECT 'PAYPAL_BILLING_AGREEMENT', 'This is billing agreement with CellPoint Mobile', id, 'merchant', 2 FROM client.merchantaccount_tbl WHERE pspid=24
+    SELECT 'PAYPAL_BILLING_AGREEMENT', 'This is billing agreement with CellPoint Mobile', id, 'merchant', 2 FROM client.merchantaccount_tbl WHERE pspid=24 AND clientid = <client id>
