13996979db7838005bf9730a3f46518464c187d8
Urmila S <urmila.s@cellpointmobile.com>
2019-06-18 18:25:07 +0530

    CMP-3039: added get payment summary config dependency required for wallet cvv flow.

--- db/snapshot/19-12-SIT-Build.sql
+++ db/snapshot/19-12-SIT-Build.sql
@@ -5,3 +5,9 @@ INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
 --CMP-3015 default value set to empty
 alter table client.merchantaccount_tbl alter column username set default 'empty';
 alter table client.merchantaccount_tbl alter column passwd set default 'empty';
+
+--SIT PR enable this endpoint for wallet CVV flow
+INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select 39, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
+
+--Prod SQL PR enable this endpoint for wallet CVV flow
+INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select <groupid>, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
