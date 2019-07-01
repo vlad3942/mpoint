529c0817488353c6789a22082befb0ba4bdeccbc
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-07-01 12:40:06 +0530

    CMP-3064: Production-PCI : Consolidates SQL Script

---
+++ db/snapshot/Production-PCI-2.14.sql
@@ -0,0 +1,23 @@
+
+
+-- Hpp Iframe flag
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled) VALUES ('isEmbeddedHpp', 'true', <clientid>, 'client', true);
+
+
+INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
+    SELECT 'PAYPAL_BILLING_AGREEMENT', 'This is billing agreement with CellPoint Mobile', id, 'merchant', 2 FROM client.merchantaccount_tbl WHERE pspid=24 AND clientid = 10020;
+
+--CMP-3015 default value set to empty
+alter table client.merchantaccount_tbl alter column username set default 'empty';
+alter table client.merchantaccount_tbl alter column passwd set default 'empty';
+
+--SIT PR enable this endpoint for wallet CVV flow
+INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select 39, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
+
+--Prod SQL PR enable this endpoint for wallet CVV flow
+INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select <groupid>, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
+
+
+-- UATP
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('SETTLEMENT_BATCH_LIMIT', '20', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10069 and pspid = 52), 'merchant');
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('SETTLEMENT_BATCH_LIMIT', '20', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10069 and pspid = 50), 'merchant');
