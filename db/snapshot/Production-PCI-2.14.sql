b00d83a5d833fe5208efcb584c2dfd852e81c158
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-07-01 12:59:27 +0530

    CMP-3064: Production-PCI : Consolidates SQL Script

--- db/snapshot/Production-PCI-2.14.sql
+++ db/snapshot/Production-PCI-2.14.sql
@@ -11,13 +11,6 @@ INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
 alter table client.merchantaccount_tbl alter column username set default 'empty';
 alter table client.merchantaccount_tbl alter column passwd set default 'empty';
 
---SIT PR enable this endpoint for wallet CVV flow
-INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select 39, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
-
---Prod SQL PR enable this endpoint for wallet CVV flow
-INSERT INTO client.endpointaccess_tbl (groupid, endpointid) select <groupid>, id from system.endpoint_tbl where path='mpoint/get-payment-summary';
-
-
 -- UATP
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('SETTLEMENT_BATCH_LIMIT', '20', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10069 and pspid = 52), 'merchant');
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) VALUES ('SETTLEMENT_BATCH_LIMIT', '20', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = 10069 and pspid = 50), 'merchant');
