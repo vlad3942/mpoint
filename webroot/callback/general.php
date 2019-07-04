bb50c2cd45aeccf1706cc0d74f06ff3f1c12beaa
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-07-04 12:06:38 +0530

    CMP-3069: Payments are getting declined after captured state

--- webroot/callback/general.php
+++ webroot/callback/general.php
@@ -391,7 +391,7 @@ try
         {
             $responseCode = $obj_mPoint->capture($obj_TxnInfo->getAmount());
 
-            if ($responseCode == 1000 || $responseCode == 1002)
+            if ($responseCode == 1000 || $responseCode == Constants::iPAYMENT_CAPTURED_AND_CALLBACK_SENT)
             {
                 array_push($aStateId,Constants::iPAYMENT_CAPTURED_STATE);
                 //$obj_mPoint->newMessage($obj_TxnInfo->getID(), Constants::iPAYMENT_CAPTURED_STATE, "");
