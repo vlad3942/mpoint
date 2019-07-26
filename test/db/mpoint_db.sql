b3fb1a3e21f2f7da99a32f29d3eec201cb8f7502
Sagar Narayane <sagar@cellpointmobile.com>
2019-07-26 13:15:24 +0530

    CMP-3091
    Support multiple capture, refund and cancel operation

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -8043,6 +8043,7 @@ INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (5012, 'Cance
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (5013, 'Refund Operation Requested', 'Passbook', null, true);
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (5011, 'Capture Operation Requested', 'Passbook', null, true);
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (5014, 'Initialize Operation Requested', 'Passbook', null, true);
+INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (5015, 'Void Operation Requested', 'Passbook', null, true);
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (6100, 'Invalid Passbook Operation', 'Passbook', null, true);
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (6200, 'Operation Not Allowed ', 'Passbook', null, true);
 INSERT INTO log.state_tbl (id, name, module, func, enabled) VALUES (6201, 'Amount is Higher', 'Passbook', null, true);
