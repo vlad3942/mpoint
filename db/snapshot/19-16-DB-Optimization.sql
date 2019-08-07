48d59e5116732de8ba3c6d5e10b9c5994c536fdc
Urmila S <urmila.s@cellpointmobile.com>
2019-08-07 17:22:44 +0530

    CMP-3129: added index for log.order_tbl

--- db/snapshot/19-16-DB-Optimization.sql
+++ db/snapshot/19-16-DB-Optimization.sql
@@ -1,2 +1,5 @@
 --CMP-3128
 CREATE INDEX CONCURRENTLY additional_data_tbl_externalid_type_index on log.additional_data_tbl (externalid, type);
+
+--CMP-3129
+CREATE INDEX CONCURRENTLY order_tbl_txnid_index on log.order_tbl (txnid);
