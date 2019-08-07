e1a51a4abfc73d0be9f533d196f1a529fb3d0e66
Urmila S <urmila.s@cellpointmobile.com>
2019-08-07 18:21:30 +0530

    CMP-3131: added index for log.txn_tbl

--- db/snapshot/19-16-DB-Optimization.sql
+++ db/snapshot/19-16-DB-Optimization.sql
@@ -6,3 +6,6 @@ CREATE INDEX CONCURRENTLY order_tbl_txnid_index on log.order_tbl (txnid);
 
 --CMP-3130
 CREATE INDEX CONCURRENTLY flight_tbl_orderid_index on log.flight_tbl (order_id);
+
+--CMP-3131
+CREATE INDEX CONCURRENTLY transaction_tbl_clientid_pspid_index on log.transaction_tbl (clientid,pspid);
