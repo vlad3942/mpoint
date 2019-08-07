ab5f0556f104b7a0d061bbe9bcc56d2aea3cc5ce
Urmila S <urmila.s@cellpointmobile.com>
2019-08-07 17:12:37 +0530

    CMP-3128: updated index for log.additional_data tbl

--- db/master_pg_v2.16.sql
+++ db/master_pg_v2.16.sql
@@ -1,3 +1,2 @@
 --CMP-3128
-create index additional_data_tbl_externalid_type_index
-    on log.additional_data_tbl (externalid, type);
+CREATE INDEX CONCURRENTLY additional_data_tbl_externalid_type_index on log.additional_data_tbl (externalid, type);
