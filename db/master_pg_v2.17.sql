68f718021794610c0baa28725e547b0a54cb9760
Urmila S <urmila.s@cellpointmobile.com>
2019-09-11 23:28:09 +0530

    CMP-3178: updated profile id to bigint type

--- db/master_pg_v2.17.sql
+++ db/master_pg_v2.17.sql
@@ -1,4 +1,4 @@
 --CMP-3128
-ALTER TABLE log.transaction_tbl ADD profileid varchar(50) NULL;
+ALTER TABLE log.transaction_tbl ADD profileid int8 NULL;
 
 comment on column log.transaction_tbl.profileid is 'mProfile id associated with the txn';
