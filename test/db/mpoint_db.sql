b9ab2202db911b8f5638effd18a513335c0f0651
Rohit Malhotra <rohit@cellpointmobile.com>
2019-10-03 12:14:19 +0530

    CMP-3161 : Make OrderData configurable for every merchant

--- test/db/mpoint_db.sql
+++ test/db/mpoint_db.sql
@@ -8135,3 +8135,36 @@ Possible values % (constants)
 5 % 5		= Cancel
 3 % 3		= Refund
 2 % 2		= Capture';
+
+/* Ticket level transaction - Add new column fees in log.order_tbl */
+ALTER TABLE Log.order_tbl ADD COLUMN fees integer DEFAULT 0;
+
+-- Create new table system.businesstype_tbl to store businesstype for each client
+CREATE TABLE system.businesstype_tbl
+(
+  id serial NOT NULL,
+  name character varying(50),
+  enabled boolean DEFAULT true,
+  CONSTRAINT businesstype_pk PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE system.businesstype_tbl OWNER TO postgres;
+
+-- Insert business type details
+insert into system.businesstype_tbl (id,name) values
+(0,'None'),
+(1,'Non-Industry-Specific'),
+(2,'Airline Industry'),
+(3,'Auto Rental Industry'),
+(4,'Cruise Industry'),
+(5,'Hospitality Industry'),
+(6,'Entertainment/Ticketing Industry'),
+(7,'e-commerce Industry');
+
+-- Added new column and foreign key constraint
+ALTER TABLE client.account_tbl
+	ADD COLUMN businessType integer DEFAULT 0,
+	ADD CONSTRAINT businessType_pk FOREIGN KEY (businessType) REFERENCES system.businesstype_tbl (id);
