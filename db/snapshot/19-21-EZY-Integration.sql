16a350e717c8759b839b45357ac26ee8e1db9c9c
Anna Lagad <anna.lagad@cellpointmobile.com>
2019-11-06 15:13:35 +0530

    CPMRM-4310: Resolved PSP ID conflict

--- db/snapshot/19-21-EZY-Integration.sql
+++ db/snapshot/19-21-EZY-Integration.sql
@@ -3,15 +3,15 @@ INSERT INTO system.processortype_tbl (id, name) VALUES (9, 'Fraud Gateway');
 /*======= END NEW PROCESSOR TYPE FOR TOKENIZATION SYSTEM ======== */
 
 /* ========== CONFIGURE EZY START========== */
-INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (58, 'EZY Fraud Gateway',9);
-INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (15, 58); /*With Apple-Pay*/
+INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (60, 'EZY Fraud Gateway',9);
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (15, 60); /*With Apple-Pay*/
 
-INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name) VALUES (10007, 58, 'EZY');
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100007, 58, '-1');
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name) VALUES (<clientid>, 58, 'EZY');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (<accountid>, 58, '-1');
 
-INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (208,58,'DKK');
-INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (840,58,'USD');
-INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (826,58,'GBP');
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (208,60,'DKK');
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (840,60,'USD');
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (826,60,'GBP');
 
 /*=================== Adding new states for Fraud Check : START =======================*/
 INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2040 , 'Fraud Check Passed', 'Authorization', true);
@@ -20,6 +20,6 @@ INSERT INTO log.state_tbl (id, name, module, enabled) VALUES (2041 , 'Fraud Chec
 
 
 /*=================== Create a new static route for Fraud check : START =======================*/
-INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, psp_type, stateid) VALUES (10007, 15, true, 58, 200, 9, 1);
+INSERT INTO client.cardaccess_tbl (clientid, cardid, enabled, pspid, countryid, psp_type, stateid) VALUES (<clientid>, 15, true, 60, 200, 9, 1);
 /*=================== Create a new static route for Fraud check : END =======================*/
 
