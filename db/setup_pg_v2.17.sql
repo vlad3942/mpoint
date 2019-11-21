2163ee21e608a9fb801c3fe51950f84d1547d13c
Sagar Narayane <sagar@cellpointmobile.com>
2019-11-21 16:50:35 +0530

    CMP-3294
    Add index on newly added tables/columns

--- db/setup_pg_v2.17.sql
+++ db/setup_pg_v2.17.sql
@@ -46,26 +46,6 @@ VALUES ('ENABLE_PROFILE_ANONYMIZATION', <value>, <client-id>, 'client', 0 );
 INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
 VALUES ('ENABLE_PROFILE_ANONYMIZATION', 'true', 10018, 'client', 0 );
 
-
-/* Ticket level transaction - Add new column fees in log.order_tbl */
-ALTER TABLE Log.order_tbl ADD COLUMN fees integer DEFAULT 0;
-
--- Create new table system.businesstype_tbl to store businesstype for each client
-CREATE TABLE system.businesstype_tbl
-(
-  id serial NOT NULL,
-  name character varying(50),
-  enabled boolean DEFAULT true,
-  CONSTRAINT businesstype_pk PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
-);
-
-ALTER TABLE system.businesstype_tbl OWNER TO mpoint;
-GRANT ALL ON TABLE system.businesstype_tbl TO mpoint;
-GRANT ALL ON TABLE system.businesstype_tbl TO jona;
-
 -- Insert business type details
 insert into system.businesstype_tbl (id,name) values
 (0,'None'),
@@ -77,22 +57,14 @@ insert into system.businesstype_tbl (id,name) values
 (6,'Entertainment/Ticketing Industry'),
 (7,'e-commerce Industry');
 
--- Added new column and foreign key constraint
-ALTER TABLE client.account_tbl 
-	ADD COLUMN businessType integer DEFAULT 0,
-	ADD CONSTRAINT businessType_pk FOREIGN KEY (businessType) REFERENCES system.businesstype_tbl (id);
-
--- Set businesstype 2 for UATP client
-update client.account_tbl set businesstype = 2 where clientid = 10069;
-
 -- Insert New transactions states into log.state_tbl
 insert into log.state_tbl (id,name, module,func) values
 (2010101,'Failed during Capture','Payment','Capture'),
 (2010201,'Failed during Cancel','Payment','Cancel'),
 (2010301,'Failed during Refund','Payment','Refund');
 
-CREATE INDEX account_tbl_businessType_index ON client.account_tbl (businessType);
-CREATE INDEX order_tbl_orderref_index ON Log.Order_Tbl (orderref);
+-- Set businesstype 2 for UATP client
+update client.account_tbl set businesstype = 2 where clientid = 10069;
 
 --Sandbox token for OD - DEV , SIT
 INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope )
