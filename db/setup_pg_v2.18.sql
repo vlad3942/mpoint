87613a2f2a824b556cf58187d2590b179ac010b2
Abhinav <abhinav.shaha@cellpointmobile.com>
2019-12-12 18:57:02 +0530

    CMP-3000: Added db queries to v2.19 SIT setup

--- db/setup_pg_v2.18.sql
+++ db/setup_pg_v2.18.sql
@@ -189,13 +189,3 @@ UPDATE client.additionalproperty_tbl SET VALUES = 'PAN_ONLY' WHERE  key = 'ALLOW
 
 INSERT INTO client.additionalproperty_tbl (key,value,enabled,externalid,type,scope)
 SELECT 'ALLOWEDPAYMENTMETHODS','PAN_ONLY',true, id, 'merchant', 1 FROM client.merchantaccount_tbl WHERE clientid=<clientid> AND pspid=44;
-
-/* Update callback url for UATP client Ref Jira : CMP-3000
- * Replace host url as per env while running the query
- * DevPune - http://mpoint.dev2.cellpointmobile.com/uatp/callback.php
- * SITLondon - http://mpoint.sit.cellpointmobile.com/uatp/callback.php
- * SITPune - http://mpoint.sit2.cellpointmobile.com/uatp/callback.php
- * UATAWS - http://mpoint.uat-01.cellpointmobile.net/uatp/callback.php
- * PROD - http://mpoint.cellpointmobile.net/uatp/callback.php 
-*/
-update client.client_tbl set callbackurl = 'http://mpoint.sit2.cellpointmobile.com/uatp/callback.php' where id = 10069;
