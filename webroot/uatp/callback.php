e473645bd8a2daca58f5f885bd2ee3d7e5eb16d1
Abhinav <abhinav.shaha@cellpointmobile.com>
2019-12-12 16:09:46 +0530

    CMP-3000 : [UATP - Settlement] 4030 Session Callback: 4030 Callback gets failed for UATP transaction : Added missing code

--- webroot/uatp/callback.php
+++ webroot/uatp/callback.php
@@ -26,94 +26,107 @@ require_once(sAPI_CLASS_PATH ."simpledom.php");
 // Require specific Business logic for the UATP component
 require_once(sCLASS_PATH . "/uatp_card_account.php");
 
-
-
 set_time_limit(600);
+
 // Standard retry strategy connecting to the database has proven inadequate
 $i = 0;
 while ( ($_OBJ_DB instanceof RDB) === false && $i < 5)
 {
-    // Instantiate connection to the Database
-    $_OBJ_DB = RDB::produceDatabase($aDB_CONN_INFO["mpoint"]);
-    $i++;
+	// Instantiate connection to the Database
+	$_OBJ_DB = RDB::produceDatabase($aDB_CONN_INFO["mpoint"]);
+	$i++;
 }
-$obj_XML = simplexml_load_string(file_get_contents("php://input") );
 
-
-$id = (integer)$obj_XML->callback->transaction["id"];
-$xml = '';
+$id = $_REQUEST['mpoint-id'];
+$orderno  = $_REQUEST['orderid'];
+$currency = $_REQUEST['currency'];
+$amount = $_REQUEST['amount'];
+$pspid = 50;
+$ticketNumbers = $_REQUEST['tickernumbers'];
+$status = $_REQUEST['status'];
 
 $aStateId = array();
 
-try
+//Suppress 4030,2010,2000 callback as a part of CMP-3052,CMP-3000
+if($status != Constants::iSESSION_COMPLETED && $status != Constants::iPAYMENT_REJECTED_STATE && $status != Constants::iPAYMENT_ACCEPTED_STATE)
 {
-    $obj_TxnInfo = TxnInfo::produceInfo($id, $_OBJ_DB);
-    $_OBJ_TXT = new TranslateText(array(sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/global.txt", sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/custom.txt"), sSYSTEM_PATH, 0, "UTF-8");
-    $obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $obj_TxnInfo->getClientConfig()->getID(), $obj_TxnInfo->getClientConfig()->getAccountConfig()->getID(), intval($obj_XML->callback->{"psp-config"}["id"]) );
-
-    $iStateID = (integer) $obj_XML->callback->status["code"];
-    $isTicketLevelSettlement = $obj_PSPConfig->getAdditionalProperties(Constants::iInternalProperty,'IS_TICKET_LEVEL_SETTLEMENT');
-    $aTicketNumbers = [];
-    if($isTicketLevelSettlement === 'true')
-    {
-        $sAdditionalData = (string)$obj_XML->callback->{'additional-data'};
-        $aAdditionalData = [];
-        if($sAdditionalData !== '') {
-            parse_str($sAdditionalData, $aAdditionalData);
-            $aTicketNumbersStr = array_filter(explode(',', $aAdditionalData['tickernumbers']));
-            foreach ($aTicketNumbersStr as $ticketNumbersStr)
-            {
-                $temp = explode(':', $ticketNumbersStr);
-                $aTicketNumbers[$temp[0]] = $temp[1];
-            }
-
-        }
-    }
-    $obj_TxnInfo->produceOrderConfig($_OBJ_DB, $aTicketNumbers);
-    $aMessages = $obj_TxnInfo->getMessageHistory($_OBJ_DB);
-    $createdtimestamp = null;
-    foreach ($aMessages as $m) {
-        $iMessageID = (integer)$m["id"];
-        $iStateId = (integer)$m["stateid"];
-        if($iStateId === $iStateID)
-        {
-            $createdtimestamp = $m["created"];
-            break;
-        }
-    }
-
-    $obj_UATP = Callback::producePSP($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO, $obj_PSPConfig);
-    $code = $obj_UATP->initCallback($obj_PSPConfig, $obj_TxnInfo, $iStateID, '',$obj_TxnInfo->getCardID(),$createdtimestamp);
-
-    if($code === 1000)
-    {
-        header("HTTP/1.1 200 Ok");
-        header("Content-Type: text/xml; charset=\"UTF-8\"");
-        echo '<?xml version="1.0" encoding="UTF-8"?>';
-        echo '<root>';
-        echo '<status code="1000">Callback Success</status>';
-        echo '</root>';
-    }
-    else
-    {
-        header("HTTP/1.1 400 Bad Request");
-        header("Content-Type: text/xml; charset=\"UTF-8\"");
-        echo '<?xml version="1.0" encoding="UTF-8"?>';
-        echo '<root>';
-        echo '<status code="'.$code.'">Callback Failed</status>';
-        echo '</root>';
-    }
+	try
+	{
+		$obj_TxnInfo = TxnInfo::produceInfo($id, $_OBJ_DB);
+		$_OBJ_TXT = new TranslateText(array(sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/global.txt", sLANGUAGE_PATH . $obj_TxnInfo->getLanguage() ."/custom.txt"), sSYSTEM_PATH, 0, "UTF-8");
+		$obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $obj_TxnInfo->getClientConfig()->getID(), $obj_TxnInfo->getClientConfig()->getAccountConfig()->getID(), $pspid);
+		
+		$iStateID = (integer) $status;
+		$isTicketLevelSettlement = $obj_PSPConfig->getAdditionalProperties(Constants::iInternalProperty,'IS_TICKET_LEVEL_SETTLEMENT');
+		
+		$aTicketNumbers = [];
+		if($isTicketLevelSettlement === 'true')
+		{
+			$sAdditionalData = $ticketNumbers;
+			if($sAdditionalData !== ''){
+				$aTicketNumbersStr = array_filter(explode(',', $sAdditionalData));
+				foreach ($aTicketNumbersStr as $ticketNumbersStr)
+				{
+					$temp = explode(':', $ticketNumbersStr);
+					$aTicketNumbers[$temp[0]] = $temp[1];
+				}
+			}
+		}
+		
+		$obj_TxnInfo->produceOrderConfig($_OBJ_DB, $aTicketNumbers);
+		$aMessages = $obj_TxnInfo->getMessageHistory($_OBJ_DB);
+		$createdtimestamp = null;
+		foreach ($aMessages as $m) {
+			$iMessageID = (integer)$m["id"];
+			$iStateId = (integer)$m["stateid"];
+			if($iStateId === $iStateID)
+			{
+				$createdtimestamp = $m["created"];
+				break;
+			}
+		}
+		
+		$obj_UATP = Callback::producePSP($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $aHTTP_CONN_INFO, $obj_PSPConfig);
+		$code = $obj_UATP->initCallback($obj_PSPConfig, $obj_TxnInfo, $iStateID, '',$obj_TxnInfo->getCardID(),$createdtimestamp);
+		
+		if($code === 1000)
+		{
+			header("HTTP/1.1 200 Ok");
+			header("Content-Type: text/xml; charset=\"UTF-8\"");
+			echo '<?xml version="1.0" encoding="UTF-8"?>';
+			echo '<root>';
+			echo '<status code="1000">Callback Success</status>';
+			echo '</root>';
+		}
+		else
+		{
+			header("HTTP/1.1 400 Bad Request");
+			header("Content-Type: text/xml; charset=\"UTF-8\"");
+			echo '<?xml version="1.0" encoding="UTF-8"?>';
+			echo '<root>';
+			echo '<status code="'.$code.'">Callback Failed</status>';
+			echo '</root>';
+		}
+	}
+	catch (TxnInfoException $e)
+	{
+		header("HTTP/1.1 500 Internal Server Error");
+		$xml .= '<status code="'. $e->getCode() .'">'. htmlspecialchars($e->getMessage(), ENT_NOQUOTES). '</status>';
+		trigger_error($e->getMessage() ."\n". $HTTP_RAW_POST_DATA, E_USER_WARNING);
+	}
+	catch (CallbackException $e)
+	{
+		header("HTTP/1.1 500 Internal Server Error");
+		$xml .= '<status code="'. $e->getCode() .'">'. htmlspecialchars($e->getMessage(), ENT_NOQUOTES). '</status>';
+		trigger_error($e->getMessage() ."\n". $HTTP_RAW_POST_DATA, E_USER_WARNING);
+	}
 }
-catch (TxnInfoException $e)
+else
 {
-    header("HTTP/1.1 500 Internal Server Error");
-    $xml .= '<status code="'. $e->getCode() .'">'. htmlspecialchars($e->getMessage(), ENT_NOQUOTES). '</status>';
-    trigger_error($e->getMessage() ."\n". $HTTP_RAW_POST_DATA, E_USER_WARNING);
+	header("HTTP/1.1 200 Ok");
+	header("Content-Type: text/xml; charset=\"UTF-8\"");
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+	echo '<root>';
+	echo '<status code="1000">Callback Suppressed</status>';
+	echo '</root>';
 }
-catch (CallbackException $e)
-{
-    header("HTTP/1.1 500 Internal Server Error");
-    $xml .= '<status code="'. $e->getCode() .'">'. htmlspecialchars($e->getMessage(), ENT_NOQUOTES). '</status>';
-    trigger_error($e->getMessage() ."\n". $HTTP_RAW_POST_DATA, E_USER_WARNING);
-}
-
