a697d0600497e2af2ce96f5a8576974252745573
Pranav Manikpure <pranav@cellpointmobile.com>
2020-01-27 10:48:33 +0530

    CMP-3392 Create DragonPay on boarding Script

---
+++ db/snapshot/20-01-dragonpay.sql
@@ -0,0 +1,26 @@
+/* ========== Global Configuration for DragonPay = STARTS ========== */
+INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (61, 'DragonPay',1);
+
+INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (608,61,'PHP');
+
+/*
+* Dragon pay cad with Dragon Pay aggregator 
+*/
+INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (47, 61);
+
+
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (<clientid>, 61, 'DragonPay', <DragonPay_merchatid>, <DragonPay_MerchatAuthKey>);
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10018, 61, 'DragonPay', 'DragonPayMerchatID', 'DragonPayAuthKey');
+
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (<accountid>, 61, '-1');
+INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100094, 61, '-1');
+
+
+INSERT INTO Client.CardAccess_Tbl(clientid,cardid,enabled,pspid,countryid,stateid,psp_type) values (<clientid>,47,true,61,<countryid>,1,1);
+-- Route DragonPay Card to DragonPayAggregator with country Japan
+INSERT INTO Client.CardAccess_Tbl(clientid,cardid,enabled,pspid,countryid,stateid,psp_type) values (10018,47,true,61,64,1,1);
+
+
+
+/* ========== Global Configuration for DragonPay = ENDS ========== */
+INSERT INTO client.additionalproperty_tbl( key, value, externalid, type, scope ) VALUES (<KeyName>, <Value>, <ClientID>, 'client', 2 );
