2f26e52314dc2debf6b7598e65fe15252b712928
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-02-13 17:48:06 +0530

    CMP-3374 Updated column name in test cases

--- test/api/DCCPayAPITest.php
+++ test/api/DCCPayAPITest.php
@@ -19,7 +19,7 @@ class DCCPayAPITest extends PayAPITest
         $this->queryDB("INSERT INTO Client.Keyword_Tbl (id, clientid, name, standard) VALUES (1, 10018, 'CPM', true)");
         $this->queryDB("INSERT INTO Client.MerchantAccount_Tbl (id, clientid, pspid, name) VALUES (1, 10018, $pspID, '4216310')");
         $this->queryDB("INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (1100, $pspID, '-1')");
-        $this->queryDB("INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid,countryid,dcc) VALUES (10018, 8, $pspID,100,true)");
+        $this->queryDB("INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid,countryid,dccenabled) VALUES (10018, 8, $pspID,100,true)");
         $this->queryDB("INSERT INTO System.CardPricing_Tbl (pricepointid, cardid) SELECT C.currencyid * -1 AS pricepointid, 8 FROM System.Country_Tbl C, System.Card_Tbl Card WHERE C.id = 100 GROUP BY pricepointid;");
         $this->queryDB("INSERT INTO client.countrycurrency_tbl(clientid, countryid, currencyid, enabled) VALUES (10018,100,840, true)");
         $this->queryDB("INSERT INTO log.session_tbl (id, clientid, accountid, currencyid, countryid, stateid, orderid, amount, mobile, deviceid, ipaddress, externalid, sessiontypeid) VALUES (1, 10018, 1100, 208, 100, 4001, '1513-005', 5000, 29612109, '', '127.0.0.1', -1, 1);");
@@ -37,7 +37,6 @@ class DCCPayAPITest extends PayAPITest
         var_dump($res);
         $this->assertTrue(is_resource($res) );
         $res =  $this->queryDB("SELECT convetredcurrencyid FROM Log.Transaction_Tbl where id=1001011 and convetredcurrencyid = 208 and currencyid=840 and convertedamount=20000");
-        var_dump($res);
         $this->assertTrue(is_resource($res)&& pg_num_rows($res) == 1 );
     }
 
