4e47f6918bdba3c3ddbac44759f5a1bbafac1d97
Anupam <anupam@cellpointmobille.com>
2020-02-24 12:55:14 +0530

    CMP-3457 || removed retry count from additional-property-table

--- db/setup_pg_v2.20.sql
+++ db/setup_pg_v2.20.sql
@@ -1,6 +1,5 @@
 
-/* ========== retry and batch-size for the chase connector:: CMP-3457 ========== */
+/* ========== batch-size for the chase connector:: CMP-3457 ========== */
 
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_RETRY_COUNT', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid = <pspid>), 'merchant',1);
 
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
