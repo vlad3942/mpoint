b444f0adda98b75d3c42444e668f4a8cce706570
gauravp <gaurav.pawar@cellpointmobile.com>
2020-02-27 14:51:12 +0530

    CMP-3405 Consolidate mPoint DB sqls for UAT

---
+++ db/snapshot/v2.20_UAT_BUILD.sql
@@ -0,0 +1,40 @@
+--Aquirer level auto-capture--
+create table system.capturetype_tbl
+(
+    id serial not null
+        constraint capturetype_pk
+            primary key,
+    name varchar(50),
+    created timestamp default now(),
+    modified timestamp default now(),
+    enabled boolean default true
+);
+
+alter table system.capturetype_tbl owner to postgres;
+
+INSERT INTO system.capturetype_tbl (id, name, enabled) VALUES (1, 'Manual Capture', true);
+INSERT INTO system.capturetype_tbl (id, name, enabled) VALUES (2, 'PSP Level Auto Capture ', true);
+INSERT INTO system.capturetype_tbl (id, name, enabled) VALUES (3, 'Merchant Level Auto Capture', true);
+INSERT INTO system.capturetype_tbl (id, name, enabled) VALUES (4, 'Batch Capture', true);
+
+ALTER TABLE client.cardaccess_tbl
+    ADD COLUMN capture_type int2
+        CONSTRAINT cardaccess2capturetype_fk
+            REFERENCES system.capturetype_tbl DEFAULT (1) ;
+
+ALTER TABLE log.transaction_tbl ALTER COLUMN auto_capture DROP DEFAULT;
+ALTER TABLE log.transaction_tbl ALTER COLUMN auto_capture TYPE int2 USING CASE WHEN auto_capture=TRUE THEN 3 ELSE 1 END;
+ALTER TABLE log.transaction_tbl ALTER COLUMN auto_capture SET DEFAULT 1;
+
+ALTER TABLE client.client_tbl DROP COLUMN auto_capture;
+---End---
+
+/* ========== batch-size for the chase connector:: CMP-3457 ========== */
+
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
+
+ ALTER TABLE Log.Transaction_Tbl ALTER COLUMN attempt SET DEFAULT 1;
+
+
+
+
