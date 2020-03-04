6c4a4842cac8de28aa66c4aec8cb48062655c749
arvind <arvind@cellpointmobile.com>
2020-03-04 15:03:09 +0530

    CMP-3412 Added Southwest clients Id

--- db/snapshot/20-03-SIT-Build.sql
+++ db/snapshot/20-03-SIT-Build.sql
@@ -4,5 +4,10 @@
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
 
 
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('APPLEPAY_MERCHANT_DOMAIN', 'cpm-pay-dev2.cellpointmobile.com', <clientid>, 'client', true, 2);
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('APPLEPAY_JS_URL', 'https://s3.ap-southeast-1.amazonaws.com/cpmassets/psp/applepay.js', <clientid>, 'client', true, 2);
+/* ========== Below sql statements has to be updated to all the Apple Pay Live clients ========== */
+/* ========== While preparing prod release not please consider updating below queries ========== */
+
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('APPLEPAY_MERCHANT_DOMAIN', 'cpm-pay-dev2.cellpointmobile.com', 10069, 'client', true, 2);
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('APPLEPAY_JS_URL', 'https://s3.ap-southeast-1.amazonaws.com/cpmassets/psp/applepay.js', 10069, 'client', true, 2);
+
+/* ==================== */
