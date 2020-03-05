a7f433f8de64b8e53958a410c56b50351bd463ce
gauravp <gaurav.pawar@cellpointmobile.com>
2020-03-05 12:15:02 +0530

    CMP-3464 Consolidate mPoint DB sqls for UAT

--- db/snapshot/v2.20_UAT_BUILD.sql
+++ db/snapshot/v2.20_UAT_BUILD.sql
@@ -36,5 +36,70 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) V
  ALTER TABLE Log.Transaction_Tbl ALTER COLUMN attempt SET DEFAULT 1;
 
 
+CREATE TABLE CLIENT.STATICROUTELEVELCONFIGURATION
+(
+    ID           SERIAL
+        CONSTRAINT STATICROUTELEVELCONFIGURATION_PK
+            PRIMARY KEY,
+    CARDACCESSID INT                     NOT NULL,
+    CVCMANDATORY BOOLEAN   DEFAULT TRUE  NOT NULL,
+    ENABLED      BOOLEAN   DEFAULT TRUE  NOT NULL,
+    CREATED      TIMESTAMP DEFAULT now() NOT NULL,
+    MODIFIED     TIMESTAMP DEFAULT now() NOT NULL
+);
+
+COMMENT ON TABLE CLIENT.STATICROUTELEVELCONFIGURATION IS 'This table will contain the configuration based on '
+    'card schema, Provider and Country';
+
+COMMENT ON COLUMN CLIENT.STATICROUTELEVELCONFIGURATION.CARDACCESSID IS 'Primary key of client.cardaccess_tbl';
+
+create unique index staticroutelevelconfiguration_cardaccessid_uindex
+	on client.staticroutelevelconfiguration (cardaccessid);
+
+ALTER TABLE client.StaticRouteLevelConfiguration OWNER TO mpoint;
+
+CREATE TRIGGER UPDATE_STATICROUTELEVELCONFIGURATION
+    BEFORE UPDATE
+    ON CLIENT.STATICROUTELEVELCONFIGURATION
+    FOR EACH ROW
+EXECUTE PROCEDURE PUBLIC.Update_Table_Proc();
+
+
+
+/* PASSBOOK IMPROVEMENT - START*/
+
+alter table log.txnpassbook_tbl
+	add clientid int;
+
+alter table log.txnpassbook_tbl
+	add constraint txnpassbook_tbl_client_tbl_id_fk
+		foreign key (clientid) references client.client_tbl;
+
+/* Run migrate script before adding not null constraint */
+alter table log.txnpassbook_tbl alter column clientid set not null;
+
+/* PASSBOOK IMPROVEMENT - END */
+
+
+/* PASSBOOK IMPROVEMENT - START*/
+
+/* If required add range check to avoid high peak in RDS CPU */
+UPDATE LOG.TXNPASSBOOK_TBL PASSBOOK
+SET CLIENTID = TRANSACTION.CLIENTID
+FROM LOG.TRANSACTION_TBL TRANSACTION
+WHERE PASSBOOK.TRANSACTIONID = TRANSACTION.ID;
+
+/* PASSBOOK IMPROVEMENT - END */
+/* ========== batch-size for the chase connector:: CMP-3457 ========== */
+
+
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
+
+
+INSERT INTO CLIENT.STATICROUTELEVELCONFIGURATION (CARDACCESSID) SELECT id FROM CLIENT.CARDACCESS_TBL WHERE PSP_TYPE = 1;
+
+
+
+
 
 
