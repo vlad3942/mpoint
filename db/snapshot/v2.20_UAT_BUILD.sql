18e4869ea6505080c004e4c0874e10d20e4ebfbd
gauravp <gaurav.pawar@cellpointmobile.com>
2020-03-05 13:59:12 +0530

    CMP-3464 Consolidate mPoint DB sqls for UAT - code review comment fixes

--- db/snapshot/v2.20_UAT_BUILD.sql
+++ db/snapshot/v2.20_UAT_BUILD.sql
@@ -71,16 +71,15 @@ EXECUTE PROCEDURE PUBLIC.Update_Table_Proc();
 alter table log.txnpassbook_tbl
 	add clientid int;
 
-/* If required add range check to avoid high peak in RDS CPU */
-UPDATE LOG.TXNPASSBOOK_TBL PASSBOOK
-
-SET CLIENTID = TRANSACTION.CLIENTID
-FROM LOG.TRANSACTION_TBL TRANSACTION
-WHERE PASSBOOK.TRANSACTIONID = TRANSACTION.ID;
-
 alter table log.txnpassbook_tbl
 	add constraint txnpassbook_tbl_client_tbl_id_fk
 		foreign key (clientid) references client.client_tbl;
+
+/* If required add range check to avoid high peak in RDS CPU */
+UPDATE LOG.TXNPASSBOOK_TBL PASSBOOK
+SET CLIENTID = TRANSACTION.CLIENTID
+FROM LOG.TRANSACTION_TBL TRANSACTION
+WHERE PASSBOOK.TRANSACTIONID = TRANSACTION.ID;
 
 /* Run migrate script before adding not null constraint */
 alter table log.txnpassbook_tbl alter column clientid set not null;
@@ -90,7 +89,7 @@ alter table log.txnpassbook_tbl alter column clientid set not null;
 /* ========== batch-size for the chase connector:: CMP-3457 ========== */
 
 
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', '1', (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) VALUES ('MVAULT_BATCH_SIZE', <value>, (SELECT id FROM Client.MerchantAccount_Tbl WHERE clientid = <client id> and pspid =  <pspid>), 'merchant',1);
 
 
 INSERT INTO CLIENT.STATICROUTELEVELCONFIGURATION (CARDACCESSID) SELECT id FROM CLIENT.CARDACCESS_TBL WHERE PSP_TYPE = 1;
