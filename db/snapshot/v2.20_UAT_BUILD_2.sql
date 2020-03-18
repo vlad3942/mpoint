289846b79c5cfb2f899d907ed62d2662e52117b8
Abhinav <abhinav.shaha@cellpointmobile.com>
2020-03-18 16:45:08 +0530

    CMP-3526 : UAT Build Preparation

--- db/snapshot/v2.20_UAT_BUILD_2.sql
+++ db/snapshot/v2.20_UAT_BUILD_2.sql
@@ -1,2 +1,8 @@
 -- Update system_type for EGHL FPX 
 update system.psp_tbl set system_type = 7 where id = 51;
+
+/* ========== HPP Additional Property for Card Option Display : CPMEH-728 ========== */
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('hideCardPaymentOption', 'true', <clientid>, 'client', true, 2);
+
+-- every psp other than chase payment is set to Psp auto capture
+update client.cardaccess_tbl set capture_type = 2 where pspid != 52;
