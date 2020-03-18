d8db67082fff9a2d6a338a8e6c43551ad4ef72f9
Abhinav <abhinav.shaha@cellpointmobile.com>
2020-03-18 17:12:47 +0530

    CMP-3526:UAT-Build-Preparation

--- db/snapshot/v2.20_UAT_BUILD_2.sql
+++ db/snapshot/v2.20_UAT_BUILD_2.sql
@@ -3,6 +3,3 @@ update system.psp_tbl set system_type = 7 where id = 51;
 
 /* ========== HPP Additional Property for Card Option Display : CPMEH-728 ========== */
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('hideCardPaymentOption', 'true', <clientid>, 'client', true, 2);
-
--- every psp other than chase payment is set to Psp auto capture
-update client.cardaccess_tbl set capture_type = 2 where pspid != 52;
