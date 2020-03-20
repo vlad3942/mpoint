b6a28e52c242f13cff0163463cbb28230c549752
Abhinav <abhinav.shaha@cellpointmobile.com>
2020-03-20 14:53:29 +0530

    CPMRM-4910 : Added sql query

--- db/snapshot/20-04-SIT-Build.sql
+++ db/snapshot/20-04-SIT-Build.sql
@@ -1,2 +1,5 @@
 -- Update system_type for EGHL FPX 
 update system.psp_tbl set system_type = 7 where id = 51;
+
+-- Update system_type for Paytabs 
+update system.psp_tbl set system_type = 7 where id = 38;
