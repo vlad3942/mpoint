210efc4615cd91111a4e9b5bded3c086b011bc68
Anna Lagad <anna.lagad@cellpointmobile.com>
2020-03-23 22:00:37 +0530

    CMP-3471: optimize card client_info node

--- api/classes/clientinfo.php
+++ api/classes/clientinfo.php
@@ -183,43 +183,29 @@ class ClientInfo
     public function toAttributeLessXML()
     {
         $xml = '';
-        if(empty($this->getAppID())===false)
+        $xml .= '<platform>'.htmlspecialchars($this->_sPlatform, ENT_NOQUOTES).'</platform>';
+        $xml .= '<language>'.htmlspecialchars($this->_sLanguage, ENT_NOQUOTES).'</language>';
+        $xml .= '<version>'.number_format($this->_fVersion, 2).'</version>';
+        if(empty($this->_iAppID) == false)
         {
-            $xml .= '<app_id>'.$this->getAppID().'</app_id>';
+            $xml .= '<app_id>'.$this->_iAppID.'</app_id>';
         }
-        if(empty($this->getPlatform())===false)
+        if(empty($this->_sMobile) === false)
         {
-            $xml .= '<platform>'.htmlspecialchars($this->getPlatform(), ENT_NOQUOTES).'</platform>';
-        }
-        if(empty($this->getLanguage())===false)
-        {
-            $xml .= '<language>'.htmlspecialchars($this->getLanguage(), ENT_NOQUOTES).'</language>';
-        }
-        if(empty($this->getVersion())===false)
-        {
-            $xml .= '<version>'.number_format($this->getVersion(), 2).'</version>';
-        }
-        if(empty($this->getMobile())===false)
-        {
-            $xml .= '<mobile>'.$this->getMobile().'</mobile>';
-        }
-        if(empty($this->_obj_CountryConfig->getID())===false)
-        {
+            $xml .= '<mobile>'.$this->_sMobile.'</mobile>';
             $xml .= '<country_id>'.$this->_obj_CountryConfig->getID().'</country_id>';
         }
-        if(empty($this->getEMail())===false)
+        if(empty($this->_sEMail) === false)
         {
-            $xml .= '<email>'. htmlspecialchars($this->getEMail(), ENT_NOQUOTES) .'</email>';
+            $xml .= '<email>'. htmlspecialchars($this->_sEMail, ENT_NOQUOTES) .'</email>';
         }
-        if(empty($this->getDeviceID())===false)
+        if(empty($this->_sDeviceID) === false)
         {
-            $xml .= '<device_id>'.htmlspecialchars($this->getDeviceID(), ENT_NOQUOTES).'</device_id>';
+            $xml .= '<device_id>'. htmlspecialchars($this->_sDeviceID, ENT_NOQUOTES) .'</device_id>';
         }
-        if(empty($this->getIP())===false)
-        {
-            $xml .= '<ip>'. htmlspecialchars($this->getIP(), ENT_NOQUOTES) .'</ip>';
+        if(empty($this->_sIP) === false){
+            $xml .= '<ip>'. htmlspecialchars($this->_sIP, ENT_NOQUOTES) .'</ip>';
         }
-
         return $xml;
     }
 	
