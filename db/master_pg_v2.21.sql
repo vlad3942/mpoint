89adda03fed1962f9c74854e98f4275c59542b10
Sagar Narayane <sagar@cellpointmobile.com>
2020-04-10 14:39:51 +0530

    CMP-3576
    DB Scripts

--- db/master_pg_v2.21.sql
+++ db/master_pg_v2.21.sql
@@ -2,3 +2,7 @@
 ALTER TABLE client.cardaccess_tbl ADD walletid int4;
 drop index cardaccess_card_country_uq;
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl USING btree (clientid, cardid, pspid, countryid, psp_type,walletid) WHERE (enabled = true);
+
+DROP TABLE IF EXISTS CLIENT.RETRIAL_TBL;
+
+DROP TABLE IF EXISTS SYSTEM.RETRIALTYPE_TBL;
