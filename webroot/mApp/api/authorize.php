bde480f1b87fba78eaa263fc4598c6d34d83fcd9
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-04-13 14:49:24 +0530

    CMP-3587 wallet based routing stored card fix

--- webroot/mApp/api/authorize.php
+++ webroot/mApp/api/authorize.php
@@ -538,7 +538,13 @@ try
 																	$obj_Elem->cvc = (string) $obj_DOM->{'authorize-payment'}[$i]->transaction->card[$j]->cvc;
 																}
 
-                                                                    $obj_XML = $obj_CardXML->xpath("/cards/item[@type-id = ". $obj_Elem["type-id"] ." and @state-id=1 and @walletid=".$wallet_Processor->getPSPConfig()->getID()."]");
+																    $walletid = '';
+															        if($wallet_Processor->getPSPConfig()->getID() !== Constants::iMVAULT_WALLET)
+                                                                    {
+                                                                        $walletid = $wallet_Processor->getPSPConfig()->getID();
+                                                                    }
+
+                                                                    $obj_XML = $obj_CardXML->xpath("/cards/item[@type-id = ". $obj_Elem["type-id"] ." and @state-id=1 and @walletid=".$walletid."]");
                                                                     if (count($obj_XML) == 0)
                                                                     {
                                                                         $code = 5;
