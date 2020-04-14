9c7fe25f81d6546662bed2cd2ead50322d00f346
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-04-14 15:44:24 +0530

    CMP-3611
    [Unit Test Case] Test Cases Fix

--- db/master_pg_v2.19.sql
+++ db/master_pg_v2.19.sql
@@ -10,4 +10,5 @@ CREATE TABLE log.externalreferencetype_tbl (
 ALTER TABLE log.externalreference_tbl ADD type int4 CONSTRAINT externalreferencetype_fk REFERENCES log.externalreferencetype_tbl(id);
 ALTER TABLE log.transaction_tbl ADD convetredcurrencyid int4 NULL CONSTRAINT convertedcurrency_fk REFERENCES system.currency_tbl(id);
 ALTER TABLE log.transaction_tbl ADD convertedamount int8 NULL;
+ALTER TABLE log.transaction_tbl ADD conversionrate decimal DEFAULT 1;
 ALTER TABLE client.cardaccess_tbl ADD dccenabled bool NULL DEFAULT false;
--- db/master_pg_v2.21.sql
+++ db/master_pg_v2.21.sql
@@ -2,3 +2,20 @@
 ALTER TABLE client.cardaccess_tbl ADD walletid int4;
 drop index cardaccess_card_country_uq;
 CREATE UNIQUE INDEX cardaccess_card_country_uq ON client.cardaccess_tbl USING btree (clientid, cardid, pspid, countryid, psp_type,walletid) WHERE (enabled = true);
+
+
+--DCC---
+CREATE TABLE log.externalreferencetype_tbl (
+	id serial NOT NULL,
+	"name" text NOT NULL,
+	created timestamp NULL DEFAULT now(),
+	modified timestamp NULL DEFAULT now(),
+	enabled bool NULL DEFAULT true,
+	CONSTRAINT externalreferencetype_pk PRIMARY KEY (id)
+);
+ALTER TABLE log.externalreference_tbl ADD type int4 CONSTRAINT externalreferencetype_fk REFERENCES log.externalreferencetype_tbl(id);
+ALTER TABLE log.transaction_tbl ADD convetredcurrencyid int4 NULL CONSTRAINT convertedcurrency_fk REFERENCES system.currency_tbl(id);
+ALTER TABLE log.transaction_tbl ADD convertedamount int8 NULL;
+ALTER TABLE log.transaction_tbl ADD conversionrate decimal DEFAULT 1;
+ALTER TABLE client.cardaccess_tbl ADD dccenabled bool NULL DEFAULT false;
+---DCC---
