fcc1690cb286a5bc9dc22fdd409780d3f00d74d1
Abhinav <abhinav.shaha@cellpointmobile.com>
2020-04-17 18:05:02 +0530

    CMP-3504 : Worked on review comments

--- api/classes/txninfo.php
+++ api/classes/txninfo.php
@@ -422,7 +422,7 @@ class TxnInfo
      *
      * @var string
      */
-    private $sIssuing_Bank;
+    private $_sIssuingBank;
 
     /**
 	 * Default Constructor
@@ -549,7 +549,7 @@ class TxnInfo
         $this->_lConvertedAmount = (float) $ofAmt;
         $this->_obj_ConvertedCurrencyConfig = $oFCR;
         $this->_fconversionRate = (float)$fconversionRate;
-        $this->sIssuing_Bank = trim($sIssuing_Bank);
+        $this->_sIssuingBank = trim($sIssuing_Bank);
 
         }
 
@@ -1010,7 +1010,7 @@ class TxnInfo
      */
 	public function getIssuingBankName()
 	{
-		return $this->sIssuing_Bank;
+		return $this->_sIssuingBank;
 	}
 
 
@@ -1613,7 +1613,7 @@ class TxnInfo
                 $paymentSession = PaymentSession::Get($obj_db,$misc["sessionid"]);
             }
 
-            $obj_TxnInfo = new TxnInfo($id, $misc["typeid"], $obj, $misc["country-config"],$misc["currency-config"], $misc["amount"], $misc["points"], $misc["reward"], $misc["refund"], $misc["orderid"], $misc["extid"], $misc["mobile"], $misc["operator"], $misc["email"], $misc["device-id"], $misc["logo-url"], $misc["css-url"], $misc["accept-url"], $misc["decline-url"], $misc["cancel-url"], $misc["callback-url"], $misc["icon-url"], $misc["auth-url"], $misc["language"], $obj->getMode(), AutoCaptureType::eRunTimeAutoCapt, $misc["accountid"], @$misc["customer-ref"], $misc["gomobileid"], $misc["auto-store-card"], $misc["markup"], $misc["description"], $misc["ip"], $misc["attempt"], $paymentSession, $misc["producttype"],$misc["installment-value"], $misc["profileid"],-1,0,0,-1,-1,"","","","","","","",array(),array(),$misc["converted-amount"],$misc["converted-currency-config"],$misc["conversion-rate"]);
+            $obj_TxnInfo = new TxnInfo($id, $misc["typeid"], $obj, $misc["country-config"],$misc["currency-config"], $misc["amount"], $misc["points"], $misc["reward"], $misc["refund"], $misc["orderid"], $misc["extid"], $misc["mobile"], $misc["operator"], $misc["email"], $misc["device-id"], $misc["logo-url"], $misc["css-url"], $misc["accept-url"], $misc["decline-url"], $misc["cancel-url"], $misc["callback-url"], $misc["icon-url"], $misc["auth-url"], $misc["language"], $obj->getMode(), AutoCaptureType::eRunTimeAutoCapt, $misc["accountid"], @$misc["customer-ref"], $misc["gomobileid"], $misc["auto-store-card"], $misc["markup"], $misc["description"], $misc["ip"], $misc["attempt"], $paymentSession, $misc["producttype"],$misc["installment-value"], $misc["profileid"],-1,0,0,-1,-1,"","","","","","","",array(),array(),$misc["converted-amount"],$misc["converted-currency-config"],$misc["conversion-rate"],"");
 			break;
 		case ($obj_db instanceof RDB):		// Instantiate from Transaction Log
             $obj = $obj_db;
