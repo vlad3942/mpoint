c32cbbfd20e60b40568e80aee6498c961240ba48
arvind <arvind@cellpointmobile.com>
2020-04-21 09:55:48 +0530

    removed local include

--- test/api/initializeAPIValidationTest.php
+++ test/api/initializeAPIValidationTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../webroot/inc/include_test.php';
+require_once __DIR__ . '/../../webroot/inc/include.php';
 require_once __DIR__ . '/../inc/testinclude.php';
 
 class InitializeAPIValidationTest extends baseAPITest
