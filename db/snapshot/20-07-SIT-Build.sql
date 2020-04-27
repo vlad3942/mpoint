a68e3ff375bb95f5fa0a1bd3614670cc0891f8a3
arvind <arvind@cellpointmobile.com>
2020-04-27 12:20:45 +0530

    CMP-3634 Added DB scripts

--- db/snapshot/20-07-SIT-Build.sql
+++ db/snapshot/20-07-SIT-Build.sql
@@ -1,3 +1,9 @@
 -- passenger tbl --
 ALTER TABLE log.passenger_tbl alter column first_name type varchar(50);
 ALTER TABLE log.passenger_tbl alter column last_name type varchar(50);
+
+-- currency improvement --
+ALTER TABLE system.currency_tbl ADD COLUMN symbol VARCHAR(5);
+UPDATE system.currency_tbl AS cur SET symbol = con.symbol FROM system.country_tbl AS con WHERE cur.id = con.currencyid;
+-- Execute below query after completion of first two
+ALTER TABLE system.country_tbl DROP COLUMN symbol;
