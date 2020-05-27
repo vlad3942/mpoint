214efa0281dc342670620e409fe227178765e7e0
urmila <urmila@cellpointmobile.com>
2020-05-27 12:43:43 +0530

    CMP-3855: MID config for rest of the currencies.

--- db/setup_pg_v2.22.sql
+++ db/setup_pg_v2.22.sql
@@ -36,3 +36,41 @@ INSERT INTO Client.CardAccess_Tbl(clientid,cardid,enabled,pspid,countryid,statei
 --set timezone for CEBU
 INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('TIMEZONE', 'Asia/Kuala_Lumpur', true, 10077, 'client', 2);
 
+
+--------- start CEBU Paypal configuration to setup multiple MIDs as per currency
+
+--PHP and all other currencies not configured in additionalproperty_tbl will go the the default merchant account entry for PHP.
+-- that is JPY, AED, AUD, IDR, BND, CNY, KRW,THB,TWD and MOP as defined in CEBU-6
+-- default merchantaccount_tbl onboarding sql
+INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd, enabled, stored_card) VALUES (10077, 24,'ACCu12.jik2Wb3kzHJgFE1palQEsAHD7wsynoTYqRcOZAX7RzupgW4sQ', 'sb-sahh431638744_api1.business.example.com', '7W56K2VQBRYF8FLX', true, null);
+
+---additional MIDs for SGD, HKD, MYR, USD
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PayPal.rule', 'username ::= "PAYPAL_USERNAME_"(transaction/authorized-amount/@currency)
+password ::= "PAYPAL_PASSWORD_"(transaction/authorized-amount/@currency)
+mid ::= "PAYPAL_MID_"(transaction/authorized-amount/@currency)', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_USERNAME_SGD', 'sb-mohn91867880_api1.business.example.com', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_PASSWORD_SGD', 'B9WX2HPY9DPD6284', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_MID_SGD', 'ATG2YLmUZFFk6n5kHVGwCC2A2dsDAEAdhFEakpPDRN5lxmGq3zimTG6A', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_USERNAME_HKD', 'sb-ph1ko1832308_api1.business.example.com', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_PASSWORD_HKD', '5QBM4GMSFPV8AHN', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_MID_HKD', 'A78isDTCcuwhNZKyvzOsXXSzxdUPA25dTmhkEQxH7G1T4iQSksAr-SK0', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_USERNAME_MYR', 'sb-ivizq1858258_api1.business.example.com', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_PASSWORD_MYR', 'VMXEJAT9DCLCR7LQ', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_MID_MYR', 'AJXtiLishvWwk7Jm0EnHXPmvJv1xAqMsrFcJMX8Exsbl9aQ5NJRKPyem', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_USERNAME_USD', 'sb-43kvng1868465_api1.business.example.com', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_PASSWORD_USD', '37JT6WGJFFUJFRM3', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) select 'PAYPAL_MID_USD', 'Awzp6NMJcMOsM6SlpR13Cez-7vPFAMODxiW5ZT0qx6EbatLFSrMoBKtc', true, id, 'merchant', 0 from Client.MerchantAccount_Tbl where clientid=10077 and pspid=24;
+---end CEBU paypal --
