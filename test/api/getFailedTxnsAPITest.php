44826e8a69b0b00aaa5f097e24d48f4902f7a197
praveen <praveen@cellpointmobile.com>
2020-06-01 23:59:18 +0530

    CMP-3764 PHPUnit : fix test cases

--- test/api/getFailedTxnsAPITest.php
+++ test/api/getFailedTxnsAPITest.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../../webroot/inc/include.php';
 require_once __DIR__ . '/../inc/testinclude.php';
 
-class GetFailedTransactionsAPITest extends baseAPITest
+class GetFailedTxnsAPITest extends baseAPITest
 {
 	protected $_aMPOINT_CONN_INFO;
 
