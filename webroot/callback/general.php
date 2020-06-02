e55f0c7f7c0225bad7006fef4b9bd50b5946bba2
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-06-02 22:50:59 +0530

    CMP-3888
    Fixed Class 'RefundException' not found in cpm_psp.php

--- webroot/callback/general.php
+++ webroot/callback/general.php
@@ -23,6 +23,8 @@ require_once(sCLASS_PATH ."/customer_info.php");
 require_once(sCLASS_PATH ."/callback.php");
 // Require specific Business logic for Capture component (for use with auto-capture functionality)
 require_once(sCLASS_PATH ."/capture.php");
+// Require specific Business logic for the Refund component
+require_once(sCLASS_PATH ."/refund.php");
 // Require specific Business logic for the CPM PSP component
 require_once(sINTERFACE_PATH ."/cpm_psp.php");
 // Require specific Business logic for the CPM FRAUD GATEWAY component
