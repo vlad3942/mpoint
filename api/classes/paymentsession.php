107bef7d80ad92f19eb3a850c991e57038688761
Sagar Narayane <sagar@cellpointmobile.com>
2020-06-05 19:01:11 +0530

    CMP-3894
    Same session get assign to different transaction of different clients

--- api/classes/paymentsession.php
+++ api/classes/paymentsession.php
@@ -285,7 +285,7 @@ final class PaymentSession
 
     public function updateTransaction($txnId)
     {
-        $sql = "UPDATE log" . sSCHEMA_POSTFIX . ".transaction_tbl SET sessionid = " . $this->_id . " WHERE id = " . intval($txnId);
+        $sql = "UPDATE log" . sSCHEMA_POSTFIX . ".transaction_tbl SET sessionid = " . $this->_id . " WHERE id = " . (int)$txnId ." and SESSIONID NOTNULL";
         $this->_obj_Db->query($sql);
     }
 
