4f4888ef292e7ec7899bbbf0ec66ae34dc207af7
Nitin Murlidhar Gaikwad <nitin@cellpointmobile.com>
2020-06-08 20:43:53 +0530

    Revert "CMP-3645 added the businesstype in client config tag"
    This reverts commit 8312a4c2774c02197c03929e22e4a73bd40628ce.

--- api/interfaces/cpm_psp.php
+++ api/interfaces/cpm_psp.php
@@ -530,8 +530,8 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 		$code = 0;
 		$b  = '<?xml version="1.0" encoding="UTF-8"?>';
 		$b .= '<root>';
-		$b .= '<authorize client-id="'. $this->getClientConfig()->getID(). '" account="'. $this->getClientConfig()->getAccountConfig()->getBusinessType(). '">';
-        $b .= '<client-config business-type="'.$this->getClientConfig()->getAccountConfig()->getBusinessType(). '">';
+		$b .= '<authorize client-id="'. $this->getClientConfig()->getID(). '" account="'. $this->getClientConfig()->getAccountConfig()->getID(). '">';
+        $b .= '<client-config>';
         $b .= '<additional-config>';
 
         foreach ($this->getClientConfig()->getAdditionalProperties(Constants::iPrivateProperty) as $aAdditionalProperty)
