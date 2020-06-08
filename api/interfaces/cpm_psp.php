903c8435189e5b6bb1c9e302f1c8fc335ab195d3
Nitin Murlidhar Gaikwad <nitin@cellpointmobile.com>
2020-06-08 20:40:53 +0530

    Revert "CMP-3645 added null check for businesstype and made businesstype optional in xsd"
    This reverts commit d87be09898757342940b0a429172582c2b08d24b.

--- api/interfaces/cpm_psp.php
+++ api/interfaces/cpm_psp.php
@@ -531,13 +531,7 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 		$b  = '<?xml version="1.0" encoding="UTF-8"?>';
 		$b .= '<root>';
 		$b .= '<authorize client-id="'. $this->getClientConfig()->getID(). '" account="'. $this->getClientConfig()->getAccountConfig()->getID(). '">';
-		if($this->getClientConfig()->getAccountConfig()->getBusinessType() !=null)
-        {
-            $b .= '<client-config business-type="'.$this->getClientConfig()->getAccountConfig()->getBusinessType(). '">';
-        }
-        else{
-            $b .= '<client-config>';
-        }
+        $b .= '<client-config business-type="'.$this->getClientConfig()->getAccountConfig()->getBusinessType(). '">';
         $b .= '<additional-config>';
 
         foreach ($this->getClientConfig()->getAdditionalProperties(Constants::iPrivateProperty) as $aAdditionalProperty)
@@ -608,6 +602,7 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 				if($approvalCode != ''){
 					$sql .= ",approval_action_code = '".$approvalCode."'";
 				}
+					
 
 				// In case of 3D verification status code 2005 will be received
 				if($code == 2005)
