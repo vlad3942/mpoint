2cf24200d37b4a589406e26cd0ec7d508feb2698
urmila <urmila@cellpointmobile.com>
2020-07-15 16:21:21 +0530

    CMP-4095: currency symbols sql update

--- db/snapshot/20-13-Currency-symbols.sql
+++ db/snapshot/20-13-Currency-symbols.sql
@@ -1,42 +1,5 @@
-/* ========== Global Configuration for DragonPay Offline = STARTS ========== */
+--SQL update query
 
-INSERT INTO system.card_tbl (id, name, logo, enabled, position, minlength, maxlength, cvclength, paymenttype) VALUES (88, 'DRAGONPAYOFFLINE', null, true, 23, -1, -1, -1, 4);
-
-/* ========== Global Common Configuration for DragonPay Offline/DragonPay Online = STARTS ========== */
-INSERT INTO System.PSP_Tbl (id, name,system_type) VALUES (61, 'DragonPay',1);
-
-INSERT INTO System.PSPCurrency_Tbl (currencyid, pspid, name) VALUES (608,61,'PHP');
-
-
-INSERT INTO system.cardpricing_tbl (pricepointid, cardid, enabled) VALUES (-608, 88, true);
-
-/*
-* Dragon pay offline card with Dragon Pay aggregator 
-*/
-
-INSERT INTO System.PSPCard_Tbl (cardid, pspid) VALUES (88, 61);
-
-/* ========== Global Common Configuration for DragonPay Offline/DragonPay Online = STARTS ========== */
-
-INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (<clientid>, 61, 'DragonPay', <DragonPay_merchatid>, <DragonPay_MerchatAuthKey>);
-INSERT INTO Client.MerchantAccount_Tbl (clientid, pspid, name, username, passwd) VALUES (10018, 61, 'DragonPay', 'CPM', '3GJ8LubyWVUMgqY');
-
-INSERT INTO client.countrycurrency_tbl (clientid,countryid,currencyid,enabled)VALUES (<ClientID>, <countryid>, <CurrencyID>,true)
-INSERT INTO client.countrycurrency_tbl (clientid,countryid,currencyid,enabled)VALUES (10018,640,608,true)
-
-
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (<accountid>, 61, '-1');
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100094, 61, '-1');
-
--- Route DragonPay Offline Card to DragonPayAggregator with country Japan
-INSERT INTO Client.CardAccess_Tbl(clientid,cardid,enabled,pspid,countryid,stateid,psp_type) values (<clientid>,88,true,61,<countryid>,1,1);
--- Route DragonPay Offline  Card to DragonPayAggregator with country Japan
-INSERT INTO Client.CardAccess_Tbl(clientid,cardid,enabled,pspid,countryid,stateid,psp_type) values (10018,88,true,61,640,1,1);
-
---set timezone for CEBU
-INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('TIMEZONE', 'Asia/Kuala_Lumpur', true, 10077, 'client', 2);
-
---currency symbols SQL update query
 update system.currency_tbl set symbol='L'  where id='8'  and code='ALL';
 update system.currency_tbl set symbol='DA'  where id='12'  and code='DZD';
 update system.currency_tbl set symbol='$'  where id='32'  and code='ARS';
