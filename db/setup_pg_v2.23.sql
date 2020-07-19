475fa9ce751e80fddf7293ffa05676ce17552079
Gaorav Vishnoi <gaorav@cellpointmobile.com>
2020-07-19 17:56:33 +0530

    CMP-4123 SQL for mPoint setup for 3DS2.0

--- db/setup_pg_v2.23.sql
+++ db/setup_pg_v2.23.sql
@@ -2,3 +2,8 @@
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('GOOGLE_PAY_JS_URL', <Google Pay JS URL>, <Client-id>, 'client', true, 2);
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('GOOGLE_PAY_JS_URL', 'https://devcpmassets.s3-ap-southeast-1.amazonaws.com/payment/od/gpay/googlepay.js', <Client-id>, 'client', true, 2);
 --------------------------Custom Google Pay JS Path-----------------------------------------------
+
+-------------------------- Property TO enable 3DS2.0 FOR a client id -------------------------------------
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope)
+VALUES('3DSVERSION', '2.0', true, <client ID>, 'client', 2);
+-------------------------- Property TO enable 3DS2.0 FOR a client id -------------------------------------
