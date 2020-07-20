0c328d4ec6221bb9400aeb3a584c354c18f89f86
Anna Lagad <anna.lagad@cellpointmobile.com>
2020-07-20 17:32:50 +0530

    CMP-4116: Query Optimization

--- api/classes/client_routes_config.php
+++ api/classes/client_routes_config.php
@@ -9,7 +9,7 @@
  * File Name:get_routes_config.php
  */
 
-class ClientRoutesConfig extends BasicConfig
+class ClientRoutesConfig
 {
     /**
      * The unique ID of the contry the configuration is valid in or -1 for "ALL"
@@ -47,40 +47,28 @@ class ClientRoutesConfig extends BasicConfig
      * @var integer
      */
     private $_iCardType;
-    /**
-     * The unique ID of the currency for Payment
-     *
-     * @var integer
-     */
-    private $_iCurrencyId;
 
     /**
      * Default Constructor
      *
-     * @param 	integer $id 		The unique ID for the client's Payment Method (Card) configuration
      * @param 	integer $pmid 		The unique ID for the Payment Method (Card) type
-     * @param 	integer $name	 	The name of the Payment Method (Card)
      * @param 	integer $countryid	The unique ID of the contry the configuration is valid in. Pass -1 for "ALL"
      * @param 	integer $stateid	The unique ID of the current Payment Method (Card) state for the client
      * @param 	integer $pspid 		The unique ID of the Payment Service Provider (PSP) that will process payments for this Payment Method (Card)
      * @param 	boolean $enabled 	Flag indicating whether the routing configuration is currently active
      * @param 	integer $cardtype 	The unique ID for the Payment type
-     * @param 	integer $currencyid The unique ID of the currency for Payment
      */
-    public function __construct($id, $pmid, $name, $countryid, $stateid, $pspid, $enabled, $cardtype, $currencyid)
+    public function __construct($pmid, $countryid, $stateid, $pspid, $enabled, $cardtype)
     {
-        parent::__construct($id, $name);
         $this->_iPaymentMethodID = (integer) $pmid;
         $this->_iCountryId = (integer) $countryid;
         $this->_iStateID = (integer) $stateid;
         $this->_iPSPID = (integer) $pspid;
         $this->_bEnabled = (bool) $enabled;
         $this->_iCardType = $cardtype;
-        $this->_iCurrencyId = $currencyid;
     }
 
     public function getCountryID() { return $this->_iCountryId; }
-    public function getCurrencyID() { return $this->_iCurrencyId; }
     public function getStateID() { return $this->_iStateID; }
     public function getPSPID() { return $this->_iPSPID; }
     public function getPaymentMethodID() { return $this->_iPaymentMethodID; }
@@ -92,7 +80,6 @@ class ClientRoutesConfig extends BasicConfig
         $xml = '<route>';
         $xml .= '<card_type_id>'.$this->getPaymentMethodID().'</card_type_id>';
 	    $xml .= '<country_id>'.$this->getCountryID().'</country_id>';
-	    $xml .= '<currency_id>'.$this->getCurrencyID().'</currency_id>';
 	    $xml .= '<psp_id>'.$this->getPSPID().'</psp_id>';
 	    $xml .= '<enabled>'.General::bool2xml($this->isEnabled()).'</enabled>';
 	    $xml .= '<psp_type>'.$this->getCardType().'</psp_type>';
@@ -104,45 +91,21 @@ class ClientRoutesConfig extends BasicConfig
     /**
      * Produces a new instance of a client Routes Configuration Object.
      *
-     * @param 	RDB $oDB 		Reference to the Database Object that holds the active connection to the mPoint Database
-     * @param 	integer $id 	Unique ID for the static route the request is performed in
-     * @return 	clientRoutescyConfig
-     */
-    public static function produceConfig(RDB $oDB, $id)
-    {
-        $sql = "SELECT DISTINCT CA.id, Coalesce(CA.countryid, 0) AS countryid, CA.stateid, CA.pspid, CA.enabled, CA.cardid, C.name, CA.psp_type AS paymenttype, CC.currencyid		
-				FROM Client". sSCHEMA_POSTFIX .".CardAccess_Tbl CA
-				INNER JOIN System". sSCHEMA_POSTFIX .".Card_Tbl C ON CA.cardid = C.id
-				LEFT JOIN Client". sSCHEMA_POSTFIX .".countrycurrency_tbl CC ON CA.countryid = CC.countryid
-				WHERE CA.id = ". intval($id);
-
-        $RS = $oDB->getName($sql);
-
-        if (is_array($RS) === true && count($RS) > 0)
-        {
-            return new ClientRoutesConfig($RS["ID"], $RS["CARDID"], $RS["NAME"], $RS["COUNTRYID"], $RS["STATEID"], $RS["PSPID"], $RS["ENABLED"], $RS['PAYMENTTYPE'], $RS['CURRENCYID']);
-        }
-        else { return null; }
-    }
-
-    /**
-     * Produces a new instance of a client Routes Configuration Object.
-     *
      * @param 	RDB $oDB 		             Reference to the Database Object that holds the active connection to the mPoint Database
      * @param 	integer $clientid 	         Unique ID for the client
      * @return 	array $aObj_Configurations   Static Routes Configuration
      */
-    public static function produceConfigurations(RDB $oDB, $clientid)
+    public static function produceConfig(RDB $oDB, $clientid)
     {
-        $sql = "SELECT DISTINCT CA.id
+        $sql = "SELECT Coalesce(CA.countryid, 0) AS countryid, CA.stateid, CA.pspid, CA.enabled, CA.cardid, CA.psp_type AS paymenttype		
 				FROM Client". sSCHEMA_POSTFIX .".CardAccess_Tbl CA
 				WHERE CA.clientid = ". intval($clientid);
 
-        $aObj_Configurations = array();
         $res = $oDB->query($sql);
+        $aObj_Configurations = array();
         while ($RS = $oDB->fetchName($res) )
         {
-            $aObj_Configurations[] = self::produceConfig($oDB, $RS["ID"]);
+            $aObj_Configurations[] = new ClientRoutesConfig($RS["CARDID"], $RS["COUNTRYID"], $RS["STATEID"], $RS["PSPID"], $RS["ENABLED"], $RS['PAYMENTTYPE']);
         }
 
         return $aObj_Configurations;
