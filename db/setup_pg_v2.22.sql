06935fc2779bc7457d2ec654fad8f337ceaf896a
urmila <urmila@cellpointmobile.com>
2020-07-29 08:18:42 +0530

    CMP-4191: PAL invoice id rule for paypal updated

--- db/setup_pg_v2.22.sql
+++ db/setup_pg_v2.22.sql
@@ -84,3 +84,12 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, "type","scope
 
 --CMP-4185
 INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('invoiceidrule_CCPP_CEBU', 'invoiceid ::= (psp-config/@id)=="40"=(transaction.@id)', true, 10077, 'client', 0);
+
+--CMP-4191
+update client.additionalproperty_tbl set value='orderid ::= (transaction.orderid)
+attempt ::= (@attempt)
+date ::= {date.ymd}
+invoiceid ::= (psp-config/@id)=="24"="NM"<orderid><date><attempt>' where value='orderid ::= (transaction.orderid)
+attempt ::= (@attempt)
+date ::= {date.ymd}
+invoiceid ::= "NM"<orderid><date><attempt>' and key='invoiceidrules' and externalid=10020;
