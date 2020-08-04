8006387f31efa883c552c67432563e400c871b0b
Abhinav <abhinav.shaha@cellpointmobile.com>
2020-08-04 11:50:18 +0000

    CMP-4124 : Worked on review comments

--- webroot/mApp/api/authorize.php
+++ webroot/mApp/api/authorize.php
@@ -943,8 +943,7 @@ try
                                                                     if ($obj_TxnInfo->getCallbackURL() != "") { $obj_Processor->notifyClient(Constants::iPAYMENT_REJECTED_STATE, $aCallbackArgs, $obj_TxnInfo->getClientConfig()->getSurePayConfig($_OBJ_DB)); }
 
                                                                     $aStateId = array(Constants::iPAYMENT_REJECTED_STATE);
-                                                                    $iForeignExchangeId = $obj_TxnInfo->getExternalRef(Constants::iForeignExchange,$obj_TxnInfo->getPSPID());
-                                                                    if($iForeignExchangeId !==null && empty($iForeignExchangeId) === false && sizeof($aStateId)>0) { $obj_Processor->getPSPInfo()->notifyForeignExchange($aStateId,$aHTTP_CONN_INFO['foreign-exchange']); }
+                                                                    $obj_Processor->getPSPInfo()->notifyForeignExchange($aStateId,$aHTTP_CONN_INFO['foreign-exchange']);
 
                                                                     $obj_mPoint->newMessage($obj_TxnInfo->getID(),Constants::iPAYMENT_REJECTED_STATE,'Authorization Declined Due to Failed Fraud Check And Authorization is not attempted');
                                                                     $obj_Processor->getPSPInfo()->updateSessionState(Constants::iPAYMENT_REJECTED_STATE,$obj_Processor->getPSPInfo()->getPSPID(),$obj_TxnInfo->getAmount(),"",null,"",$obj_TxnInfo->getClientConfig()->getSurePayConfig($_OBJ_DB));
