1d361833e699828232522ad83652684c9002d5d1
amar.kumar <amar@cellpointmobile.com>
2020-08-18 12:01:54 +0530

    CMP-4254

--- db/setup_pg_v2.23.sql
+++ db/setup_pg_v2.23.sql
@@ -125,3 +125,12 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) s
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'FareBasisCode', 'BK', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'TravelAgencyName', 'CebuPacificair', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'TravelAgencyCode', '5J', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
+
+-------------G-CASH 2C2P-ALC FOR CEBU START------------
+INSERT INTO system.card_tbl (id, name, position, minlength, maxlength, cvclength, paymenttype) VALUES (95, 'Gcash', 23, -1, -1, -1, 3);
+INSERT INTO System.PSPCard_Tbl (pspid, cardid) VALUES (40, 95);
+INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) SELECT 10077, PC.cardid, PC.pspid 
+FROM System.PSPCard_Tbl PC, Client.Client_Tbl Cl WHERE PC.cardid IN (95,40) AND PC.pspid ='40' GROUP BY PC.cardid, PC.pspid;
+INSERT INTO System.CardPricing_Tbl (cardid, pricepointid) SELECT 95, id FROM System.PricePoint_Tbl WHERE amount = -1 AND currencyid = 608;
+INSERT INTO system.pspcurrency_tbl (currencyid, pspid, name) VALUES (608,40,'PHP');
+-------------G-CASH 2C2P-ALC FOR CEBU END------------
