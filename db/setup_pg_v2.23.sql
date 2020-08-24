e28a6940262b9473327df2fbba0d417584967b8a
Anna Lagad <anna.lagad@cellpointmobile.com>
2020-08-24 15:07:02 +0530

    CMP-4283: Update fraud state descriptions to be used in Vision

--- db/setup_pg_v2.23.sql
+++ db/setup_pg_v2.23.sql
@@ -125,3 +125,23 @@ INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) s
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'FareBasisCode', 'BK', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'TravelAgencyName', 'CebuPacificair', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type,scope) select 'TravelAgencyCode', '5J', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
+
+--------------------------------------------------------------------------------
+----  Update fraud state descriptions
+--------------------------------------------------------------------------------
+UPDATE log.state_tbl SET name='Pre Auth Initiated' WHERE id=3010;
+UPDATE log.state_tbl SET name='Pre Auth Success' WHERE id=3011;
+UPDATE log.state_tbl SET name='Pre Auth Unavbl' WHERE id=3012;
+UPDATE log.state_tbl SET name='Pre Auth Unknown' WHERE id=3013;
+UPDATE log.state_tbl SET name='Pre Auth Review' WHERE id=3014;
+UPDATE log.state_tbl SET name='Pre Auth Fail' WHERE id=3015;
+UPDATE log.state_tbl SET name='Pre Auth Conx Fail' WHERE id=3016;
+
+UPDATE log.state_tbl SET name='Post Auth Initiated' WHERE id=3110;
+UPDATE log.state_tbl SET name='Post Auth Success' WHERE id=3111;
+UPDATE log.state_tbl SET name='Post Auth Unavbl' WHERE id=3112;
+UPDATE log.state_tbl SET name='Post Auth Unknown' WHERE id=3113;
+UPDATE log.state_tbl SET name='Post Auth Review' WHERE id=3114;
+UPDATE log.state_tbl SET name='Post Auth Fail' WHERE id=3115;
+UPDATE log.state_tbl SET name='Post Auth Conx Fail' WHERE id=3116;
+
