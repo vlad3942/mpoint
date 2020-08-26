9cd0f7c2fb0c7916f915b46b8fcb4b10670192d5
Chaitenya Yadav <chaitenya.yadav@cellpointmobile.com>
2020-08-26 21:29:45 +0530

    CMP-4296: update Paypal invoiceid rule for CEBU

---
+++ db/snapshot/20-16-SIT-Build.sql
@@ -0,0 +1,3 @@
+--- CMP-4296 ---
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('invoiceidrule_PAYPAL_CEBU', 'invoiceid ::= (psp-config/@id)=="24"=(transaction.@id)', true, 10077, 'client', 0);
+
