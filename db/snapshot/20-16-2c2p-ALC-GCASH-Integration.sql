481e03c8233c92d1c3dd54effefde6aca1c0df76
amar.kumar <amar@cellpointmobile.com>
2020-08-27 10:32:48 +0530

    CMP-4254

--- db/snapshot/20-16-2c2p-ALC-GCASH-Integration.sql
+++ db/snapshot/20-16-2c2p-ALC-GCASH-Integration.sql
@@ -1,6 +1,6 @@
 -------------G-CASH 2C2P-ALC FOR CEBU------------
+INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) SELECT 10077, PC.cardid, PC.pspid FROM System.PSPCard_Tbl PC, Client.Client_Tbl Cl WHERE PC.cardid IN (93,40) AND PC.pspid ='40' GROUP BY PC.cardid, PC.pspid;
 INSERT INTO Client.CardAccess_Tbl (clientid, cardid, pspid) SELECT <client ID>, PC.cardid, PC.pspid FROM System.PSPCard_Tbl PC, Client.Client_Tbl Cl WHERE PC.cardid IN (93,40) AND PC.pspid ='40' GROUP BY PC.cardid, PC.pspid;
-
 -------------G-CASH 2C2P-ALC FOR CEBU------------
 INSERT INTO system.card_tbl (id, name, position, minlength, maxlength, cvclength, paymenttype) VALUES (93, 'Gcash', 23, -1, -1, -1, 3);
 INSERT INTO System.PSPCard_Tbl (pspid, cardid) VALUES (40, 93);
