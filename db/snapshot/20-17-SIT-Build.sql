b796942e7bb885ad5d149bab271b6a9a4d2c0718
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-09-10 15:41:32 +0530

    CTECH-3765 SQL update

--- db/snapshot/20-17-SIT-Build.sql
+++ db/snapshot/20-17-SIT-Build.sql
@@ -10,17 +10,17 @@ DELETE FROM client.additionalproperty_tbl where key = 'post_fraud_rule';
 DELETE FROM client.additionalproperty_tbl where key = 'mpi_rule';
 ---2c2p-alc Rule---
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<pspid>=="40"
-pspid::=(psp-config.@id)', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=<> AND pspid=40;
+pspid::=(psp-config.@id)', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=40;
 
 ---First Data Rule---
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<status>=="1"OR<status>=="2"OR<status>=="4"
-status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=<> AND pspid=62;
+status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=62;
 ---WorldPay Rule for MPI---
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'mpi_rule', 'isSkippAuth::=<status>!=="1"AND<status>!=="2"AND<status>!=="4"AND<status>!=="5"AND<status>!=="6"
-status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
+status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=4;
 ---WorldPay Rule for FRAUD---
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<status>=="1"OR<status>=="4"
-status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=<> AND pspid=4;
+status::=(card.info-3d-secure.additional-data.param[@name=''status''])', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=4;
 
 update client.cardaccess_tbl set enabled = false where psp_type in (9,10) and cardid not in (7,8) and clientid = 10077;
 
