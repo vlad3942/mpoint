a665b7dcefdc3ccfc7ed1a34433f923ac5c70138
Anna Lagad <anna.lagad@cellpointmobile.com>
2020-09-21 13:45:08 +0530

    CMP-4291: Move salt param at the end

--- api/classes/validate.php
+++ api/classes/validate.php
@@ -1230,7 +1230,7 @@ class Validate extends ValidateBase
 
 		$chk = hash('sha512',$obj_ClientConfig->getID() . $orderno . $amount . $countryid . $mobile . $country_id . $obj_ClientInfo->getEMail() . $obj_ClientInfo->getDeviceID() . $obj_ClientConfig->getSalt());
 		$chkWithCountryISOCode = hash('sha512',$obj_ClientConfig->getID() . $orderno . $amount . $countryISOCode . $mobile . $countryISO_id . $obj_ClientInfo->getEMail() . $obj_ClientInfo->getDeviceID() . $obj_ClientConfig->getSalt());
-        $chkWithAuthTokenCustRefCustId = hash('sha512',$obj_ClientConfig->getID() . $orderno . $amount . $countryid . $mobile . $country_id . $obj_ClientInfo->getEMail() . $obj_ClientInfo->getDeviceID() . $obj_ClientConfig->getSalt(). $authToken. $obj_ClientInfo->getCustomerRef() . $obj_ClientInfo->getProfileID());
+        $chkWithAuthTokenCustRefCustId = hash('sha512',$obj_ClientConfig->getID() . $orderno . $amount . $countryid . $mobile . $country_id . $obj_ClientInfo->getEMail() . $obj_ClientInfo->getDeviceID() . $authToken. $obj_ClientInfo->getCustomerRef() . $obj_ClientInfo->getProfileID() . $obj_ClientConfig->getSalt());
 		if (strtolower($mac) === strtolower($chk) || strtolower($mac) === strtolower($chkWithCountryISOCode) || strtolower($mac) === strtolower($chkWithAuthTokenCustRefCustId))
 		{
 			$code = 10;
