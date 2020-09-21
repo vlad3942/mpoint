85613d04342d1db48924990ef9a9e466d4d3d41f
Sagar Narayane <sagarnarayane@gmail.com>
2020-09-21 16:29:14 +0530

    CMP-4359
    Add GooglePubSub library in mPoint

---
+++ test/api/GooglePubSubTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sagar Narayane
+ * Copyright: Cellpoint Mobile
+ * Link: http://www.cellpointmobile.com
+ * Project: server
+ * Package:
+ * File Name: GooglePubSubTest
+ */
+
+require_once __DIR__ . '/../../webroot/inc/include.php';
+require_once __DIR__ . '/../inc/testinclude.php';
+
+use api\classes\messagequeue\client\GooglePubSub;
+use Google\Cloud\PubSub\PubSubClient;
+
+class GooglePubSubTest extends baseAPITest
+{
+    protected $_aMessage_Queue_Provider_info;
+
+    public function setUp()
+    {
+        parent::setUp(FALSE);
+        global $aMessage_Queue_Provider_info;
+        //$this->bIgnoreErrors = true;
+        $this->_aMessage_Queue_Provider_info = $aMessage_Queue_Provider_info;
+    }
+
+    public function testSuccessfulPublish()
+    {
+        $keyFile = $this->_aMessage_Queue_Provider_info['keyfile'];
+        $projectId = $this->_aMessage_Queue_Provider_info['projectid'];
+        $topicName = $this->_aMessage_Queue_Provider_info['topicname'];
+        $_mqClient = new GooglePubSub($keyFile, $projectId, $topicName);
+        $_mqClient->authenticate();
+
+        $getMockMethod = self::getReflectionMethod('api\classes\messagequeue\client\GooglePubSub', 'getMessageQueueClient');
+        $getMessageQueueClientResponse = $getMockMethod->invoke($_mqClient);
+
+        $this->assertInstanceOf(PubSubClient::class,$getMessageQueueClientResponse);
+
+        try {
+            $thisResponse =  $_mqClient->publish("Hello");
+            $this->assertTrue($thisResponse);
+        }
+        catch (Exception $exception)
+        {
+            $this->assertTrue(false);
+        }
+    }
+
+
+    public function testUnSuccessfulPublish()
+    {
+        $keyFile = $this->_aMessage_Queue_Provider_info['keyfile'];
+        $projectId = $this->_aMessage_Queue_Provider_info['projectid'];
+        $topicName = "WrongTopic";
+        $_mqClient = new GooglePubSub($keyFile, $projectId, $topicName);
+        $_mqClient->authenticate();
+
+        $getMockMethod = self::getReflectionMethod('api\classes\messagequeue\client\GooglePubSub', 'getMessageQueueClient');
+        $getMessageQueueClientResponse = $getMockMethod->invoke($_mqClient);
+
+        $this->assertInstanceOf(PubSubClient::class,$getMessageQueueClientResponse);
+
+        try {
+            $thisResponse =  $_mqClient->publish("Hello");
+            $this->assertFalse($thisResponse);
+        }
+        catch (Exception $exception)
+        {
+            $this->assertTrue(True);
+        }
+
+
+    }
+
+    public function test__constructWithoutKeyFile()
+    {
+
+        $keyFile = NUll;
+        $projectId = $this->_aHTTP_CONN_INFO['projectid'];
+        $topicName = $this->_aHTTP_CONN_INFO['topicname'];
+        $_mqClient = new GooglePubSub($keyFile, $projectId, $topicName);
+    }
+}
