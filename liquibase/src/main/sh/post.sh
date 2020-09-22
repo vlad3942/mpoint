c1a02a455699b8a2b89eab130a9f63ce65c7d3f0
Jona <jonatan.buus@cellpointmobile.com>
2020-09-22 11:30:18 +0200

    CMP-4423 Added generalized files for building the Docker image in which the database runs: - docker.gradle: Tells gradle how to build the Docker Image for mPoint's database - Dockerfile: Tells Docker how to build the Docker Image for mPoint's database - pre.sh: Runs  before Liquibase when the container can connect to PostGreSQL - post.sh: Runs after Liquibase has created the database

---
+++ liquibase/src/main/sh/post.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export PGHOST=${DB_HOST}
+export PGUSER=${DB_USER}
+export PGPASSWORD=${DB_PASSWORD}
+export PGDATABASE=${DB_DBNAME}
+export PGPORT=${DB_PORT:-5432}
+
+if [ -z "$APP_USER" ]; then
+  APP_USER="mpoint"
+fi
+
+if [ -z $DEBUG ]; then
+  set -x
+fi
+
+echo "This script run after liquibase and will set the password for the database user: ${APP_USER}"
+
+USER_EXISTS=$(echo "SELECT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = '${APP_USER}');" | psql -t | tr -d '[:space:]')
+
+UPDATE_USER_PASS="ALTER ROLE ${APP_USER} WITH LOGIN PASSWORD '${APP_PASS}'"
+
+if [ "$USER_EXISTS" = "t" ]; then
+    echo "USER EXISTS, updating ${APP_USER} password"
+    echo "${UPDATE_USER_PASS}" | psql
+fi
