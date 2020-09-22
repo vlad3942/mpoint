d93a9142cc76976e53ab90de62ff8c01acd6e76c
Jona <jonatan.buus@cellpointmobile.com>
2020-09-22 10:31:37 +0200

    CMP-4423 Updated the SQL file with DDL statements from the backup of mPoint's database schema: schema/schema_import.1.0.0.sql
    Improved the SQL file with DDL statements for permissions: acl/acl.1.0.0.sql so it gracefully handles the following scenarios:
    - Creation of a new role: mpoint if the role doesn't exists
    - Installation of the procedural language: plpgsql if the language doesn't exists

--- liquibase/src/main/resources/liquibase/db/acl/acl.1.0.0.sql
+++ liquibase/src/main/resources/liquibase/db/acl/acl.1.0.0.sql
@@ -1,2 +1,16 @@
-DROP ROLE IF EXISTS mpoint;
-CREATE ROLE mpoint;
+DO $$
+BEGIN
+    CREATE ROLE mpoint;
+    EXCEPTION WHEN duplicate_object THEN
+        RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
+
+DO $$
+BEGIN
+    CREATE TRUSTED PROCEDURAL LANGUAGE 'plpgsql' HANDLER plpgsql_call_handler VALIDATOR plpgsql_validator;
+    ALTER LANGUAGE plpgsql OWNER TO postgres;
+    EXCEPTION WHEN duplicate_object THEN
+        RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
