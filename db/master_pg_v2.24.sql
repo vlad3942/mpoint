b9f6fdeaf45aa8c9e9de72b0efeb598f35417917
Anna Lagad <anna.lagad@cellpointmobile.com>
2020-09-24 11:39:13 +0530

    CMP-4376: Spelling mistake in name of the column

--- db/master_pg_v2.24.sql
+++ db/master_pg_v2.24.sql
@@ -1,2 +1,6 @@
 -- Change the code column datatype to var_char from integer
 ALTER TABLE system.country_tbl  ALTER COLUMN code type character varying(5)
+
+-- Rename column name
+ALTER TABLE log.transaction_tbl 
+RENAME COLUMN convetredcurrencyid TO convertedcurrencyid;
