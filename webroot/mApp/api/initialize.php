44e56e4a7c6ab08178ef41e5983ec6abc0a40424
Sagar Narayane <sagarnarayane@gmail.com>
2020-10-08 15:23:15 +0530

    CMP-4482
    [FOP] Unable to get Stored Cards for one of the country selected for Initialization.

--- webroot/mApp/api/initialize.php
+++ webroot/mApp/api/initialize.php
@@ -664,11 +664,11 @@ if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PH
                                         $obj_SR = StaticRoute::produceConfigurations($_OBJ_DB, $_OBJ_TXT, $obj_TxnInfo, $obj_PaymentMethods);
                                         $paymentMethodCount = count($obj_SR);
                                         $obj_XML = '<cards>';
-                                        for($p=1; $paymentMethodCount >= $p; $p++)
+                                        foreach ($obj_SR as $key => $value)
                                         {
-                                            if(($obj_SR[$p] instanceof StaticRoute) === true )
+                                            if(($value instanceof StaticRoute) === true )
                                             {
-                                                $obj_XML .= $obj_SR[$p]->toXML();
+                                                $obj_XML .= $value->toXML();
                                             }
                                         }
                                         $obj_XML .= '</cards>';
