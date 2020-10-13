192c78e71e2b9ed97940641e0ffe8b68290f9879
LAPTOP-U16E8CNF\Sagar Badave <sagar.badave@cellpointmobile.com>
2020-10-13 22:44:29 +0530

    CMP-4509  mPoint earth release hotfix 2.23 Production Scripts

--- db/snapshot/prod_earth_hotfix_v2.23.sql
+++ db/snapshot/prod_earth_hotfix_v2.23.sql
@@ -1,5 +1,5 @@
 --CMP-4471 [Chase Payment] Limit process file additional property	CMP-4471[Chase Payment] Limit process file additional property--
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, scope) select 'MAX_DOWNLOAD_FILE_LIMIT', '1', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=10069 AND pspid=52;
+INSERT INTO client.additionalproperty_tbl (id,key, value, externalid, type, scope) select (SELECT max(id)+1 FROM client.additionalproperty_tbl),'MAX_DOWNLOAD_FILE_LIMIT', '1', id, 'merchant',2 from client.merchantaccount_tbl WHERE clientid=10069 AND pspid=52;
 --END CMP-4471 [Chase Payment] Limit process file additional property	CMP-4471[Chase Payment] Limit process file additional property--
 
 UPDATE client.additionalproperty_tbl SET value='4' WHERE id=239 and externalid=428;
