ff7c0cd2f46c36a8f221bd91c3c12ce151bcbfaf
Karishan Kumar <Karishan.Kumar@cellpointmobile.com>
2020-10-28 00:34:53 +0530

    CMP-4536 include verificaitonn class and check sso preference

--- webroot/mApp/api/pay.php
+++ webroot/mApp/api/pay.php
@@ -161,6 +161,7 @@ require_once(sCLASS_PATH . '/payment_route.php');
 require_once(sCLASS_PATH ."/grabpay.php");
 // Require specific Business logic for the Paymaya component
 require_once(sCLASS_PATH .'/apm/paymaya.php');
+require_once(sCLASS_PATH .'/verification/verification.php');
 
 $aMsgCds = array();
 
@@ -318,6 +319,12 @@ if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PH
                                 }
                             }
 
+						}
+						// sso verification conditions checking 
+						$sso_response = Verification::verify($_OBJ_DB, $obj_ClientConfig, $obj_DOM, $i, $_OBJ_TXT);
+						if(count($sso_response) > 0 )
+						{
+							$aMsgCds[$sso_response['code']] = $sso_response['msg'] ;
 						}
 
 						// Success: Input Valid
