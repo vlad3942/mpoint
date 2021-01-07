7bde63dbfbd9e9e7927cbe688a8cc3eb047fa62b
Anna Lagad <anna.lagad@cellpointmobile.com>
2021-01-07 10:55:24 +0530

    CMP-4749: phpunit fixes

--- api/classes/routing_service.php
+++ api/classes/routing_service.php
@@ -243,7 +243,7 @@ class RoutingService extends General
             $obj_HTTP = new HTTPClient(new Template(), $obj_ConnInfo);
             $obj_HTTP->connect();
             $code = $obj_HTTP->send($this->constHTTPHeaders(), $b);
-            $obj_HTTP->disConnect(); print_r($obj_HTTP->getReplyBody());exit;
+            $obj_HTTP->disConnect();
             $obj_XML = simplexml_load_string($obj_HTTP->getReplyBody());
             if($obj_XML instanceof SimpleXMLElement){
                 return RoutingServiceResponse::produceGetRouteResponse($obj_XML);
