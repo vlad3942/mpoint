08b771349b73be0a3c4869f83653983eeffacd35
anna <anna.lagad@cellpointmobile.com>
2021-03-08 15:03:53 +0530

    CMP-4901: Validation API to validate rule

---
+++ api/classes/crs/ValidateRule.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Anna Lagad
+ * Copyright: Cellpoint Digital
+ * Link: http://www.cellpointdigital.com
+ * Project: mPoint
+ * File Name:ValidateRule.php
+ */
+
+class ValidateRule
+{
+
+	/**
+	 * Hold Unique ID of Route Config
+	 *
+	 * @var int
+	 */
+	private int $_iRouteConfigId;
+
+    /**
+     * Hold Route Configuration count
+     *
+     * @var int
+     */
+    private int $_iRouteConfigCount;
+
+	public function __construct(int $routeConfigId, int $routeConfigCount)
+	{
+        $this->_iRouteConfigId = $routeConfigId;
+        $this->_iRouteConfigCount = $routeConfigCount;
+	}
+
+	public function getRouteConfigCount()
+    {
+        return $this->_iRouteConfigCount;
+    }
+
+    public function getRouteConfigId()
+    {
+        return $this->_iRouteConfigId;
+    }
+
+    public function toXML(array $aMissingRouteConfiguration)
+    {
+        $xml = '';
+        $xml .= '<status>';
+       if(empty($aMissingRouteConfiguration) === true) {
+           $xml .= '<code>200</code>';
+           $xml .= '<description>Success</description>';
+       } else {
+           $xml .= '<code>404</code>';
+           $xml .= '<description>Configuration Not Found For Route : '.implode(',', $aMissingRouteConfiguration).'</description>';
+       }
+        $xml .= '</status>';
+       return $xml;
+    }
+    public function produceConfig(RDB $oDB, int $routeId, array $aCards, array $aCountries, array $aCurrencies)
+    {
+        $obj_RuleValidation = null;
+        try {
+            $sql = "SELECT COUNT (*) AS count 
+                        FROM Client" . sSCHEMA_POSTFIX . ".Routeconfig_Tbl RC
+                            INNER JOIN Client" . sSCHEMA_POSTFIX . ".RouteCountry_Tbl RCON ON RC.id = RCON.routeconfigid AND RCON.enabled = '1'
+                            INNER JOIN Client" . sSCHEMA_POSTFIX . ".RouteCurrency_Tbl RCUR ON RC.id = RCUR.routeconfigid AND RCUR.enabled = '1' 
+                            INNER JOIN Client" . sSCHEMA_POSTFIX . ".Route_Tbl R ON RC.routeid = R.id AND R.enabled = '1'
+                            INNER JOIN Client" . sSCHEMA_POSTFIX . ".Account_Tbl A ON R.clientid = A.clientid AND A.enabled = '1'
+                            INNER JOIN Client" . sSCHEMA_POSTFIX . ".MerchantSubAccount_Tbl MSA ON A.id = MSA.accountid AND MSA.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".PSP_Tbl PSP ON R.providerid = PSP.id AND MSA.pspid = PSP.id AND PSP.system_type NOT IN (" . Constants::iPROCESSOR_TYPE_TOKENIZATION . "," . Constants::iPROCESSOR_TYPE_PRE_FRAUD_GATEWAY . "," . Constants::iPROCESSOR_TYPE_POST_FRAUD_GATEWAY . ") AND PSP.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".PSPCurrency_Tbl PC ON PSP.id = PC.pspid  AND PC.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".PSPCard_Tbl PCD ON PSP.id = PCD.pspid AND PCD.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".Card_Tbl C ON C.id = PCD.cardid AND C.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".CardPricing_Tbl CP ON C.id = CP.cardid AND CP.enabled = '1'
+                            INNER JOIN System" . sSCHEMA_POSTFIX . ".PricePoint_Tbl PP ON CP.pricepointid = PP.id AND PC.currencyid = PP.currencyid AND PP.enabled = '1'
+                        WHERE RC.id = " . $routeId . "
+                        AND C.id IN (" . implode(",", $aCards) . ")";
+            if (empty($aCountries) === false) {
+                $sql .= " AND (RCON.countryid IN ( " . implode(",", $aCountries) . ") OR RCON.countryid IS NULL)";
+            }
+            if (empty($aCurrencies) === false) {
+                $sql .= " AND PP.currencyid IN (" . implode(",", $aCurrencies) . ")";
+                $sql .= " AND PC.currencyid IN (" . implode(",", $aCurrencies) . ")";
+                $sql .= " AND (RCUR.currencyid IN(" . implode(",", $aCurrencies) . ") OR RCUR.currencyid IS NULL)";
+            }
+            $sql .= "AND RC.enabled = '1'";
+
+            $result = $oDB->getName($sql);
+            if(count($result) > 0)
+            {
+                $obj_RuleValidation = new ValidateRule($routeId, $result['COUNT']);
+            }
+        }catch (SQLQueryException $e){
+            trigger_error($e->getMessage(), E_USER_ERROR);
+        }
+        return $obj_RuleValidation;
+    }
+
+}
+?>
---
+++ webroot/mApp/api/validate_rule.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * @author Anna Lagad
+ * @copyright Cellpoint Digital
+ * @link http://www.cellpointdigital.com
+ * @package mPoint
+ * @version 1.0
+ */
+
+// Require Global Include File
+require_once("../../inc/include.php");
+// Require API for Simple DOM manipulation
+require_once(sAPI_CLASS_PATH ."simpledom.php");
+// Require Business logic for the validating client Input
+require_once(sCLASS_PATH ."validate.php");
+require_once(sCLASS_PATH ."crs/ValidateRule.php");
+
+/*
+$_SERVER['PHP_AUTH_USER'] = "CPMDemo";
+$_SERVER['PHP_AUTH_PW'] = "DEMOisNO_2";
+
+$HTTP_RAW_POST_DATA = '<?xml version="1.0" encoding="UTF-8"?>';
+$HTTP_RAW_POST_DATA .= '<root>';
+$HTTP_RAW_POST_DATA .= '<validate_rule_request>';
+$HTTP_RAW_POST_DATA .= '<cards>';
+$HTTP_RAW_POST_DATA .= '<card_id>8</card_id>';
+$HTTP_RAW_POST_DATA .= '<card_id>7</card_id>';
+$HTTP_RAW_POST_DATA .= '</cards>';
+$HTTP_RAW_POST_DATA .= '<currencies>';
+$HTTP_RAW_POST_DATA .= '<currency_id>644</currency_id>';
+$HTTP_RAW_POST_DATA .= '<currency_id>356</currency_id>';
+$HTTP_RAW_POST_DATA .= '</currencies>';
+$HTTP_RAW_POST_DATA .= '<countries>';
+$HTTP_RAW_POST_DATA .= '<country_id>200</country_id>';
+$HTTP_RAW_POST_DATA .= '<country_id>603</country_id>';
+$HTTP_RAW_POST_DATA .= '</countries>';
+$HTTP_RAW_POST_DATA .= '<route_configurations>';
+$HTTP_RAW_POST_DATA .= '<route_id>12</route_id>';
+$HTTP_RAW_POST_DATA .= '<route_id>78</route_id>';
+$HTTP_RAW_POST_DATA .= '</route_configurations>';
+$HTTP_RAW_POST_DATA .= '</validate_rule_request>';
+$HTTP_RAW_POST_DATA .= '</root>';
+*/
+
+$xml = '';
+$obj_DOM = simpledom_load_string(file_get_contents('php://input'));
+//print_r($obj_DOM);
+if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PHP_AUTH_PW", $_SERVER) === true)
+{
+    if ( ($obj_DOM instanceof SimpleDOMElement) === true && $obj_DOM->validate(sPROTOCOL_XSD_PATH ."mpoint.xsd") === true && count($obj_DOM->{'validate_rule_request'}) > 0)
+    {
+        $obj_mPoint = new General($_OBJ_DB, $_OBJ_TXT);
+        $aCards = (array) $obj_DOM->{'validate_rule_request'}->cards->{'card_id'} ?? null;
+        $aCountries = (array) $obj_DOM->{'validate_rule_request'}->countries->{'country_id'} ?? null;
+        $aCurrencies = (array) $obj_DOM->{'validate_rule_request'}->currencies->{'currency_id'} ?? null;
+        $sRoutes = (array) $obj_DOM->{'validate_rule_request'}->{'route_configurations'}->{'route_id'} ?? null;
+        $aMissingRouteConfiguration = array();
+        foreach ($sRoutes as $route){
+            $obj_validateRule = ValidateRule::produceConfig($_OBJ_DB, $route, $aCards, $aCountries, $aCurrencies);
+            if(empty($obj_validateRule) === false){
+                if(!$obj_validateRule->getRouteConfigCount()){
+                    $aMissingRouteConfiguration[] = $obj_validateRule->getRouteConfigId();
+                }
+            }
+        }
+        $xml .= $obj_validateRule->toXML($aMissingRouteConfiguration);
+    }
+    // Error: Invalid XML Document
+    elseif ( ($obj_DOM instanceof SimpleDOMElement) === false)
+    {
+        header("HTTP/1.1 415 Unsupported Media Type");
+        $xml = '<status code="415">Invalid XML Document</status>';
+    }
+    // Error: Wrong operation
+    elseif (count($obj_DOM->{'validate_rule_request'}) == 0)
+    {
+        header("HTTP/1.1 400 Bad Request");
+        $xml = '';
+        foreach ($obj_DOM->children() as $obj_Elem)
+        {
+            $xml .= '<status code="400">Wrong operation: '. $obj_Elem->getName() .'</status>';
+        }
+    }
+    // Error: Invalid Input
+    else
+    {
+        header("HTTP/1.1 400 Bad Request");
+        $aObj_Errs = libxml_get_errors();
+        $xml = '';
+        for ($i=0; $i<count($aObj_Errs); $i++)
+        {
+            $xml .= '<status code="400">'. htmlspecialchars($aObj_Errs[$i]->message, ENT_NOQUOTES) .'</status>';
+        }
+    }
+}
+else
+{
+    header("HTTP/1.1 401 Unauthorized");
+
+    $xml = '<status code="401">Authorization required</status>';
+}
+header("Content-Type: text/xml; charset=\"UTF-8\"");
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<root>';
+echo $xml;
+echo '</root>';
+?>
