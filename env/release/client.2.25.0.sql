403277eb1e3670ad20a3f63c2a45d4bd7ecf4d11
vikas gupta <vikas.gupta@cellpointmobile.com>
2021-03-09 15:50:19 +0530

    CMP-5212 Update Timeout for Web

--- env/release/client.2.25.0.sql
+++
@@ -1,6 +0,0 @@
--- mPoint DB Scripts :
-
---Table Name : Client.Additionalproperty_Tbl
-
--- CMP-5212
-UPDATE client.additionalproperty_tbl SET value = 13 WHERE externalid = 10077 and key = 'webSessionTimeout';
