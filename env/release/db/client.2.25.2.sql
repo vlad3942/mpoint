02696c5586dba800bce7d62c65892373a1890593
vikas gupta <vikas.gupta@cellpointmobile.com>
2021-03-10 19:54:09 +0530

    Online Banking issue

--- env/release/db/client.2.25.2.sql
+++ env/release/db/client.2.25.2.sql
@@ -3,3 +3,9 @@
 UPDATE client.cardaccess_tbl
 SET psp_type = 7
 WHERE clientid = 10018 and cardid = 73 and pspid = 51 and id = 1452;
+
+
+--Prod PPRO Alipay config. change
+UPDATE client.cardaccess_tbl
+SET psp_type=4
+WHERE clientid=10098 and cardid=32 and pspid=46 and id=1589;
