c59a2a1e357caaaf31771e9ac51f8def3a78b226
vikas gupta <vikas.gupta@cellpointmobile.com>
2021-04-01 16:26:28 +0530

    CMP-5338 SQL for Enable Alternate Routing

---
+++ env/dev/db/client.2.26.0.sql
@@ -0,0 +1,6 @@
+-- mPoint DB Scripts :
+
+-- CMP-5338
+--Table Name : Client.Additionalproperty
+-- Cebu Client
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type) VALUES ('PAYMENT_RETRY_WITH_ALTERNATE_ROUTE', 'true', true, 10077, 'client');
