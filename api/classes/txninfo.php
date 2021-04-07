bcaec75e2fa974cd3199998aa9ffe7a8fef4821e
kalpesh <kalpesh.parikh@cellpointmobile.com>
2021-04-07 16:27:21 +0530

    CMP-5248 Review comment addressed - Reverted formatting changes

--- api/classes/txninfo.php
+++ api/classes/txninfo.php
@@ -2204,14 +2204,15 @@ class TxnInfo
 		$aReturnValue = "";
 		if( is_array($aPassengerData) === true )
 		{
+
 			    $sql = "SELECT Nextvalue('Log".sSCHEMA_POSTFIX.".passenger_Tbl_id_seq') AS id FROM DUAL";
 				$RS = $obj_DB->getName($sql);
 				// Error: Unable to generate a new Passenger ID
 				if (is_array($RS) === false) { throw new mPointException("Unable to generate new Passenger ID", 1001); }
 	
 				
-				$sql = "INSERT INTO Log".sSCHEMA_POSTFIX.".passenger_tbl(id, first_name, last_name, type, order_id, created, modified, title, email, mobile, country_id, amount, seq)
-				VALUES(". $RS["ID"] .", '". $aPassengerData["first_name"] ."', '". $aPassengerData["last_name"] ."','". $aPassengerData["type"] ."', ". $aPassengerData["order_id"] .", now(), now(), '". $aPassengerData["title"] ."', '". $aPassengerData["email"] ."', '". $aPassengerData["mobile"] ."', '". $aPassengerData["country_id"] ."',". $aPassengerData["amount"] .", ". $aPassengerData["seq"] .")";
+					$sql = "INSERT INTO Log".sSCHEMA_POSTFIX.".passenger_tbl(id, first_name, last_name, type, order_id, created, modified, title, email, mobile, country_id, amount, seq)
+					VALUES(". $RS["ID"] .", '". $aPassengerData["first_name"] ."', '". $aPassengerData["last_name"] ."','". $aPassengerData["type"] ."', ". $aPassengerData["order_id"] .", now(), now(), '". $aPassengerData["title"] ."', '". $aPassengerData["email"] ."', '". $aPassengerData["mobile"] ."', '". $aPassengerData["country_id"] ."',". $aPassengerData["amount"] .", ". $aPassengerData["seq"] .")";
 
 				// Error: Unable to insert a new passenger record in the Passenger Table
 				$this->setAdditionalDetails($obj_DB, $aAdditionalDatas, $RS["ID"]);
@@ -2222,6 +2223,7 @@ class TxnInfo
 				else
 				{
 					$aReturnValue = $RS["ID"];
+
 				}
 	
 			
