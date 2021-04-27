3778cd84571f81ef1297180be03607880c2b980b
Chaitenya Yadav <chaitenya.yadav@cellpointmobile.com>
2021-04-27 15:06:31 +0530

    CTECH-4674: Split Payment configs Prod SQLs for mPoint

--- env/release/db/release-akash/client.2.25.0.sql
+++ env/release/db/release-akash/client.2.25.0.sql
@@ -13,3 +13,12 @@ SELECT id, 'travelfund', 2, 'travelfund', 'travelfund', 'travelfund', enabled FR
 INSERT INTO client.routecountry_tbl(routeconfigid) SELECT rc.id FROM client.routeconfig_tbl rc inner join client.route_tbl r on r.id = rc.routeid WHERE r.clientid = 10077 and mid ='travelfund';
  
 INSERT INTO client.routecurrency_tbl(routeconfigid) select rc.id from client.routeconfig_tbl rc INNER JOIN client.route_tbl r on r.id = rc.routeid where r.clientid = 10077 and rc.mid = 'travelfund';
+
+
+-- Split payment configs
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('SplitPaymentFOPConfig', '{"1": -1, "2": -1}', false, 10077, 'client', 0);
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('SplitPaymentConfig', '{"split_count":2,"combinations":[{"combination":[{"index":1,"id":1,"is_clubbable":true},{"index":2,"id":2,"is_clubbable":true}]}]}', false, 10077, 'client', 0);
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('sessiontype', '2', false, 10077, 'client', 0);
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('isVoucherPreferred', 'false', false, 10077, 'client', 0);
+INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES ('PROXY_CALLBACK', 'https://mpoint.prod-01.cellpoint.cloud/cebu/callback.php', true, 10077, 'client', 0);
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type, enabled, scope) VALUES ('max_session_retry_count', 1000, 10077, 'client', true, 0);
