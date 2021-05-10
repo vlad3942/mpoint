4412f62693c6a60ba3cc4ab9a1ca6552d57c11f5
Priya Alamwar <priya.alamwar@cellpointmobile.com>
2021-05-10 16:00:36 +0530

    CMP-5464 Link transactions created by mPoint in Authorization (for split payment)

--- api/classes/general.php
+++ api/classes/general.php
@@ -562,8 +562,14 @@ class General
              $data["conversion-rate"] = 1;
              $txnInfo->setFXServiceTypeID(0);
              $obj_AssociatedTxnInfo = TxnInfo::produceInfo($iAssociatedTxnId, $this->getDBConn(), $txnInfo, $data);
+             //link parent transaction to new created txn
+             $index = count($additionalTxnData);
+             $additionalTxnData[$index]['name'] = 'linked_txn_id';
+             $additionalTxnData[$index]['value'] = (string)$txnInfo->getID();
+             $additionalTxnData[$index]['type'] = 'Transaction';
              if (count($additionalTxnData) > 0) {
                  $obj_AssociatedTxnInfo->setAdditionalDetails($this->getDBConn(), $additionalTxnData, $iAssociatedTxnId);
+                 //link new transaction to parent txn
                  $additionalData[0]['name'] = 'linked_txn_id';
                  $additionalData[0]['value'] = (string)$iAssociatedTxnId;
                  $additionalData[0]['type'] = 'Transaction';
