01835ddad78bd50a0e8ebd68ab511f5540878acd
Omkar Shirole <omkar.shirole@cellpointmobile.com>
2021-05-12 17:36:22 +0530

    CMP-5419: Unit test cases added

--- test/api/PaymayaAcqAuthTest.php
+++ test/api/PaymayaAcqAuthTest.php
@@ -7,15 +7,15 @@ class PaymayaAcqAuthTest extends AuthorizeAPITest
     public function testSuccessfulAuthorize()
     {
         $pspID = Constants::iPAYMAYA_ACQ;
-        // $pspID = Constants::iWORLDPAY_PSP;
         $clientId = 10099;
         $accountId = 1100;
         $countryId = 640;
         $amount = 5000;
         $endUserAccountId = 5001;
         $currencyId = 608;
-        $cardId = 7;
+        $cardId = Constants::iMASTERCARD;
         $profilePass = 'profilePass';
+        $transcationId = 1001001;
 
         $sCallbackURL = $this->_aMPOINT_CONN_INFO["protocol"] ."://". $this->_aMPOINT_CONN_INFO["host"]. "/_test/simulators/mticket/callback.php";
 
@@ -30,9 +30,10 @@ class PaymayaAcqAuthTest extends AuthorizeAPITest
         $this->queryDB("INSERT INTO EndUser.CLAccess_Tbl (clientid, accountid) VALUES ($clientId, $endUserAccountId)");
         $this->queryDB("INSERT INTO EndUser.Card_Tbl (id, accountid, cardid, pspid, mask, expiry, preferred, clientid, name, ticket, card_holder_name) VALUES (61775, $endUserAccountId, $cardId, $pspID, '542606******4979', '12/21', TRUE, $clientId, NULL, '1767989 ### CELLPOINT ### 100 ### DKK', NULL);");
         $this->queryDB("INSERT INTO log.session_tbl (id, clientid, accountid, currencyid, countryid, stateid, orderid, amount, mobile, deviceid, ipaddress, externalid, sessiontypeid) VALUES (1, $clientId, $accountId, $currencyId, $countryId, 4001, '103-1418291', $amount, 9876543210, '', '127.0.0.1', -1, 1);");
-        $this->queryDB("INSERT INTO Log.Transaction_Tbl (id, typeid, clientid, accountid, keywordid, pspid, euaid, countryid, orderid, callbackurl, amount, ip, enabled, currencyid,sessionid,convertedamount,convertedcurrencyid) VALUES (1001001, 100, $clientId, $accountId, 1,  $pspID, $endUserAccountId, $countryId, '103-1418291', '". $sCallbackURL ."', $amount, '127.0.0.1', TRUE, $currencyId, 1,$amount,$currencyId)");
+        $this->queryDB("INSERT INTO Log.Transaction_Tbl (id, typeid, clientid, accountid, keywordid, pspid, euaid, countryid, orderid, callbackurl, amount, ip, enabled, currencyid,sessionid,convertedamount,convertedcurrencyid) VALUES ($transcationId, 100, $clientId, $accountId, 1,  $pspID, $endUserAccountId, $countryId, '103-1418291', '". $sCallbackURL ."', $amount, '127.0.0.1', TRUE, $currencyId, 1,$amount,$currencyId)");
 
-        $xml = $this->getAuthDoc($clientId, $accountId, 1001001, $amount, $profilePass, 0, '', null, null, $cardId);
+        
+        $xml = $this->getAuthDoc($clientId, $accountId, $transcationId, $amount, $profilePass, 0, '', null, null, $cardId);
 
         $this->_httpClient->connect();
 
@@ -42,5 +43,23 @@ class PaymayaAcqAuthTest extends AuthorizeAPITest
         $this->assertEquals(200, $iStatus);
         $this->assertEquals('<?xml version="1.0" encoding="UTF-8"?><root><status code="2000">Payment authorized</status></root>', $sReplyBody);
 
+        $res =  $this->queryDB("SELECT stateid FROM Log.Message_Tbl WHERE txnid = $transcationId ORDER BY ID ASC");
+        $this->assertTrue(is_resource($res) );
+
+        $aStates = array();
+        while ($row = pg_fetch_assoc($res) )
+        {
+            $aStates[] = $row["stateid"];
+        }
+
+        $this->assertEquals(1, count($aStates) );
+
+        $s = 0;
+        $this->assertEquals(Constants::iPAYMENT_WITH_ACCOUNT_STATE, $aStates[$s++]);
+
+        $res =  $this->queryDB("SELECT t.euaid, et.accountid FROM Log.Transaction_Tbl t LEFT JOIN Enduser.Transaction_Tbl et ON et.txnid = t.id WHERE t.id = $transcationId");
+        $this->assertTrue(is_resource($res) );
+        $row = pg_fetch_assoc($res);
+        $this->assertEquals(5001, $row["euaid"]);
     }
 }
