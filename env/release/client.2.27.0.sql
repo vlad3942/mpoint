391ac77639fbe7cec397ecf5f5ee86bd2b9c8da7
Priya Alamwar <priya.alamwar@cellpointmobile.com>
2021-05-18 12:02:54 +0530

    CMP-5444 Onboard merchant configs required for DeviceFingerPrint and Adobe Target Script

--- env/release/client.2.27.0.sql
+++ env/release/client.2.27.0.sql
@@ -2,8 +2,8 @@
 -- mPoint DB Scripts :
 
 -- CMP-5444
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('DFP_GENERATION','true',<client-id>,'merchant',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_ORGID','k8vif92e',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_MID','avianca_master',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT','true',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT_PATH','6ac3e976c146/92ff2d2716e2/launch-18e8855fefb3.min.js',<client-id>,'client',2)
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('DFP_GENERATION','true',<client-id>,'merchant',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_ORGID','k8vif92e',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_MID','avianca_master',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT','true',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT_PATH','6ac3e976c146/92ff2d2716e2/launch-18e8855fefb3.min.js',<client-id>,'client',2);
--- env/sit/db/client.av_properties.sql
+++ env/sit/db/client.av_properties.sql
@@ -8,11 +8,11 @@ insert into client.additionalproperty_tbl (key,value,externalid,type,scope) valu
 insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('TravelAgencyCode','av',<WP-merchant-id>,'merchant',2);
 insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('TravelAgencyName','Avinca',<WP-merchant-id>,'merchant',2);
 
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('AIRLINE_NUMRIC_CODE',134,<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CARRIER_NAME','AVINCA',<client-id>,'client',2)
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('AIRLINE_NUMRIC_CODE',134,<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CARRIER_NAME','AVINCA',<client-id>,'client',2);
 
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('DFP_GENERATION','true',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_ORGID','1snn5n9w',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_MID','avianca_master',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT','true',<client-id>,'client',2)
-insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT_PATH','6ac3e976c146/92ff2d2716e2/launch-9a08edba7641-staging.min.js',<client-id>,'client',2)
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('DFP_GENERATION','true',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_ORGID','1snn5n9w',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('CYBS_DM_MID','avianca_master',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT','true',<client-id>,'client',2);
+insert into client.additionalproperty_tbl (key,value,externalid,type,scope) values ('ADOBE_TARGET_SCRIPT_PATH','6ac3e976c146/92ff2d2716e2/launch-9a08edba7641-staging.min.js',<client-id>,'client',2);
