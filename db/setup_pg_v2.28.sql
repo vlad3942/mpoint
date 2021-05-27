73a193a160994e241fe207ca653e97c6b0c3fdef
Omkar Shirole <omkar.shirole@cellpointmobile.com>
2021-05-27 18:27:27 +0530

    Scope added

--- db/setup_pg_v2.28.sql
+++ db/setup_pg_v2.28.sql
@@ -1,3 +1,3 @@
 -- CMP-5595 & CMP-5596
-INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled) VALUES ('COUPON_GEN', 'true',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true);
-INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled) VALUES ('COUPON_BUCKET_NAME', 'av-sp',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true);
+INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_GEN', 'true',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
+INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_BUCKET_NAME', 'av-sp',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
