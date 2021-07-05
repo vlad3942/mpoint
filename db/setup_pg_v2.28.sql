cd6de0b800163c44903d0ed4b343394dd7531cd0
Omkar Shirole <omkar.shirole@cellpointmobile.com>
2021-07-05 11:34:46 +0530

    CMP-5546: Post auth fraud rule MPGS

--- db/setup_pg_v2.28.sql
+++ db/setup_pg_v2.28.sql
@@ -2,3 +2,11 @@
 INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_GEN', 'true',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
 INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_BUCKET_NAME', 'av-sp',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
 
+-- CMP-5546
+INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<isPostFraudAttemp1>OR<isPostFraudAttemp2>OR<isPostFraudAttemp3>OR<isPostFraudAttemp4>
+isPostFraudAttemp1::=<eci>=="2"AND<isCryptogrm>!==""
+isPostFraudAttemp2::=<eci>=="5"AND<isCryptogrm>!==""
+isPostFraudAttemp3::=<eci>=="1"AND<isCryptogrm>!==""
+isPostFraudAttemp4::=<eci>=="7"AND<isCryptogrm>!==""
+eci::=(card.info-3d-secure.cryptogram.@eci)
+isCryptogrm::={trim.(card,info-3d-secure,cryptogram)}', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=72;
