5216e2d65ff5613823f307d63d08f6f69ce395c9
Omkar Shirole <omkar.shirole@cellpointmobile.com>
2021-07-06 11:22:52 +0530

    CMP-5546: SQL added in correct file

--- db/setup_pg_v2.28.sql
+++ db/setup_pg_v2.28.sql
@@ -2,11 +2,3 @@
 INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_GEN', 'true',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
 INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enabled, scope) VALUES ('COUPON_BUCKET_NAME', 'av-sp',(SELECT ID FROM client.merchantaccount_tbl WHERE clientid = 10101 and pspid = 70) ,'merchant', true, 1);
 
--- CMP-5546
-INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<isPostFraudAttemp1>OR<isPostFraudAttemp2>OR<isPostFraudAttemp3>OR<isPostFraudAttemp4>
-isPostFraudAttemp1::=<eci>=="02"AND<isCryptogrm>!==""
-isPostFraudAttemp2::=<eci>=="05"AND<isCryptogrm>!==""
-isPostFraudAttemp3::=<eci>=="01"AND<isCryptogrm>!==""
-isPostFraudAttemp4::=<eci>=="07"AND<isCryptogrm>!==""
-eci::=(card.info-3d-secure.cryptogram.@eci)
-isCryptogrm::={trim.(card,info-3d-secure,cryptogram)}', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=72;
