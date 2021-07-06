a185268f70e251653ddf8f8d900f9ce87d290931
Omkar Shirole <omkar.shirole@cellpointmobile.com>
2021-07-06 09:12:13 +0530

    CMP-5546: Post Auth Fraud Rule Corrected

--- db/setup_pg_v2.28.sql
+++ db/setup_pg_v2.28.sql
@@ -4,9 +4,9 @@ INSERT INTO client.additionalproperty_tbl ( key, value, externalid, type, enable
 
 -- CMP-5546
 INSERT INTO client.additionalproperty_tbl (key, value, externalid, type) select 'post_fraud_rule', 'isPostFraudAttemp::=<isPostFraudAttemp1>OR<isPostFraudAttemp2>OR<isPostFraudAttemp3>OR<isPostFraudAttemp4>
-isPostFraudAttemp1::=<eci>=="2"AND<isCryptogrm>!==""
-isPostFraudAttemp2::=<eci>=="5"AND<isCryptogrm>!==""
-isPostFraudAttemp3::=<eci>=="1"AND<isCryptogrm>!==""
-isPostFraudAttemp4::=<eci>=="7"AND<isCryptogrm>!==""
+isPostFraudAttemp1::=<eci>=="02"AND<isCryptogrm>!==""
+isPostFraudAttemp2::=<eci>=="05"AND<isCryptogrm>!==""
+isPostFraudAttemp3::=<eci>=="01"AND<isCryptogrm>!==""
+isPostFraudAttemp4::=<eci>=="07"AND<isCryptogrm>!==""
 eci::=(card.info-3d-secure.cryptogram.@eci)
 isCryptogrm::={trim.(card,info-3d-secure,cryptogram)}', id, 'merchant' from client.merchantaccount_tbl WHERE clientid=10077 AND pspid=72;
