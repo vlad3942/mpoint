6482200116afb73e7075090a769030b1d924e718
Sagar Narayane <sagar@cellpointmobile.com>
2021-07-09 19:55:28 +0530

    CMP-5753
    PHP Unit test case fixes

--- test/api/SplitPaymentAuthorizeTest.php
+++ test/api/SplitPaymentAuthorizeTest.php
@@ -214,8 +214,11 @@ class SplitPaymentAuthorizeTest extends baseAPITest
                 $aStates[] = $row["stateid"];
             }
             if (count($aStates) >= 5) { break; }
-            usleep(1000000);// As callback happens asynchroniously, sleep a bit here in order to wait for transaction to complete in other thread
+            usleep(2000000);// As callback happens asynchroniously, sleep a bit here in order to wait for transaction to complete in other thread
         }
+
+        var_dump($aStates);
+
         $this->assertEquals(5, count($aStates) );
 
         $res =  $this->queryDB("SELECT stateid FROM Log.Message_Tbl WHERE txnid = 1 ORDER BY ID ASC");
@@ -320,9 +323,6 @@ class SplitPaymentAuthorizeTest extends baseAPITest
             if (count($aStates) >= 5) { break; }
             usleep(2000000);// As callback happens asynchroniously, sleep a bit here in order to wait for transaction to complete in other thread
         }
-
-        var_dump($aStates);
-
         $this->assertEquals(5, count($aStates) );
 
         $res =  $this->queryDB("SELECT stateid FROM Log.Message_Tbl WHERE txnid = 1 ORDER BY ID ASC");
