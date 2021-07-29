f9c714fce4f10fa95a6bcd6197a50200cb105408
kalpesh <kalpesh.parikh@cellpointmobile.com>
2021-07-29 18:20:55 +0530

    CMP-5808 Removed unwanted function

--- api/classes/general.php
+++ api/classes/general.php
@@ -2167,30 +2167,5 @@ class General
         }
         return $obj_CardResultSet;
     }
-
-    /***
-     * Function to get error msg text
-     * @param int $code Error code
-     * @return string $msg Error message
-    */
-    public static function getMsgForInvalidIIN(int $code) : string
-    {
-        $msg = '';
-        switch ($code) {
-            case 1:
-                $msg = 'Undefined Issuer Identification Number';
-                break;
-            case 2:
-                $msg = 'Issuer Identification Number is too small';
-                break;
-            case 3:
-                $msg = 'Issuer Identification Number is too great';
-                break;
-            case 4:
-                $msg = 'Issuer Identification Number is blocked';
-                break;
-        }
-        return $msg;
-    }
 }
 ?>
