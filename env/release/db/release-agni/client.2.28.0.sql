dd582da3da468eca4073e47f965407f014f8ced8
Nitin Murlidhar Gaikwad <nitin@cellpointmobile.com>
2021-08-11 18:36:15 +0530

    Revert "Remove commit keyword"
    This reverts commit abbc25a88c9f982bd0e1b69f00e16a6815dbb335.

--- env/release/db/release-agni/client.2.28.0.sql
+++ env/release/db/release-agni/client.2.28.0.sql
@@ -41,6 +41,7 @@ as select * from log.txnpassbook_tbl_10018_default;
 
 delete from log.txnpassbook_tbl_10018_default;
 
+Commit;
 
 select * from log.fn_generate_txnpassbook_partitions('log.txnpassbook_tbl',10018,'-1',30000001,50000000,1000000);
 select * from log.fn_add_primary_key_nested_partitions('log.txnpassbook_tbl',10018,30000001,50000000,1000000);
@@ -50,10 +51,12 @@ select * from log.fn_generate_txnpassbook_publications('log.txnpassbook_tbl',100
 select * from log.fn_add_txnpassbook_permissions('log.txnpassbook_tbl',10018,'Y',30000001,50000000,1000000);
 select * from log.fn_add_txnpassbook_permissions_repuser('log.txnpassbook_tbl',10018,'Y',30000001,50000000,1000000);
 
+Commit;
 
 insert into log.txnpassbook_tbl_10018
 select * from log.temp_txnpassbook_tbl_10018_default;
 
+Commit;
 
 
 ----Sarvesh (Scripts for PAL)
@@ -62,6 +65,7 @@ as select * from log.txnpassbook_tbl_10020_default;
 
 delete from log.txnpassbook_tbl_10020_default;
 
+COMMIT;
 
 select * from log.fn_generate_txnpassbook_partitions('log.txnpassbook_tbl',10020,'-1',30000001,50000000,1000000);
 select * from log.fn_add_primary_key_nested_partitions('log.txnpassbook_tbl',10020,30000001,50000000,1000000);
@@ -71,10 +75,12 @@ select * from log.fn_generate_txnpassbook_publications('log.txnpassbook_tbl',100
 select * from log.fn_add_txnpassbook_permissions('log.txnpassbook_tbl',10020,'Y',30000001,50000000,1000000);
 select * from log.fn_add_txnpassbook_permissions_repuser('log.txnpassbook_tbl',10020,'Y',30000001,50000000,1000000);
 
+COMMIT;
 
 insert into log.txnpassbook_tbl_10020
 select * from log.temp_txnpassbook_tbl_10020_default;
 
+COMMIT;
 
 
 ----Sarvesh (Scripts for CEBU)
@@ -83,6 +89,7 @@ as select * from log.txnpassbook_tbl_default where clientid=10077;
 
 delete from log.txnpassbook_tbl_default where clientid=10077;
 
+Commit;
 
 select * from log.fn_generate_txnpassbook_partitions('log.txnpassbook_tbl',10077,'Y',1,50000000,1000000);
 select * from log.fn_add_primary_key_nested_partitions('log.txnpassbook_tbl',10077,1,50000000,1000000);
@@ -92,6 +99,7 @@ select * from log.fn_add_txnpassbook_permissions('log.txnpassbook_tbl',10077,'Y'
 select * from log.fn_add_txnpassbook_permissions_repuser('log.txnpassbook_tbl',10077,'Y',1,50000000,1000000);
 select * from log.fn_generate_txnpassbook_publications('log.txnpassbook_tbl',10077,'Y',1,50000000,1000000);
 
+Commit;
 
 CREATE TABLE Log.TxnPassbook_Tbl_10077_Default PARTITION OF Log.TxnPassbook_Tbl_10077 DEFAULT;
 ALTER TABLE log.txnpassbook_tbl_10077_default OWNER TO mpoint;
@@ -110,6 +118,7 @@ GRANT SELECT ON log.txnpassbook_tbl_10077_default TO repuser;
 Insert into Log.TxnPassbook_Tbl_10077
 select * from log.temp_txnpassbook_tbl_10077_default;
 
+Commit;
 
 -----------
 
