a39c46d262ad3719b702a3fa53c78b9a211b6b8c
ramesh <ramesh@cellpointmobile.com>
2021-08-17 17:41:06 +0530

    CMP-5890 payu test case

---
+++ test/api/PayUPayAPITest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: Ramesh Tiwari
+ * Copyright: Cellpoint Mobile
+ * Link: http://www.cellpointmobile.com
+ * Project: mPoint
+ * Package:
+ * File Name:PayUPayAPITest.php
+ */
+
+require_once __DIR__ . '/PayAPITest.php';
+
+class PayUPayAPITest extends PayAPITest
+{
+    public function testSuccessfulSafetyPayPay()
+    {
+        parent::testSuccessfulPay(Constants::iPAYU_PSP, 98, 4, 7, 405,170 );
+    }
+
+    public function testSuccessfulSafetyPayPaymentPending()
+    {
+        parent::testSuccessfulPay(Constants::iPAYU_PSP, 98, 4, 7, 403,986, 1041 );
+    }
+
+    public function testSuccessfulPSEPay()
+    {
+        parent::testSuccessfulPay(Constants::iPAYU_PSP, 97, 4, 7, 405,170);
+    }
+
+}
