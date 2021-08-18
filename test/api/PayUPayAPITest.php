6d26da703bbe056117f8eef2fbc73c511e7b3b69
ramesh <ramesh@cellpointmobile.com>
2021-08-18 21:26:40 +0530

    CMP-5890 payu test case

--- test/api/PayUPayAPITest.php
+++ test/api/PayUPayAPITest.php
@@ -12,12 +12,12 @@ require_once __DIR__ . '/PayAPITest.php';
 
 class PayUPayAPITest extends PayAPITest
 {
-    public function testSuccessfulSafetyPayPay()
+    public function testSuccessfulPayUPay()
     {
         parent::testSuccessfulPay(Constants::iPAYU_PSP, 98, 4, 7, 405,170 );
     }
 
-    public function testSuccessfulSafetyPayPaymentPending()
+    public function testSuccessfulPayUPaymentPending()
     {
         parent::testSuccessfulPay(Constants::iPAYU_PSP, 98, 4, 7, 403,986, 1041 );
     }
