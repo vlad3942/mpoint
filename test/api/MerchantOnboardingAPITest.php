6a2a95081860e6c94de825e16c83ce5309846e5e
vikas gupta <vikas.gupta@cellpointmobile.com>
2021-10-18 20:31:59 +0530

    CMP-6054 PHPUnit: Integration Client Configuration

--- test/api/MerchantOnboardingAPITest.php
+++ test/api/MerchantOnboardingAPITest.php
@@ -364,4 +364,108 @@ class MerchantOnboardingAPITest extends baseAPITest
                   
     }
 
+    /**
+     * Test GET :: ClientConfiguration | Success
+     *
+     * @throws \ErrorException
+     * @throws \HTTPConnectionException
+     * @throws \HTTPSendException
+     */
+    public function testSuccessfulGetClientConfiguration()
+    {
+
+        $this->queryDB("INSERT INTO Client.Client_Tbl (id, flowid, countryid, name, username, passwd, cssurl, callbackurl) VALUES (10099, 1, 100, 'Test Client', 'Tuser', 'Tpass','https://devcpmassets.s3-ap-southeast-1.amazonaws.com', 'https://hpp2.sit-01.cellpoint.dev/views/callback.php')");
+        $this->queryDB("UPDATE Client.Client_Tbl SET smsrcpt = false where id = 10099");
+
+        $this->queryDB("INSERT INTO Client.URL_Tbl (clientid, urltypeid, url) VALUES (10099, 4, 'http://mpoint.local.cellpointmobile.com/')");
+
+        $this->queryDB("INSERT INTO Client.Account_Tbl (id, clientid) VALUES (1100, 10099)");
+        $this->queryDB("INSERT INTO Client.Keyword_Tbl (id, clientid, name, standard) VALUES (1, 10099, 'CPM', TRUE)");
+
+        $this->queryDB("INSERT INTO client.services_tbl (clientid, dcc_enabled, mcp_enabled, pcc_enabled, fraud_enabled, tokenization_enabled, splitpayment_enabled, callback_enabled, void_enabled, enabled, created, modified) VALUES (10099::integer, DEFAULT, true::boolean, true::boolean, true::boolean, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);");
+        $this->queryDB("INSERT INTO client.pm_tbl (clientid, pmid, enabled, created, modified) VALUES (10099::integer, 1::integer, DEFAULT, DEFAULT, DEFAULT);");
+        $this->queryDB("INSERT INTO client.pm_tbl (clientid, pmid, enabled, created, modified) VALUES (10099::integer, 4::integer, DEFAULT, DEFAULT, DEFAULT);");
+        $this->queryDB("INSERT INTO client.pm_tbl (clientid, pmid, enabled, created, modified) VALUES (10099::integer, 12::integer, DEFAULT, DEFAULT, DEFAULT);");
+
+        $this->constHTTPClient("/merchantservices/api/Onboarding.php?service=clientconfig&params=client_id/10099");
+
+        $this->_httpClient->connect();
+
+        $iStatus = $this->_httpClient->send($this->constHTTPHeaders('Tuser', 'Tpass'));
+        $sReplyBody = $this->_httpClient->getReplyBody();
+        $this->assertEquals(200, $iStatus);
+        $this->assertEquals('<?xml version="1.0" encoding="UTF-8"?><client_configuration><id>10099</id><name>Test Client</name><salt></salt><max_amount>0</max_amount><country_id>100</country_id><email_notification>true</email_notification><sms_notification>false</sms_notification><client_urls><client_url><id>1</id><name>Mobile Enterprise Servicebus</name><type_id>4</type_id><value>http://mpoint.local.cellpointmobile.com/</value></client_url></client_urls><merchant_urls><client_url><id>10099</id><name>Callback URL</name><type_id>7</type_id><value>https://hpp2.sit-01.cellpoint.dev/views/callback.php</value></client_url></merchant_urls><hpp_urls><client_url><id>10099</id><name>CSS URL</name><type_id>6</type_id><value>https://devcpmassets.s3-ap-southeast-1.amazonaws.com</value></client_url></hpp_urls><services><dcc>true</dcc><fraud>true</fraud><callback>true</callback><mcp>true</mcp><pcc>true</pcc><split_payment>true</split_payment><tokenization>true</tokenization><void>true</void></services><account_configurations><account_config><id>1100</id><client_id>10099</client_id><name></name><markup></markup><mobile></mobile></account_config></account_configurations><pm_configurations><pm_configuration><pm_id>1</pm_id><enabled>true</enabled></pm_configuration><pm_configuration><pm_id>4</pm_id><enabled>true</enabled></pm_configuration><pm_configuration><pm_id>12</pm_id><enabled>true</enabled></pm_configuration></pm_configurations><property_details><property_detail><property_sub_category>HPP</property_sub_category><properties><property><id>48</id><name>ADOBE_TARGET_SCRIPT</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>49</id><name>ADOBE_TARGET_SCRIPT_PATH</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>50</id><name>CYBS_DM_MID</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>51</id><name>CYBS_DM_ORGID</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>52</id><name>DFP_GEN</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>56</id><name>HPP_SHOW_CONTACT_DETAILS</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>62</id><name>SWISH_HPP_ECOMMERCE_QRCODE_ENABLE</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>53</id><name>enableHppAuthentication</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>54</id><name>getTxnStatusPollingTimeOut</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>55</id><name>googleAnalyticsId</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>57</id><name>isAutoRedirect</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>58</id><name>mandateBillingDetails</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>59</id><name>maxPollingInterval</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>60</id><name>minPollingInterval</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>61</id><name>showBillingDetails</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>63</id><name>webSessionTimeout</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property></properties></property_detail><property_detail><property_sub_category>Basic</property_sub_category><properties><property><id>1</id><name>AIRLINE_CODE</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>2</id><name>AIRLINE_NUMRIC_CODE</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>3</id><name>APPLEPAY_JS_URL</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>4</id><name>CARD_ACCEPTOR_ADDRESS</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>5</id><name>CARD_ACCEPTOR_BUSINESS_CODE</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>6</id><name>CARD_ACCEPTOR_CITY</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>7</id><name>CARD_ACCEPTOR_COUNTRY</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>8</id><name>CARD_ACCEPTOR_IDENTIFICATION_CODE</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>9</id><name>CARD_ACCEPTOR_NAME</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>10</id><name>CARD_ACCEPTOR_REGION</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>11</id><name>CARD_ACCEPTOR_ZIP</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>12</id><name>CARRIER_NAME</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>13</id><name>ENABLE_PROFILE_ANONYMIZATION</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>14</id><name>GOOGLE_PAY_JS_URL</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>15</id><name>HPP_HOST_URL</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>16</id><name>InstitutionIdentificationCode</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>18</id><name>PROFILE_EXPIRY</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>19</id><name>PROFILE_TOKEN</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>20</id><name>TICKET_ISSUE_CITY</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>21</id><name>TIMEZONE</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>17</id><name>invoiceidrules</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property></properties></property_detail><property_detail><property_sub_category>Technical</property_sub_category><properties><property><id>27</id><name>DR_SERVICE</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>37</id><name>GOMOBILE_EMAIL_SENDER</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>38</id><name>GOMOBILE_PUSH_CHANNEL</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>39</id><name>GOMOBILE_PUSH_KEYWORD</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>40</id><name>GOMOBILE_SMS_CHANNEL</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>41</id><name>GOMOBILE_SMS_KEYWORD</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>42</id><name>GOMOBILE_SMS_PASSWORD</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>43</id><name>GOMOBILE_SMS_PRICE</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>44</id><name>GOMOBILE_SMS_USERNAME</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>28</id><name>IS_LEGACY</name><data_type>1</data_type><enabled>true</enabled><mandatory>true</mandatory></property><property><id>29</id><name>IS_LEGACY_CALLBACK_FLOW</name><data_type>1</data_type><enabled>true</enabled><mandatory>true</mandatory></property><property><id>32</id><name>IS_STORE_BILLING_ADDRS</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>22</id><name>PROXY_CALLBACK</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>25</id><name>SSO_PREFERENCE</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>47</id><name>UATP_SETTLEMENT_FILE_NAME</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>33</id><name>autoFetchBalance</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>34</id><name>cumulativesettlement</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>26</id><name>debug</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>35</id><name>fetchBalancePaymentMethods</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>36</id><name>fetchBalanceUserType</name><data_type>3</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>45</id><name>ismutualexclusive</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>24</id><name>isnewcardconfig</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>30</id><name>max_session_retry_count</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>31</id><name>mvault</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>46</id><name>preferredvoidoperation</name><data_type>1</data_type><enabled>true</enabled><mandatory>false</mandatory></property><property><id>23</id><name>sessiontype</name><data_type>2</data_type><enabled>true</enabled><mandatory>false</mandatory></property></properties></property_detail></property_details></client_configuration>', $sReplyBody);
+    }
+
+    /***
+     *
+     * @throws \ErrorException
+     * @throws \HTTPConnectionException
+     * @throws \HTTPSendException
+     */
+    public function testSuccessfulPostClientConfiguration()
+    {
+        $this->bIgnoreErrors = true;
+        $this->queryDB("INSERT INTO Client.Client_Tbl (id, flowid, countryid, name, username, passwd) VALUES (10099, 1, 100, 'Test Client', 'Tuser', 'Tpass')");
+        $this->queryDB("UPDATE Client.Client_Tbl SET smsrcpt = false where id = 10099");
+
+        $this->queryDB("INSERT INTO Client.Account_Tbl (id, clientid) VALUES (1100, 10099)");
+        $this->queryDB("INSERT INTO Client.Keyword_Tbl (id, clientid, name, standard) VALUES (1, 10099, 'CPM', TRUE)");
+
+        # RQ Body
+        $xml= '<?xml version="1.0" encoding="UTF-8"?> <client_configuration>     <client_id>10099</client_id> <client_urls> <client_url> <id>1</id> <name>Single Sign-On Authentication</name> <type_id>15</type_id> <value>http://mpoint.local.cellpoint.dev/_test/simulators/login.php</value> </client_url> </client_urls><merchant_urls> <client_url> <id>10077</id> <name>Callback URL</name> <type_id>7</type_id> <value>https://hpp2.local-01.cellpoint.dev/test.php</value> </client_url> </merchant_urls><properties> <property> <id>60</id> <value>true</value> </property> <property> <id>61</id> <value>true</value> </property> </properties></client_configuration>';
+
+        # External Call
+        $this->constHTTPClient("/merchantservices/api/Onboarding.php?service=clientconfig&params=client_id/10099",'POST');
+        $this->_httpClient->connect();
+        $iStatus = $this->_httpClient->send($this->constHTTPHeaders('Tuser', 'Tpass'), $xml);
+        $sReplyBody = $this->_httpClient->getReplyBody();
+        $this->assertEquals(200, $iStatus);
+
+        # Test 1 : Merchant/HPP url
+        $res =  $this->queryDB("SELECT callbackurl, cssurl FROM CLIENT.Client_tbl where id = 10099 and callbackurl='https://hpp2.local-01.cellpoint.dev/test.php'" );
+        $this->assertIsResource($res);
+        $this->assertEquals(1, pg_num_rows($res), 'Error|Merchant/HPP url');
+
+        # Test 2 : Client URL
+        $res =  $this->queryDB("select * from client.url_tbl where urltypeid = 15 and clientid = 10099 and url= 'http://mpoint.local.cellpoint.dev/_test/simulators/login.php'" );
+        $this->assertIsResource($res);
+        $this->assertEquals(1, pg_num_rows($res), 'Error|Client url');
+
+        # Test 2 : Client URL
+        $res =  $this->queryDB("select * from client.client_property_tbl where clientid = 10099");
+        $this->assertIsResource($res);
+        $this->assertEquals(2, pg_num_rows($res), 'Error|Client Property Break');
+    }
+
+
+    /***
+     *
+     * @api PUT : ClientConfiguration
+     *
+     * @throws \ErrorException
+     * @throws \HTTPConnectionException
+     * @throws \HTTPSendException
+     */
+    public function testSuccessfulPutClientConfiguration()
+    {
+        $this->bIgnoreErrors = true;
+        $this->queryDB("INSERT INTO Client.Client_Tbl (id, flowid, countryid, name, username, passwd, cssurl, callbackurl) VALUES (10099, 1, 100, 'Test Client', 'Tuser', 'Tpass','https://devcpmassets.s3-ap-southeast-1.amazonaws.com', 'https://hpp2.sit-01.cellpoint.dev/views/callback.php')");
+        $this->queryDB("UPDATE Client.Client_Tbl SET smsrcpt = false where id = 10099");
+
+        $this->queryDB("INSERT INTO Client.Account_Tbl (id, clientid) VALUES (1100, 10099)");
+        $this->queryDB("INSERT INTO Client.Keyword_Tbl (id, clientid, name, standard) VALUES (1, 10099, 'CPM', TRUE)");
+
+        $this->queryDB("INSERT INTO Client.URL_Tbl (clientid, urltypeid, url) VALUES (10099, 4, 'http://mpoint.local.cellpointmobile.com/')");
+
+        # RQ Body
+        $xml= '<?xml version="1.0" encoding="UTF-8"?> <client_configuration> <client_id>10099</client_id> <id>10077</id> <name>CEBU Pacific Air</name> <salt>az1sx2dc3fv</salt> <max_amount>14748600</max_amount> <country_id>100</country_id> <email_notification>false</email_notification> <sms_notification>false</sms_notification> <authentication_mode>STRICT</authentication_mode> <timezone>+5:30</timezone> <client_urls> <client_url> <id>4</id> <name>Single Sign-On Authentication</name> <type_id>2</type_id> <value>//mpoint.dev-01.cellpoint.dev/_test/simulators/login.php</value> </client_url> </client_urls> <merchant_urls> <client_url> <id>10077</id> <name>Callback URL</name> <type_id>7</type_id> <value>http://hpp2.sit-01.cellpoint.dev/views/redirect_response.php</value> </client_url> </merchant_urls> <hpp_urls> <client_url> <id>10077</id> <name>CSS URL</name> <type_id>6</type_id> <value>://devcpmassets.s3-ap-southeast-1.amazonaws.com</value> </client_url> </hpp_urls> <account_configurations> <account_config> <id>100770</id> <client_id>10077</client_id> <name>CEBU Pacific Air Web 2</name> <markup>spa</markup> <mobile>9730156666</mobile> </account_config> </account_configurations> <properties> <property> <id>60</id> <value>false</value> </property> <property> <id>61</id> <value>false</value> </property> </properties> </client_configuration>';
+
+        # External Call
+        $this->constHTTPClient("/merchantservices/api/Onboarding.php?service=clientconfig&params=client_id/10099",'POST');
+        $this->_httpClient->connect();
+        $iStatus = $this->_httpClient->send($this->constHTTPHeaders('Tuser', 'Tpass'), $xml);
+        $this->assertEquals(200, $iStatus);
+    }
+
 }
