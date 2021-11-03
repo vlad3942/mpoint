68ed9a41a4ebd60f9a5bf8f9d084bdcc6c1f73d2
Priya Alamwar <priya.alamwar@cellpointmobile.com>
2021-11-03 17:21:51 +0530

    CMP-6091
    SQL script including rollback sql

---
+++ env/sit/db/client-CEBU-Temp-Fraud-Rule.sql
@@ -0,0 +1,41 @@
+------CMP-6091----------
+-------World pay-------
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<status>=="1"OR<status>=="4"AND<tempRule>
+status::=(card.info-3d-secure.additional-data.param[@name=''status''])
+tempRule::=(transaction.@type)=="5"OR(transaction.@type)=="3"' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=4 and enabled=true);
+
+------------First Data-------------
+
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<status>=="1"OR<status>=="2"OR<status>=="4"AND<tempRule>
+status::=(card.info-3d-secure.additional-data.param[@name='status'])
+tempRule::=(transaction.@type)=="5"OR(transaction.@type)=="3"' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=62 and enabled=true);
+
+-------------MPGS-------------
+
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<isPostFraudAttemp1>OR<isPostFraudAttemp2>OR<isPostFraudAttemp3>OR<isPostFraudAttemp4>AND<tempRule>
+isPostFraudAttemp1::=<eci>=="02"AND<isCryptogrm>!==""
+isPostFraudAttemp2::=<eci>=="05"AND<isCryptogrm>!==""
+isPostFraudAttemp3::=<eci>=="01"AND<isCryptogrm>!==""
+isPostFraudAttemp4::=<eci>=="07"AND<isCryptogrm>!==""
+eci::=(card.info-3d-secure.cryptogram.@eci)
+isCryptogrm::={trim.(card,info-3d-secure,cryptogram)}
+tempRule::=(transaction.@type)=="5"OR(transaction.@type)=="3"' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=72 and enabled=true);
+
+
+-------------------ROLLBACK sql--------------------
+
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<status>=="1"OR<status>=="4"
+status::=(card.info-3d-secure.additional-data.param[@name=''status''])' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=4 and enabled=true);
+
+
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<status>=="1"OR<status>=="2"OR<status>=="4"
+status::=(card.info-3d-secure.additional-data.param[@name='status'])' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=62 and enabled=true);
+
+
+UPDATE client.additionalproperty_tbl SET key = 'post_fraud_rule', value = 'isPostFraudAttemp::=<isPostFraudAttemp1>OR<isPostFraudAttemp2>OR<isPostFraudAttemp3>OR<isPostFraudAttemp4>
+isPostFraudAttemp1::=<eci>=="02"AND<isCryptogrm>!==""
+isPostFraudAttemp2::=<eci>=="05"AND<isCryptogrm>!==""
+isPostFraudAttemp3::=<eci>=="01"AND<isCryptogrm>!==""
+isPostFraudAttemp4::=<eci>=="07"AND<isCryptogrm>!==""
+eci::=(card.info-3d-secure.cryptogram.@eci)
+isCryptogrm::={trim.(card,info-3d-secure,cryptogram)}' WHERE externalid=(SELECT id from client.merchantaccount_tbl where clientid=10077 and pspid=72 and enabled=true);
