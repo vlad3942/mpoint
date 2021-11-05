a9ab0045aa8eadf4cd8ca4692e76a82913d6aa03
kalpesh.parikh <kalpesh.parikh@cellpointmobile.com>
2021-11-05 12:44:23 +0530

    CMP-6091 test commit reverted

--- webroot/mApp/api/pay.php
+++ webroot/mApp/api/pay.php
@@ -195,7 +195,7 @@ $HTTP_RAW_POST_DATA .= '</client-info>';
 $HTTP_RAW_POST_DATA .= '</pay>';
 $HTTP_RAW_POST_DATA .= '</root>';
 */
-//$obj_DOM = simpledom_load_string(file_get_contents('php://input'));
+$obj_DOM = simpledom_load_string(file_get_contents('php://input'));
 
 if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PHP_AUTH_PW", $_SERVER) === true)
 {
