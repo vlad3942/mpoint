229f7d0b845aca2b8eb8e65b3926a2e7fa7c582f
kalpesh.parikh <kalpesh.parikh@cellpointmobile.com>
2021-11-26 15:39:28 +0530

    CMP-6135 Removed message logging statement

--- webroot/callback/3d-redirect.php
+++ webroot/callback/3d-redirect.php
@@ -181,7 +181,6 @@ try
     $propertyValue = $obj_PSPConfig->getAdditionalProperties(Constants::iInternalProperty, '3DVERIFICATION');
     if ($obj_TxnInfo->hasEitherState($_OBJ_DB, array(Constants::iPAYMENT_3DS_SUCCESS_STATE, Constants::iPAYMENT_3DS_FAILURE_STATE)) === true) {
         $iStateID = Constants::iPAYMENT_3DS_DUPLICATE_STATE;
-        $obj_mPoint->newMessage($obj_TxnInfo->getID(), $iStateID, $sRawXML);
         $xml .= '<status code="' .$iStateID. '">Request already processed</status>';
     } else {
         //Log the incoming status code.
