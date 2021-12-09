bd413bdca35dab441800df3e633c1bae81e7a543
Chaitenya Yadav <chaitenya.yadav@cellpointmobile.com>
2021-12-09 10:15:13 +0530

    CPMRM-6636: Removed the host sql

--- env/release/db/release-prithvi/client.2.32.1.sql
+++ env/release/db/release-prithvi/client.2.32.1.sql
@@ -1,6 +1,4 @@
 
-INSERT INTO client.url_tbl (urltypeid, clientid, url, enabled) VALUES (17, 10101, 'payments.avianca.com', true);
-
 -- Fraud Rules
 UPDATE client.additionalproperty_tbl SET value = 'isPostFraudAttemp::=<status>=="1"OR<status>=="4"OR<tempRule>
 status::=(card.info-3d-secure.additional-data.param[@name=''status''])
