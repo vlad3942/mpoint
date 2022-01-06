1d3e69cc8bbdbf1890393eacb3ee7e7aff5f78a9
Chaitenya Yadav <chaitenya.yadav@cellpointmobile.com>
2022-01-06 15:54:54 +0530

    CMP-6298: updated review comment

--- env/sit/db/client.AV_CEBU_additionalproperty_tbl.sql
+++ env/sit/db/client.AV_CEBU_additionalproperty_tbl.sql
@@ -3,6 +3,6 @@
 --Table Name : Client.Additionalproperty_Tbl
 
 -- CMP-6298
-UPDATE client.additionalproperty_tbl SET value = false WHERE externalid = 10077 and key = 'enableHppAuthentication';
+UPDATE client.additionalproperty_tbl SET value = 'false', enabled = false WHERE externalid = 10077 and key = 'enableHppAuthentication';
 
-UPDATE client.additionalproperty_tbl SET value = false WHERE externalid = 10101 and key = 'enableHppAuthentication';
+UPDATE client.additionalproperty_tbl SET value = 'false', enabled = false WHERE externalid = 10101 and key = 'enableHppAuthentication';
