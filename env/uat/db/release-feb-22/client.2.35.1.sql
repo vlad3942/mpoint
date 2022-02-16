86a1cba8ce58ad874e134fb4fafddb28ce40d89d
kalpesh.parikh <kalpesh.parikh@cellpointmobile.com>
2022-02-16 13:18:20 +0530

    CMP-6379 Added improvement in mconsole void api

--- env/uat/db/release-feb-22/client.2.35.1.sql
+++ env/uat/db/release-feb-22/client.2.35.1.sql
@@ -1,6 +1,8 @@
 -- CMP-6433
 -- mPoint script for CEBU for turning on CMD callback for UAT environment
 
+-- Note : Below queries to be executed only on UAT and not on production until client confirmation
+
 DELETE FROM client.additionalproperty_tbl WHERE externalid=10077 and key="IS_LEGACY_CALLBACK_FLOW";
 
 INSERT INTO client.additionalproperty_tbl (key, value, enabled, externalid, type, scope) VALUES
