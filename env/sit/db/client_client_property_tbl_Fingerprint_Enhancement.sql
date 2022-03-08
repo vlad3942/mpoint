e0ca61b15edca8b95e2510c65d3aaf504e2cb6b8
sandeep <sandeep.sharma@cellpointmobile.com>
2022-03-08 17:21:37 +0530

    AVPOP-780 - Updated sql script

--- env/sit/db/client_client_property_tbl_Fingerprint_Enhancement.sql
+++ env/sit/db/client_client_property_tbl_Fingerprint_Enhancement.sql
@@ -1,5 +1,3 @@
 -- mPoint DB Scripts:  
 -- AVPOP-780
-UPDATE client.client_property_tbl
-SET value = '45ssiuz3'
-where propertyid = 51 and clientid = 10101;
+UPDATE client.client_property_tbl SET value = '45ssiuz3' where propertyid = (select id from system.client_property_tbl where name = 'CYBS_DM_ORGID') and clientid = 10101;
