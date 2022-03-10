6c8eb988e35a9a0858e18f7e98c214a0c1c4ae8f
sagar <sagar.badave@cellpointmobile.com>
2022-03-10 19:10:15 +0530

    CMP-6528

--- api/interfaces/cpm_psp.php
+++ api/interfaces/cpm_psp.php
@@ -1577,23 +1577,16 @@ abstract class CPMPSP extends Callback implements Captureable, Refundable, Voiad
 			if(General::xml2bool($isGenerateCoupon) === false || strlen($this->aCONN_INFO["paths"]["generate-receipt"]) == 0 ) { return false; }
 
             $objPaymentMethod = $this->getTxnInfo()->getPaymentMethod($this->getDBConn());
-
-            # Get Flight Departure TZ Offset
-            $departureDetails = $this->getDepartureDetails($this->getTxnInfo()->getID());
-            $flight_depart_tz_offset = ($departureDetails['departure_timezone']) ?? '+00:00';
-
-            // Extend <transaction> node
-            $extendElement = '<card-name>'.$objPaymentMethod->CardName.'</card-name>';
-            $extendElement .= '<first-departure-time-zone>'.(string)$flight_depart_tz_offset.'</first-departure-time-zone>';
-            $extendElement .= "</transaction>";
-
-            // Prepare XML
+            if(empty($this->getTxnInfo()->getOrderConfigs()) === true)
+            {
+                $this->updateTxnInfoObject();
+            }
             $body  = '<?xml version="1.0" encoding="UTF-8"?>';
 			$body .= '<root>';
 			$body .= '<generate-receipt>';
 			$body .= '<bucket-name>'.$couponBucketName.'</bucket-name>';
 			// transaction details
-			$body .= str_replace("</transaction>", $extendElement, $this->getTxnInfo()->toXML() );
+			$body .= str_replace("</transaction>","<card-name>".$objPaymentMethod->CardName."</card-name>" . "</transaction>", $this->getTxnInfo()->toXML() );
 
 			$body .= '</generate-receipt>';
 			$body .= '</root>';
