da4a6536010774a3a55c934c6643ee64d3c6386d
Priya Alamwar <priya.alamwar@cellpointmobile.com>
2022-03-14 20:24:03 +0530

    CMP-6500 Fix phpunit

--- api/classes/GenericFSP.php
+++ api/classes/GenericFSP.php
@@ -12,13 +12,12 @@
 namespace api\classes;
 
 use RDB;
-use TranslateText;
 use TxnInfo;
 
 class GenericFSP extends \CPMFRAUD
 {
     private int $FSP_ID = -1;
-    public function __construct(RDB $oDB, TranslateText $oTxt, TxnInfo $oTI, ?array $aConnInfo, int $fspId = -1)
+    public function __construct(RDB $oDB, \api\classes\core\TranslateText $oTxt, TxnInfo $oTI, ?array $aConnInfo, int $fspId = -1)
     {
         $this->FSP_ID = $fspId;
         parent::__construct($oDB, $oTxt, $oTI, $aConnInfo[$fspId]);
