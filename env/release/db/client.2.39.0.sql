912fdd5f151e981d1938d6eaf7e2d5614fc0f3e6
kalpesh.parikh <kalpesh.parikh@cellpointmobile.com>
2022-04-25 18:10:33 +0530

    CPMRM-6876 Added client level binsearch_required property for HPP Application

--- env/release/db/client.2.39.0.sql
+++ env/release/db/client.2.39.0.sql
@@ -1,3 +1,6 @@
 -- VA
 insert into client.client_property_tbl (propertyid,value,clientid)
 select id, true, 10106 from system.client_property_tbl where name = 'binsearch_required';
+
+insert into client.client_property_tbl (propertyid,value,clientid)
+select id, true, 10107 from system.client_property_tbl where name = 'binsearch_required';
