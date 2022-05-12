942a614a013fd0a2defdee068789122f9f3b384d
kalpesh.parikh <kalpesh.parikh@cellpointmobile.com>
2022-05-12 17:42:24 +0530

    Revert "CMP-6719 type error"
    This reverts commit 347c213ead635a6b2ab0c231565162b3a0fcfac4.

--- api/classes/GenericPSP.php
+++ api/classes/GenericPSP.php
@@ -162,8 +162,14 @@ class GenericPSP extends \CPMPSP
         $activePaymentMethods =  parent::getPaymentMethods($obj_PSPConfig);
         $aStatisticalData = $this->getStatisticalData('issuing_bank_%');
         $sortable = array();
-        if(is_object($activePaymentMethods->{'active-payment-methods'}->{'payment-method'}) && count($activePaymentMethods->{'active-payment-methods'}->{'payment-method'}) >= 1){
-            foreach ($activePaymentMethods->{'active-payment-methods'}->{'payment-method'} as $node) {
+        $paymentMethods = '';
+        if($activePaymentMethods->{'active-payment-methods'}->{'payment-method'}){
+            $paymentMethods .= $activePaymentMethods->{'active-payment-methods'}->{'payment-method'};
+                }else{
+            $paymentMethods .= $activePaymentMethods->{'active-payment-menthods'}->{'payment-method'};
+        }
+        if(is_object($paymentMethods) && count($paymentMethods) >= 1){
+            foreach ($paymentMethods as $node) {
                 $issuingBank = strtolower($node->issuingBank);
                 $usageCount = (int)$aStatisticalData['issuing_bank_' . $issuingBank];
                 $node->addChild('usage', $usageCount);
