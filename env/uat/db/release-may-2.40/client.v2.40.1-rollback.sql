512730432e9211bfa8deb0909d2c4014355fc13c
abhinav.shaha <abhinav.shaha@cellpointmobile.com>
2022-06-07 09:18:20 +0530

    CPMRM-17076 : UAT and Prod Sqls

---
+++ env/uat/db/release-may-2.40/client.v2.40.1-rollback.sql
@@ -0,0 +1,3 @@
+-- VA
+DELETE FROM client.client_property_tbl WHERE clientid = 10106 and propertyid = 68 AND value = 4277;
+DELETE FROM client.client_property_tbl WHERE clientid = 10107 and propertyid = 68 AND value = 4277;
