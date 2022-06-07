790f093ed53e67e1c180dfe8e7a0dec6ac356ed3
abhinav.shaha <abhinav.shaha@cellpointmobile.com>
2022-06-07 15:26:08 +0530

    CPMRM-17076 : Corrected MCC value

--- env/release/db/release-may-2.40/client.v2.40.1.sql
+++ env/release/db/release-may-2.40/client.v2.40.1.sql
@@ -1,3 +1,3 @@
 -- VA
-INSERT INTO client.client_property_tbl (clientid, propertyid, value, enabled) VALUES (10106, 68, '4277', true);
-INSERT INTO client.client_property_tbl (clientid, propertyid, value, enabled) VALUES (10107, 68, '4277', true);
+INSERT INTO client.client_property_tbl (clientid, propertyid, value, enabled) VALUES (10106, 68, '4722', true);
+INSERT INTO client.client_property_tbl (clientid, propertyid, value, enabled) VALUES (10107, 68, '4722', true);
