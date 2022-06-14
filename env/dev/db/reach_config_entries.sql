57c929cde0ca801061ad4c8116274181907cda8b
Bharat Bisht <bharat.bisht@cellpointmobile.com>
2022-06-14 20:07:47 +0530

    CPMRM-16891 comment the SQL for reach configuration. default wil be insterd by vision MO

--- env/dev/db/reach_config_entries.sql
+++ env/dev/db/reach_config_entries.sql
@@ -5,6 +5,9 @@ VALUES (<client-id>, 78, 'Reach', true, '<username>', '<password>', 0);
 
 INSERT INTO client.route_tbl (clientid, providerid) VALUES(<client-id>, 78);
 
+/*
+By default below entries will be done by MO (vision portal) only and but will be useful if you want to do the entries manually/
+
 INSERT INTO client.routeconfig_tbl(routeid, name, capturetype, mid, username, password, enabled) SELECT id, 'Reach', 2, '<MID>', '<username>', '<password>', enabled FROM client.route_tbl WHERE clientid = <client-id> AND providerid = 78;
 
 INSERT INTO client.routecountry_tbl(routeconfigid) SELECT rc.id FROM client.routeconfig_tbl rc inner join client.route_tbl r on r.id = rc.routeid WHERE r.clientid = <client-id> and rc.username ='<username>' and rc.mid = '<mid>';
@@ -20,3 +23,4 @@ INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
 INSERT INTO client.psp_property_tbl
 (clientid, propertyid, value, enabled)
 VALUES(<client-id>, (SELECT id FROM system.psp_property_tbl WHERE pspid = 78 and name = 'HMAC_SECRET'), '<HMAC-SECRET>', true);
+*/
