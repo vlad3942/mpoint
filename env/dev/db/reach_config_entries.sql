432f5a5a72a1cf9dce7bf28069e8bbe22390cfd6
Bharat Bisht <bharat.bisht@cellpointmobile.com>
2022-06-14 19:31:41 +0530

    CPMRM-16891 Apply PR comments

--- env/dev/db/reach_config_entries.sql
+++ env/dev/db/reach_config_entries.sql
@@ -1,62 +1,22 @@
-Insert into client.countrycurrency_tbl (clientid, countryid, currencyid) values (10077, 200, 840);
+Insert into client.countrycurrency_tbl (clientid, countryid, currencyid) values (<client-id>, <country-id>, <currency-id>);
 
 INSERT INTO client.merchantaccount_tbl (clientid, pspid, name, enabled, username, passwd, supportedpartialoperations)
-VALUES (10077, 78, 'Reach', true, 'cellpoint', '3BfNgyHmrXfbixW', 0);
+VALUES (<client-id>, 78, 'Reach', true, '<username>', '<password>', 0);
 
-INSERT INTO Client.MerchantSubAccount_Tbl (accountid, pspid, name) VALUES (100770, 78, '-1');
+INSERT INTO client.route_tbl (clientid, providerid) VALUES(<client-id>, 78);
 
-INSERT INTO client.route_tbl (clientid, providerid) VALUES(10077, 78);
+INSERT INTO client.routeconfig_tbl(routeid, name, capturetype, mid, username, password, enabled) SELECT id, 'Reach', 2, '<MID>', '<username>', '<password>', enabled FROM client.route_tbl WHERE clientid = <client-id> AND providerid = 78;
 
-INSERT INTO client.routeconfig_tbl (id,routeid,"name",capturetype,mid,username,"password",enabled,isdeleted) VALUES
-(614,131,'Reach_AE_VISA_MASTERCARD',2,'f30aab7f-7b56-4560-84c4-b9f3804e903a','cellpoint','3BfNgyHmrXfbixW',true,false);
+INSERT INTO client.routecountry_tbl(routeconfigid) SELECT rc.id FROM client.routeconfig_tbl rc inner join client.route_tbl r on r.id = rc.routeid WHERE r.clientid = <client-id> and rc.username ='<username>' and rc.mid = '<mid>';
 
+INSERT INTO client.routecurrency_tbl(routeconfigid) SELECT rc.id FROM client.routeconfig_tbl rc inner join client.route_tbl r on r.id = rc.routeid WHERE r.clientid = <client-id> and rc.username ='<username>' and rc.mid = '<mid>';
 
-INSERT INTO client.routecountry_tbl (id,routeconfigid,countryid,enabled) VALUES
-(783,614,200,true);
-
-
-INSERT INTO client.routecurrency_tbl (id,routeconfigid,currencyid,enabled) VALUES
-(757,614,840,true);
-
-
-INSERT INTO client.routefeature_tbl (id,clientid,routeconfigid,featureid,enabled) VALUES
-(231,10077,614,5,true),
-(232,10077,614,6,true),
-(233,10077,614,9,true);
-
+INSERT INTO client.routefeature_tbl (clientid,routeconfigid,featureid,enabled) VALUES
+(<client-id>,(SELECT id FROM client.routeconfig_tbl WHERE name = '<name>' and mid = '<mid>' ),<feature-id>,true);
 
 INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
-(614,1,true);
-INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
-(614,7,true);
-INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
-(614,8,true);
-INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
-(614,3,true);
-INSERT INTO client.routepm_tbl (routeconfigid,pmid,enabled) VALUES
-(614,22,true);
-
-
-INSERT INTO client.cardaccess_tbl
-(clientid, cardid, enabled, pspid, countryid, stateid, "position", preferred, psp_type, installment, capture_method, capture_type, walletid, dccenabled)
-VALUES(10077, 7, true, 78, 200, 1, NULL, false, 1, 0, 0, 2, NULL, false);
-
-INSERT INTO client.cardaccess_tbl
-(clientid, cardid, enabled, pspid, countryid, stateid, "position", preferred, psp_type, installment, capture_method, capture_type, walletid, dccenabled)
-VALUES(10077, 8, true, 78, 200, 1, NULL, false, 1, 0, 0, 2, NULL, false);
-
-INSERT INTO client.cardaccess_tbl
-(clientid, cardid, enabled, pspid, countryid, stateid, "position", preferred, psp_type, installment, capture_method, capture_type, walletid, dccenabled)
-VALUES(10077, 3, true, 78, 200, 1, NULL, false, 1, 0, 0, 2, NULL, false);
-
-INSERT INTO client.cardaccess_tbl
-(clientid, cardid, enabled, pspid, countryid, stateid, "position", preferred, psp_type, installment, capture_method, capture_type, walletid, dccenabled)
-VALUES(10077, 1, true, 78, 200, 1, NULL, false, 1, 0, 0, 2, NULL, false);
-
-INSERT INTO client.cardaccess_tbl
-(clientid, cardid, enabled, pspid, countryid, stateid, "position", preferred, psp_type, installment, capture_method, capture_type, walletid, dccenabled)
-VALUES(10077, 22, true, 78, 200, 1, NULL, false, 1, 0, 0, 2, NULL, false);
+((SELECT id FROM client.routeconfig_tbl WHERE name = '<name>' and mid = '<mid>' ),<card-id>,true);
 
 INSERT INTO client.psp_property_tbl
 (clientid, propertyid, value, enabled)
-VALUES(10077, (SELECT id FROM system.psp_property_tbl WHERE pspid = 78 and name = 'REACH_HMAC_SECRET'), 'QjTCPUk3zHita3Usk2CWEXqS5h9M571S5kBf2AFP0RVHiRO4umxuJ1sTN3ZcF89M', true);
+VALUES(<client-id>, (SELECT id FROM system.psp_property_tbl WHERE pspid = 78 and name = 'HMAC_SECRET'), '<HMAC-SECRET>', true);
