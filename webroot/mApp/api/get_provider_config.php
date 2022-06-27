307391a52902be46a6d9af262636f85fcf05c837
Devansh Sah <devansh.sah@cellpointmobile.com>
2022-06-27 20:20:35 +0530

    CPMRM-16547 testing get/route call

--- webroot/mApp/api/get_provider_config.php
+++ webroot/mApp/api/get_provider_config.php
@@ -7,7 +7,7 @@
  * @package mAPP
  * @version 1.0
  */
-
+use api\classes\merchantservices\Repositories\ReadOnlyConfigRepository;
 // Require Global Include File
 require_once("../../inc/include.php");
 // Require API for Simple DOM manipulation
@@ -25,6 +25,9 @@ require_once(sCLASS_PATH ."/crs/RouteFeature.php");
 require_once(sCLASS_PATH ."/crs/TransactionTypeConfig.php");
 require_once(sCLASS_PATH ."/crs/CardState.php");
 require_once(sCLASS_PATH ."/crs/FxServiceType.php");
+require_once(sCLASS_PATH ."/clientinfo.php");
+
+
 
 /*Sample Request
 All Fields Required
@@ -34,62 +37,75 @@ account_id -> Client's account id (optional)
 http://mpoint.local.cellpointmobile.com/mApp/api/get_provider_config.php?id=50&client_id=10069
 */
 
-if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PHP_AUTH_PW", $_SERVER) === true)
-{
+$obj_DOM = simpledom_load_string(file_get_contents('php://input'));
+$_OBJ_TXT = new api\classes\core\TranslateText(array(sLANGUAGE_PATH . $_POST['language'] ."/global.txt", sLANGUAGE_PATH . $_POST['language'] ."/custom.txt"), sSYSTEM_PATH, 0, "UTF-8");
 
-    $clientId = (integer)$_REQUEST['client_id'];
-    $pspId = (integer)$_REQUEST['id'];
-    $accountId = (integer)$_REQUEST['account_id'];
-    $code = Validate::valClient($_OBJ_DB, $clientId);
-
-    if ($code === 100)
-    {
-        if(!$accountId){
-            $clientAccountIds = PSPConfig::getClientAccountIds($_OBJ_DB, $clientId, $pspId);
-            $accountId = $clientAccountIds[0];
+if (array_key_exists("PHP_AUTH_USER", $_SERVER) === true && array_key_exists("PHP_AUTH_PW", $_SERVER) === true) {
+    if($_SERVER['REQUEST_METHOD'] == "GET"){
+        if($_REQUEST['client_id'] && $_REQUEST['id']){
+            $clientId = (integer)$_REQUEST['client_id'];
+            $pspId = (integer)$_REQUEST['id'];
+            $accountId = (integer)$_REQUEST['account_id'];
+            $code = Validate::valClient($_OBJ_DB, $clientId);
+            if ($code === 100) {
+                if(!$accountId){
+                    $clientAccountIds = PSPConfig::getClientAccountIds($_OBJ_DB, $clientId, $pspId);
+                    $accountId = $clientAccountIds[0];
+                }
+                $obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $clientId, $accountId, $pspId);
+                if($obj_PSPConfig){
+                    $toXML = "<client_provider_configuration>".$obj_PSPConfig->toXML(Constants::iPrivateProperty)."</client_provider_configuration>";
+                } else {
+                    header("HTTP/1.1 400 Bad Request");
+                    $toXML = "<status><code>99</code><description>Invalid PSP</description></status>";
+                }
+            } else {
+                header("HTTP/1.1 400 Bad Request");
+                if ($code === 2) {
+                    $description = "Invalid Client ID";
+                } elseif ($code === 3) {
+                    $description = "Unknown Client ID";
+                } elseif ($code === 4) {
+                    $description = "Client Disabled";
+                } else {
+                    $description = "Undefined Client ID";
+                }
+                $toXML = '<status><code>' . $code . '</code><description>' . $description . '</description></status>';
+            }
         }
-        $obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $clientId, $accountId, $pspId);
-        if($obj_PSPConfig){
-            $toXML = "<client_provider_configuration>".$obj_PSPConfig->toXML(Constants::iPrivateProperty)."</client_provider_configuration>";
-        } else {
-            header("HTTP/1.1 400 Bad Request");
-            $toXML = "<status><code>99</code><description>Invalid PSP</description></status>";
-        }
+    } else {
+        $clientId = (integer)$obj_DOM->client_provider_configuration->clientid;
+        $transactionId = (integer)$obj_DOM->client_provider_configuration->transaction->{'id'};
+        $cardId = (integer)$obj_DOM->client_provider_configuration->transaction->{'cardid'};
 
-    }
-    elseif ($code === 2)
-    {
-        header("HTTP/1.1 400 Bad Request");
+        $obj_ClientInfo = ClientInfo::produceInfo($obj_DOM->client_provider_configuration->{'client-info'}, CountryConfig::produceConfig($_OBJ_DB, (integer) $obj_DOM->client_provider_configuration->{'client-info'}->mobile["country-id"]), $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $obj_TxnInfo = TxnInfo::produceInfo($transactionId, $_OBJ_DB);
+        $repository = new ReadOnlyConfigRepository($_OBJ_DB,$obj_TxnInfo);
+        $accountId = $obj_TxnInfo->getClientConfig()->getAccountConfig()->getID();
+        $pspId = $obj_TxnInfo->getPSPID();
+        $obj_mPoint = new General($_OBJ_DB, $_OBJ_TXT);
 
-        $toXML = '<status><code>'.$code.'</code><description>Invalid Client ID</description></status>';
+        $route_config = General::getRouteConfiguration(
+            $repository,
+            $_OBJ_DB,
+            $obj_mPoint,
+            $obj_TxnInfo,
+            $obj_ClientInfo,
+            $aHTTP_CONN_INFO['routing-service'], $clientId,
+            $obj_TxnInfo->getCountryConfig()->getID(), $obj_TxnInfo->getCurrencyConfig()->getID(), $obj_TxnInfo->getAmount(),
+            $cardId, NULL, NULL, NULL,
+            NULL);
+        //print_r($route_config); exit;
+        $pspId = '40';
+        $obj_PSPConfig = PSPConfig::produceConfig($_OBJ_DB, $clientId, $obj_TxnInfo->getClientConfig()->getAccountConfig()->getID(), $pspId);
+        $toXML = "<client_provider_configuration>".$obj_PSPConfig->toXML(Constants::iPrivateProperty).$obj_PSPConfig->toRouteConfigXML()."</client_provider_configuration>";
     }
-    elseif ($code === 3)
-    {
-        header("HTTP/1.1 400 Bad Request");
 
-        $toXML = '<status><code>'.$code.'</code><description>Unknown Client ID</description></status>';
-    }
-    elseif ($code === 4)
-    {
-        header("HTTP/1.1 400 Bad Request");
-
-        $toXML = '<status><code>'.$code.'</code><description>Client Disabled</description></status>';
-    }
-    else
-    {
-        header("HTTP/1.1 400 Bad Request");
-
-        $toXML = '<status><code>'.$code.'</code><description>Undefined Client ID</description></status>';
-    }
-}
-else
-{
+} else {
     header("HTTP/1.1 401 Unauthorized");
-
     $toXML = '<status><code>401</code><description>Authorization required</description></status>';
 }
 header("Content-Type: text/xml; charset=\"UTF-8\"");
-
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 echo $toXML;
 ?>
