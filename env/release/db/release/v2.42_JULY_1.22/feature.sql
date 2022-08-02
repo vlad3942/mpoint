0d45a9373f130cc7f1bf7ac52cba81eca5a30f2f
Devansh Sah <devansh.sah@cellpointmobile.com>
2022-08-02 19:13:33 +0530

    Added Scripts

---
+++ env/release/db/release/v2.42_JULY_1.22/feature.sql
@@ -0,0 +1,4 @@
+insert into client.routefeature_tbl (clientid,routeconfigid,featureid,enabled) values (10077,455,6,true);
+insert into client.routefeature_tbl (clientid,routeconfigid,featureid,enabled) values (10077,462,6,true);
+insert into client.routefeature_tbl (clientid,routeconfigid,featureid,enabled) values (10077,2,6,true);
+insert into client.routefeature_tbl (clientid,routeconfigid,featureid,enabled) values (10077,457,6,true);
